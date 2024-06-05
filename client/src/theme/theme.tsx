f6e92ca5e055b142fbf2d07ee4bc4e1b7b7ec122
eziraa <ezratgab@gmail.com>
2024-06-05 15:45:42 +0300

    creating light and dark theme instances to apply dark mode

--- client/src/theme/theme.tsx
+++ client/src/theme/theme.tsx
@@ -13,3 +13,36 @@ export type Theme = {
     large: "2.2rem";
   };
 };
+
+export const lightTheme: Theme = {
+  colors: {
+    primary: "#000",
+    secondary: "#fff",
+  },
+  backgrounds: {
+    primary: "#fff",
+    secondary: "#000",
+  },
+  fontSizes: {
+    small: "1rem",
+    medium: "1.6rem",
+    large: "2.2rem",
+  },
+};
+
+export const darkTheme: Theme = {
+  colors: {
+    primary: "#fff",
+    secondary: "#000",
+  },
+  backgrounds: {
+    primary: "#000",
+    secondary: "#fff",
+  },
+  fontSizes: {
+    small: "1rem",
+    medium: "1.6rem",
+    large: "2.2rem",
+  },
+};
+
