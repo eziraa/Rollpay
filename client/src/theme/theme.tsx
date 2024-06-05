193917453fce04668145c5c8775aa6d21a45a3e8
eziraa <ezratgab@gmail.com>
2024-06-05 15:43:29 +0300

    creating a theme object

---
+++ client/src/theme/theme.tsx
@@ -0,0 +1,15 @@
+export type Theme = {
+  colors: {
+    primary: string;
+    secondary: string;
+  };
+  backgrounds: {
+    primary: string;
+    secondary: string;
+  };
+  fontSizes: {
+    small: "1rem";
+    medium: "1.6rem";
+    large: "2.2rem";
+  };
+};
