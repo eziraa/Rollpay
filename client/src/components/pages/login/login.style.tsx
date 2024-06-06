adf4a9cb1042125f51f0107203206d38256500d5
eziraa <ezratgab@gmail.com>
2024-06-06 23:58:41 +0300

    updating style of login components

--- client/src/components/pages/login/login.style.tsx
+++ client/src/components/pages/login/login.style.tsx
@@ -3,7 +3,7 @@ import { ThemeProps } from "../../../typo/theme/theme";
 import { addOpacityToColor } from "../../utils/convertor/add_opacity_color";
 
 export const LoginContainer = styled.div<ThemeProps>`
-  background-color: ${({ theme }) => theme.backgrounds.primary};
+  background-color: ${({ theme }) => theme.colors.secondary};
   color: ${({ theme }) => theme.backgrounds.primary};
   width: 25%;
   height: 60%;
@@ -16,8 +16,22 @@ export const LoginContainer = styled.div<ThemeProps>`
   gap: 1rem;
   padding: 1rem;
   border-radius: 0.5rem;
-  border: 1px solid
-    ${({ theme }) => addOpacityToColor(0.5, theme.colors.primary)};
+  /* border: 1px solid
+    ${({ theme }) => addOpacityToColor(0.5, theme.colors.primary)}; */
+`;
+
+export const Link = styled.a`
+  color: ${({ theme }) => theme.colors.link};
+  text-decoration: none;
+  font-size: 1.6rem;
+  font-weight: 500;
+
+  &:hover {
+    color: ${({ theme }) => addOpacityToColor(0.5, theme.colors.link)};
+    cursor: pointer;
+    text-decoration: underline blue;
+    transition: all 0.3s;
+  }
 `;
 
 
