1cb953b60b275d648c7c9cb01ee910318f7c405c
eziraa <ezratgab@gmail.com>
2024-06-06 13:57:03 +0300

    updating the login components style

--- client/src/components/pages/login/login.style.tsx
+++ client/src/components/pages/login/login.style.tsx
@@ -1,9 +1,35 @@
 import styled from "styled-components";
 import { ThemeProps } from "../../../typo/theme/theme";
+import { hexToRgb } from "../../utils/convertor/hex_to_rgba";
 
 export const LoginContainer = styled.div<ThemeProps>`
-  background-color: ${({ theme }) => theme.backgrounds.secondary};
+  background-color: ${({ theme }) => theme.backgrounds.primary};
+  color: ${({ theme }) => theme.backgrounds.primary};
   width: 30%;
   height: 60%;
-  box-shadow: 0px 0px 1rem 0px ${({ theme }) => theme.colors.secondary}px;
+  box-shadow: 0px 0px 1rem
+    ${({ theme }) => {
+      const rgb = hexToRgb(theme.colors.primary);
+      return rgb
+        ? `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.4)`
+        : theme.colors.primary;
+    }};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  border: 1px solid
+    ${({ theme }) => {
+      const rgb = hexToRgb(theme.colors.secondary);
+      return rgb
+        ? `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.2)`
+        : theme.backgrounds.primary;
+    }};
 `;
+
+
+
+
