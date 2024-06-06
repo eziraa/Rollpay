4e2f377fd57055104e5cce684d44ff0ae2935f30
eziraa <ezratgab@gmail.com>
2024-06-06 09:30:25 +0300

    imlementing the toggleTheme method  to toggle the theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./App.css";
 import { ThemeContext } from "./contexts/themeContext";
 import { ThemeProvider } from "styled-components";
 import { Header } from "./components/sections/header/header";
+import { Theme, darkTheme, lightTheme } from "./theme/theme";
 
 function App() {
-  const { theme, toggleTheme } = useContext(ThemeContext);
+  const [theme, setTheme] = useState<Theme>(lightTheme);
+  const toggleTheme = () => {
+    setTheme(theme === lightTheme ? darkTheme : lightTheme);
+  };
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <ThemeProvider theme={theme}>
