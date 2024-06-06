e37b4ae1078fe7daf6ab6984f05b3b97542323e9
eziraa <ezratgab@gmail.com>
2024-06-06 08:38:11 +0300

    creating the header cimponent

---
+++ client/src/components/sections/header/header.tsx
@@ -0,0 +1,11 @@
+import { HeaderContainer, HeaderTitle } from "./header.style";
+
+export const Header = () => {
+  return (
+    <>
+      <HeaderContainer>
+        <HeaderTitle>String</HeaderTitle>
+      </HeaderContainer>
+    </>
+  );
+};
