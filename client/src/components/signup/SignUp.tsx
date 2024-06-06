cf527540a0e788fd4b9758e80a7880e605f9eb6d
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-06 11:16:59 +0300

    Added Error Message Component

--- client/src/components/signup/SignUp.tsx
+++ client/src/components/signup/SignUp.tsx
@@ -2,21 +2,40 @@ import {
   SignUpContainer,
   Title,
   Button,
+  Input,
   InputName,
   InputBox,
+  ErrorMessage,
 } from "./SignUp.style";
 const SignUp = () => {
   return (
     <SignUpContainer>
       <Title>Create Account</Title>
-      <InputName>Username: </InputName>
-      <InputBox />
-      <InputName>Email: </InputName>
-      <InputBox />
-      <InputName>Password: </InputName>
-      <InputBox />
-      <InputName>Confirm Password: </InputName>
-      <InputBox />
+      <Input>
+        <InputName>Username: </InputName>
+        <InputBox />
+      </Input>
+      <ErrorMessage>Error Message </ErrorMessage>
+
+      <Input>
+        <InputName>Email: </InputName>
+        <InputBox />
+      </Input>
+
+      <ErrorMessage>Error Message </ErrorMessage>
+      <Input>
+        <InputName>Password: </InputName>
+        <InputBox />
+      </Input>
+
+      <ErrorMessage>Error Message </ErrorMessage>
+      <Input>
+        <InputName>Confirm Password: </InputName>
+        <InputBox />
+      </Input>
+
+      <ErrorMessage>Error Message </ErrorMessage>
+
       <br />
 
       <Button> Create </Button>
