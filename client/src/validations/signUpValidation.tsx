740820169342df4c30468a34b227af5c9d25b2a0
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-06 15:05:13 +0300

    Created form validation for sign up page

---
+++ client/src/validations/signUpValidation.tsx
@@ -0,0 +1,24 @@
+import * as Yup from "yup";
+const pattern = RegExp(
+  "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*{~?\\]])(?!.*[<>])[A-Za-z\\d!@#$%^&*{~?\\]]+$"
+);
+
+export const SignUpValidation = Yup.object({
+  username: Yup.string()
+    .min(3, "Username should be at least 3 characters")
+    .required("Please Enter name"),
+  email: Yup.string()
+    .email("Please enter valid email")
+    .required("Please Enter email"),
+  password: Yup.string()
+    .min(8, "Password should be atleast 8 characters")
+    .matches(
+      pattern,
+      "Password should be a mixture of uppercase, lowercase letters, numbers, and special character"
+    )
+    .required("Please Enter password"),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref("password")],
+    "Password does not match"
+  ),
+});
