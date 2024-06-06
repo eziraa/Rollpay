8fba6c764031b060e35b3e7310b161bef24171e6
eziraa <ezratgab@gmail.com>
2024-06-06 13:57:33 +0300

    Adding input fields to the login page

--- client/src/components/pages/login/login.tsx
+++ client/src/components/pages/login/login.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../sections/header/header";
+import {
+  Form,
+  Input,
+  InputContainer,
+  Label,
+  Button,
+  Title,
+} from "../../utils/form_elements/form.style";
 import { HomeContainer } from "../home/homepage.style";
+import { LoginContainer } from "./login.style";
 
 export const LoginPage = () => {
   return (
     <HomeContainer>
       <Header />
-      <LoginPage />
+      <LoginContainer>
+        <Title>Log In</Title>
+        <Form>
+          <InputContainer>
+            <Label>User name</Label>
+            <Input />
+          </InputContainer>
+          <InputContainer>
+            <Label>Password</Label>
+            <Input />
+          </InputContainer>
+          <Button type="submit" onClick={(e) => e.stopPropagation()}>
+            Login
+          </Button>
+        </Form>
+      </LoginContainer>
     </HomeContainer>
   );
 };
