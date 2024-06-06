9d6a19e1ae8db2265ae7dbf44effb3bec12430d4
eziraa <ezratgab@gmail.com>
2024-06-06 22:15:02 +0300

    Adding phone number

--- client/src/components/signup/SignUp.tsx
+++ client/src/components/signup/SignUp.tsx
@@ -1,4 +1,7 @@
 import { Button } from "../../utils/buttons/submit.style";
+// import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+// import "./signup.css";
 import {
   SignUpContainer,
   Title,
@@ -9,17 +12,20 @@ import {
   Form,
   ToggleIcon,
   PasswordContainer,
+  PhoneNumberInput,
 } from "./SignUp.style";
+
 import { useFormik } from "formik";
 import { SignUpValidation } from "../../validations/signUpValidation";
 import { FaRegEyeSlash } from "react-icons/fa";
 import { useState } from "react";
 import { IoEyeOutline } from "react-icons/io5";
-
+import "./signup.css";
 const SignUp = () => {
   const initialValues = {
     username: "",
     email: "",
+    phone_number: "",
     password: "",
     confirmPassword: "",
   };
@@ -60,6 +66,18 @@ const SignUp = () => {
             value={values.email}
             onBlur={handleBlur}
             onChange={handleChange}
+          />
+          {errors.email && <ErrorMessage>{errors.email} </ErrorMessage>}
+        </InputBox>
+
+        <InputBox>
+          <InputName htmlFor="phone_number">Phone Number: </InputName>
+          <PhoneNumberInput
+            value={values.phone_number}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            placeholder=""
+            inputClass="phone-number"
           />
           {errors.email && <ErrorMessage>{errors.email} </ErrorMessage>}
         </InputBox>
