35f73479a923060c446f013f0ed58c527b8581b7
eziraa <ezratgab@gmail.com>
2024-06-06 15:54:06 +0300

    upating the PasswordVisibility

--- client/src/components/utils/password_visiblity/password.style.tsx
+++ client/src/components/utils/password_visiblity/password.style.tsx
@@ -1,11 +1,10 @@
-import { VscEyeClosed } from "react-icons/vsc";
 import styled from "styled-components";
 import { ThemeProps } from "../../../typo/theme/theme";
 
-export const PasswordVisible = styled(VscEyeClosed)<ThemeProps>`
+export const PasswordVisible = styled.div<ThemeProps>`
   font-size: 1.6rem;
   color: ${({ theme }) => theme.colors.primary};
   cursor: pointer;
   position: absolute;
-  right: 0.3rem;
+  right: 0.5rem;
 `;
