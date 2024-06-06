2d8c53a8fd1951ef5a045056603ac320e65209af
eziraa <ezratgab@gmail.com>
2024-06-06 10:00:35 +0300

    cereating login page

---
+++ client/src/components/pages/login/login.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../sections/header/header";
+import { HomeContainer } from "../home/homepage.style";
+
+export const LoginPage = () => {
+  return (
+    <HomeContainer>
+      <Header />
+      <LoginPage />
+    </HomeContainer>
+  );
+};
