b2d2d38018c8ca6db7c88f5efaf6c55f0e6e3730
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-06 15:02:33 +0300

    Added Logo

---
+++ client/src/utils/logo/logo.tsx
@@ -0,0 +1,17 @@
+import { LogoContainer, LogoImage, LogoText } from "./logo.style";
+export default function Logo() {
+  return (
+    <LogoContainer>
+      <LogoImage />
+      <LogoText>
+        <span>R</span>
+        <span>O</span>
+        <span>L</span>
+        <span>L</span>
+        <span>P</span>
+        <span>A</span>
+        <span>Y</span>
+      </LogoText>
+    </LogoContainer>
+  );
+}
