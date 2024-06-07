46e2fd5e3e17499e81116ebfe789854a514ccb6d
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-07 09:29:34 +0300

    Added style to login page

--- client/src/components/pages/login/login.style.tsx
+++ client/src/components/pages/login/login.style.tsx
@@ -14,17 +14,35 @@ export const LoginContainer = styled.div<ThemeProps>`
   align-items: center;
   justify-content: center;
   gap: 1rem;
+  position: relative;
   padding: 1rem;
   border-radius: 0.5rem;
   /* border: 1px solid
     ${({ theme }) => addOpacityToColor(0.5, theme.colors.primary)}; */
 `;
+export const Checkbox = styled.input`
+  height: 1.5rem;
+  width: 1.5rem;
+`;
+export const Text = styled.p`
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: 1.2rem;
+  padding: 0rem 1rem;
+`;
+export const CheckboxContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
 
 export const Link = styled.a`
   color: ${({ theme }) => theme.colors.link};
   text-decoration: none;
-  font-size: 1.6rem;
-  font-weight: 500;
+  font-size: 1.2rem;
+  position: absolute;
+  padding-left: 0.5rem;
+  /* right: 1rem; */
+  /* font-weight: 500; */
 
   &:hover {
     color: ${({ theme }) => addOpacityToColor(0.5, theme.colors.link)};
@@ -33,7 +51,11 @@ export const Link = styled.a`
     transition: all 0.3s;
   }
 `;
-
-
-
-
+export const LinkContainer = styled.div`
+  position: relative;
+  width: 30rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  /* justify-items: left; */
+`
