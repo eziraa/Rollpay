39b31bcbca0d16a71e59159ccb67e0f21491d177
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-07 16:54:53 +0300

    Added rest_framework to installed apps

--- server/payroll/settings.py
+++ server/payroll/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employee'
+    'employee',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'payroll.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'payroll',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1354'
     }
 }
 
