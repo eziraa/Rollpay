e117c861e5d3a4c331279a5163a70253ac4b7a1d
eziraa <ezratgab@gmail.com>
2024-06-07 14:57:20 +0300

    updating close btn

--- client/src/components/utils/buttons/close.tsx
+++ client/src/components/utils/buttons/close.tsx
@@ -1,15 +1,17 @@
 import { RiCloseFill } from "react-icons/ri";
 import styled from "styled-components";
+import { addOpacityToColor } from "../convertor/add_opacity_color";
 
 export const CloseIcon = styled(RiCloseFill)`
-  color: ${({ theme }) => theme.backgrounds.secondary};
+  color: ${({ theme }) => addOpacityToColor(0.6, theme.colors.primary)};
+
   border: none;
   border-radius: 0.5rem;
   padding: 0.5rem;
-  font-size: 2.4rem;
+  font-size: 3.4rem;
   cursor: pointer;
   &:hover {
-    color: ${({ theme }) => theme.backgrounds.primary};
+    color: ${({ theme }) => theme.colors.primary};
   }
   position: absolute;
   right: 0.4rem;
