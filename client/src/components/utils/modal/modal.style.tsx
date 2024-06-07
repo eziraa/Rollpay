39956b7a8ec651350f0b1e30558f5dce87dd5876
eziraa <ezratgab@gmail.com>
2024-06-07 14:52:06 +0300

    styling modal components

---
+++ client/src/components/utils/modal/modal.style.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ModalContainer = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  top: 0;
+`;
+
+export const ModalContent = styled.div`
+  width: 50%;
+  height: fit-content;
+  background-color: ${({ theme }) => theme.backgrounds.primary};
+  display: flex;
+  border-radius: 1rem;
+`;
