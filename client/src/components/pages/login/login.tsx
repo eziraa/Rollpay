b51655e2bd6dd632c3a2d9759258e4f2a9f35d94
eziraa <ezratgab@gmail.com>
2024-06-07 11:35:09 +0300

    updating the layout of login components

--- client/src/components/pages/login/login.tsx
+++ client/src/components/pages/login/login.tsx
@@ -18,6 +18,8 @@ import {
   CustomLink,
   LoginContainer,
   LinkContainer,
+  Checkbox,
+  ActionsContainer,
 } from "./login.style";
 import { RiEyeFill, RiEyeOffFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
@@ -46,23 +48,24 @@ export const LoginPage = () => {
               </PasswordVisible>
             </PasswordContainer>
           </InputContainer>
-          <CheckboxContainer>
+          <ActionsContainer>
+            <CheckboxContainer>
+              <Checkbox type="checkbox" /> <Text> Remember me</Text>
+            </CheckboxContainer>
             <CustomLink>
-              <Link to="/forgot_password"> Frogot Password? </Link>
+              <Link to="/forgot_password">Frogot Password?</Link>
             </CustomLink>
-          </CheckboxContainer>
+          </ActionsContainer>
 
           <Button type="submit" onClick={(e) => e.stopPropagation()}>
             Login
           </Button>
         </Form>
         <LinkContainer>
-          <Text>
-            Don't have an account?{" "}
-            <CustomLink>
-              <Link to="/signup"> Sign up </Link>
-            </CustomLink>
-          </Text>
+          <Text>Don't have an account? </Text>
+          <CustomLink>
+            <Link to="/signup"> Sign up </Link>
+          </CustomLink>
         </LinkContainer>
       </LoginContainer>
       <FlashMessage />
