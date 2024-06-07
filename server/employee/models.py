849b16566a985aca01f5b7a763238c663cc4237a
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-07 16:52:11 +0300

    Created model for employee

--- server/employee/models.py
+++ server/employee/models.py
@@ -1,3 +1,17 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+class Employee(models.Model):
+    Male = 'M'
+    Female = 'F'
+    GENDER_CHOICES = (
+        (Male, 'Male'),
+        (Female, 'Female'),
+    )
+    first_name = models.CharField(max_length=255,null=False)
+    last_name = models.CharField(max_length=255,null=False)
+    phone_number = models.CharField(max_length=15,null=False)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=False)
+    email = models.EmailField(max_length=100, unique=True,null=False)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_hire = models.DateField(auto_now=True,null=False)
+    position = models.CharField(max_length=100,null=False)
