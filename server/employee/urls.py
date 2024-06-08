2f3be22852a4bc6e4f59f4790c93ba212a5f98a6
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-08 12:19:04 +0300

    URL to add profile picture

--- server/employee/urls.py
+++ server/employee/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('add/', views.add_employee),
-    path('update/<id>', views.update_employee)
-]
+    path('update/<id>/', views.update_employee),
+    path('upload-profile-picture/<id>/', views.upload_profile_pic),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
