7bab1a3d6c2bde91c8f5d8f20ddb6e2131024e6e
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-08 11:09:57 +0300

    URL to update employee information

--- server/employee/urls.py
+++ server/employee/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_employee)
+    path('add/', views.add_employee),
+    path('update/<id>', views.update_employee)
 ]
