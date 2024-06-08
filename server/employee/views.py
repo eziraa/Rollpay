fba56803d46bb97b15afaf286fcf0967d7ae2bd6
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-08 11:10:18 +0300

    View to update employee information

--- server/employee/views.py
+++ server/employee/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializer import EmployeeSerializer
+from .models import Employee
 # Create your views here.
 
 @api_view(['POST'])
@@ -12,4 +13,16 @@ def add_employee(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response({"message": "Could not create employee"}, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['PATCH'])
+def update_employee(request, id):
+    try:
+        employe = Employee.objects.get(pk=id)
+    except Employee.DoesNotExist:
+        return Response({"error": "Employee not found"}, status=status.HTTP_404_NOT_FOUND)
 
+    serializer = EmployeeSerializer(instance=employe, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
