8a0b8dfc2ed20e8fff905a9f3a133f89f9fdd6fa
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-08 15:35:01 +0300

    Removed profile picture and added first and lastname

--- server/employee/models.py
+++ server/employee/models.py
@@ -1,17 +1,19 @@
 # Create your models here.
 from django.db import models
-from django.contrib.auth.models import User
 
 
-class Employee(User):
+class Employee():
     Male = 'M'
     Female = 'F'
     GENDER_CHOICES = (
         (Male, 'Male'),
         (Female, 'Female'),
     )
-    phone_number = models.CharField(max_length=15,null=False)
+    first_name = models.CharField(max_length=255,null=False)
+    last_name = models.CharField(max_length=255,null=False)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=False)
+    email = models.EmailField(max_length=100, unique=True,null=False)
+    phone_number = models.CharField(max_length=15,null=False)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_hire = models.DateField(auto_now=True,null=False)
     position = models.CharField(max_length=100,null=False)
