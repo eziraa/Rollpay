b0fb2faa35a0f4dadb71199714b99325ff7359c3
eziraa <ezratgab@gmail.com>
2024-06-08 14:40:05 +0300

    updating the modal component to close the modal when click event trigrred outside the modal content

--- client/src/components/utils/modal/modal.tsx
+++ client/src/components/utils/modal/modal.tsx
@@ -1,11 +1,23 @@
+import { useRef, useState } from "react";
 import { CloseIcon } from "../buttons/close";
 import { ModalContainer, ModalContent } from "./modal.style";
 
 export const Modal = ({ children }: { children: React.ReactNode }) => {
+  const [openModal, closeModal] = useState(true);
+  if (!openModal) return;
   return (
-    <ModalContainer>
-      <ModalContent>
-        <CloseIcon />
+    <ModalContainer
+      onClick={(e) => {
+        e.stopPropagation();
+        closeModal(false);
+      }}
+    >
+      <ModalContent onClick={(e) => e.stopPropagation()}>
+        <CloseIcon
+          onClick={() => {
+            closeModal(false);
+          }}
+        />
         {children}
       </ModalContent>
     </ModalContainer>
