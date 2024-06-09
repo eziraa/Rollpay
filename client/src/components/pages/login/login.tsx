1f41e1b699477336203c1a4a7f559f56fe30bb45
eziraa <ezratgab@gmail.com>
2024-06-10 00:09:35 +0300

    updating user login to implement user login in

--- client/src/components/pages/login/login.tsx
+++ client/src/components/pages/login/login.tsx
@@ -25,44 +25,61 @@ import {
 import { RiEyeFill, RiEyeOffFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { useFormik } from "formik";
-import { SignUpSchema } from "../../../schema/SignUpSchema";
+import { LogInSchema } from "../../../schema/log-in-schema";
+import { ErrorMessage } from "../signup/SignUp.style";
+import { useAppDispatch } from "../../../utils/customHook";
+import { loginRequested } from "../../../store/user/userSLice";
 
 export const LoginPage = () => {
+  const dispatcher = useAppDispatch();
+
   const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
   const togglePasswordVisiblity = () => {
     setPasswordVisible(!passwordVisible);
   };
-
-  const formHandler = useFormik({
-    initialValues: {
-      empID: "",
-      username: "",
-      password: "",
-      confirm_password: "",
-    },
-    validationSchema: SignUpSchema,
-    onSubmit: (values, _) => {
-      console.log(values);
-    },
-  });
+  const { touched, values, handleBlur, handleChange, handleSubmit, errors } =
+    useFormik({
+      initialValues: {
+        username: "",
+        password: "",
+        confirm_password: "",
+      },
+      validationSchema: LogInSchema,
+      onSubmit: (values, _) => {
+        dispatcher(loginRequested(values));
+      },
+    });
   return (
     <HomeContainer>
       <Header />
       <LoginContainer>
         <Title>Log In</Title>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <InputContainer>
             <Label>User name</Label>
-            <Input />
+            <Input
+              name="username"
+              value={values.username}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            />
+            {errors.username && <ErrorMessage>{errors.username} </ErrorMessage>}
           </InputContainer>
           <InputContainer>
             <Label>Password</Label>
             <PasswordContainer>
-              <input type={passwordVisible ? "text" : "password"} />
+              <input
+                type={passwordVisible ? "text" : "password"}
+                name="password"
+                value={values.password}
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
               <PasswordVisible onClick={togglePasswordVisiblity}>
                 {passwordVisible ? <RiEyeFill /> : <RiEyeOffFill />}
               </PasswordVisible>
             </PasswordContainer>
+            {errors.password && <ErrorMessage>{errors.password} </ErrorMessage>}
           </InputContainer>
           <ActionsContainer>
             <CheckboxContainer>
@@ -72,7 +89,6 @@ export const LoginPage = () => {
               <Link to="/forgot_password">Frogot Password?</Link>
             </CustomLink>
           </ActionsContainer>
-
           <Button type="submit" onClick={(e) => e.stopPropagation()}>
             Login
           </Button>
