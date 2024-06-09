f2cb790599d41aa929fd8b39e8443e84384ede34
eziraa <ezratgab@gmail.com>
2024-06-09 20:48:48 +0300

    making call the sign up watcher

--- client/src/utils/saga.tsx
+++ client/src/utils/saga.tsx
@@ -2,5 +2,5 @@ import { all } from "redux-saga/effects";
 import { watchSetFlashMessage } from "../store/notification/flashmessageSaga";
 import { watchUserSignUp } from "../store/user/userSaga";
 export function* rootSaga() {
-  yield all([watchSetFlashMessage(), watchUserSignUp]);
+  yield all([watchSetFlashMessage(), watchUserSignUp()]);
 }
