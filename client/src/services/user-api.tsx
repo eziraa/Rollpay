673b33492bc6d4217af5e36a5a8ac8cf21173b40
eziraa <ezratgab@gmail.com>
2024-06-09 22:35:32 +0300

    updating regsiter remote operation method to return approraite response

--- client/src/services/user-api.tsx
+++ client/src/services/user-api.tsx
@@ -1,13 +1,26 @@
-import axios from "axios";
+/* eslint-disable no-unsafe-optional-chaining */
+import axios, { AxiosError } from "axios";
 import { SignUpParams } from "../typo/user/params";
-import { UserResponse } from "../typo/user/response";
 import api from "../config/router/api";
+import { SignUpResponse } from "../typo/user/response";
 
 const signUp = async (values: SignUpParams) => {
-  const employee = await axios
-    .post<UserResponse>(api + "/user/register", values)
-    .then((res) => res.data);
-  return employee;
+  const response = await axios
+    .post<SignUpResponse>(api + "/user/register", values)
+    .then((res) => {
+      return {
+        success: "User sign up successfully",
+        code: res.status,
+      };
+    })
+    .catch((err: AxiosError) => {
+      const { error } = err.response?.data as { error: string };
+      return {
+        error: error,
+        code: err.response?.status,
+      } as { error: string; code: number };
+    });
+  return response;
 };
 
 const UserAPI = {
