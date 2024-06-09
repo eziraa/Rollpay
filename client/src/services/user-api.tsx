4fbaf2b8e9471225b511165e1c93f69d2a07305c
eziraa <ezratgab@gmail.com>
2024-06-10 00:10:30 +0300

    creating remote api operation for user login

--- client/src/services/user-api.tsx
+++ client/src/services/user-api.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unsafe-optional-chaining */
 import axios, { AxiosError } from "axios";
-import { SignUpParams } from "../typo/user/params";
+import { LoginParams, SignUpParams } from "../typo/user/params";
 import api from "../config/router/api";
 import { SignUpResponse } from "../typo/user/response";
 
@@ -23,8 +23,28 @@ const signUp = async (values: SignUpParams) => {
   return response;
 };
 
+const login = async (values: LoginParams) => {
+  const response = await axios
+    .post<SignUpResponse>(api + "/user/login", values)
+    .then((res) => {
+      return {
+        success: "User Logged in successfully",
+        code: res.status,
+      };
+    })
+    .catch((err: AxiosError) => {
+      const { error } = err.response?.data as { error: string };
+      return {
+        error: error,
+        code: err.response?.status,
+      } as { error: string; code: number };
+    });
+  return response;
+};
+
 const UserAPI = {
   signUp,
+  login,
 };
 
 export default UserAPI;
