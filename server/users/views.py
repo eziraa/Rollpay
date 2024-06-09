e8072f7279f47d5c492a09489340d767764894b5
eziraa <ezratgab@gmail.com>
2024-06-09 15:14:08 +0300

    creating a view to register user

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def register_user(request):
+    try:
+        data = json.loads(request.body)
+        user = User.objects.create_user(
+            username=data['username'], password=data['password'])
+        user.save()
+        return JsonResponse({'message': 'User registered successfully'}, status=201)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
