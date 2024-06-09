18f6c50017fc763073e8c27e7f2ea968bd07138f
eziraa <ezratgab@gmail.com>
2024-06-09 22:36:53 +0300

    updating register middle ware notify appropriate notification to reducers when register

--- client/src/store/user/userSaga.tsx
+++ client/src/store/user/userSaga.tsx
@@ -1,34 +1,45 @@
 /* eslint-disable react-refresh/only-export-components */
 import { PayloadAction } from "@reduxjs/toolkit";
-import { SagaReturnType, call, put, takeEvery } from "redux-saga/effects";
+import { call, put, takeEvery } from "redux-saga/effects";
 import { setFlashMessage } from "../notification/flashMesssageSlice";
 import { SignUpParams } from "../../typo/user/params";
 import UserAPI from "../../services/user-api";
 import { signUpFinished } from "./userSLice";
+import { SignUpResponse } from "../../typo/user/response";
 
 function* userSignUp(action: PayloadAction<SignUpParams>) {
   try {
-    const user: SagaReturnType<typeof UserAPI.signUp> = yield call(
-      UserAPI.signUp,
-      action.payload
-    );
-    yield put(signUpFinished(user));
-    yield put(
-      setFlashMessage({
-        color: "green",
-        status: true,
-        title: "User sign up",
-        desc: "You are successfully signed up",
-        duration: 3,
-      })
-    );
+    const response: SignUpResponse = yield call(UserAPI.signUp, action.payload);
+
+    if (response.code === 201) {
+      yield put(signUpFinished());
+      yield put(
+        setFlashMessage({
+          color: "green",
+          status: true,
+          title: "User sign up",
+          desc: response.success,
+          duration: 3,
+        })
+      );
+    } else {
+      yield put(
+        setFlashMessage({
+          color: "red",
+          status: true,
+          title: "User sign up",
+          desc: response.error,
+          duration: 3,
+        })
+      );
+    }
   } catch (e) {
     yield put(
       setFlashMessage({
         color: "red",
         status: true,
         title: "User sign up",
-        desc: "User sign up failed ",
+        desc: "User sign up failed try again!!",
         duration: 3,
       })
     );
