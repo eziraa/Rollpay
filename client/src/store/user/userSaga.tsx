27b86154793e84bf04da13e3fbfd0c55325feab9
eziraa <ezratgab@gmail.com>
2024-06-10 00:12:17 +0300

    creating saga middleware to control user login

--- client/src/store/user/userSaga.tsx
+++ client/src/store/user/userSaga.tsx
@@ -2,9 +2,9 @@
 import { PayloadAction } from "@reduxjs/toolkit";
 import { call, put, takeEvery } from "redux-saga/effects";
 import { setFlashMessage } from "../notification/flashMesssageSlice";
-import { SignUpParams } from "../../typo/user/params";
+import { LoginParams, SignUpParams } from "../../typo/user/params";
 import UserAPI from "../../services/user-api";
-import { signUpFinished } from "./userSLice";
+import { loginFinished, signUpFinished } from "./userSLice";
 import { SignUpResponse } from "../../typo/user/response";
 
 function* userSignUp(action: PayloadAction<SignUpParams>) {
@@ -46,6 +46,49 @@ function* userSignUp(action: PayloadAction<SignUpParams>) {
   }
 }
 
+function* userLogin(action: PayloadAction<LoginParams>) {
+  try {
+    const response: SignUpResponse = yield call(UserAPI.login, action.payload);
+
+    if (response.code === 200) {
+      yield put(loginFinished());
+      yield put(
+        setFlashMessage({
+          color: "green",
+          status: true,
+          title: "User log in",
+          desc: response.success,
+          duration: 3,
+        })
+      );
+    } else {
+      yield put(
+        setFlashMessage({
+          color: "red",
+          status: true,
+          title: "User log in",
+          desc: response.error,
+          duration: 3,
+        })
+      );
+    }
+  } catch (e) {
+    yield put(
+      setFlashMessage({
+        color: "red",
+        status: true,
+        title: "User log in",
+        desc: "User log in failed try again!!",
+        duration: 3,
+      })
+    );
+  }
+}
+
 export function* watchUserSignUp() {
   yield takeEvery("user/signUpRequested", userSignUp);
+}
+
+export function* watchUserLogin() {
+  yield takeEvery("user/loginRequested", userLogin);
 }
