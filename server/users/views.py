76fe50b8be7343fd51c1d80e3e0ff60fc53759cd
eziraa <ezratgab@gmail.com>
2024-06-09 15:15:02 +0300

    creating a view for user login

--- server/users/views.py
+++ server/users/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 import json
 
@@ -16,3 +17,15 @@ def register_user(request):
         return JsonResponse({'message': 'User registered successfully'}, status=201)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=400)
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def login_user(request):
+    data = json.loads(request.body)
+    user = authenticate(username=data['username'], password=data['password'])
+    if user is not None:
+        login(request, user)
+        return JsonResponse({'message': 'Login successful'}, status=200)
+    else:
+        return JsonResponse({'error': 'Invalid credentials'}, status=400)
