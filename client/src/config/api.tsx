f370bc5367c460275e8eca4ae965c8812e71946b
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-09 14:36:26 +0300

    Moved api file to config folder

---
+++ client/src/config/api.tsx
@@ -0,0 +1,3 @@
+const api = import.meta.env.VITE_REACT_APP_API_URL;
+
+export default api;
--- client/src/config/router/api.tsx
+++
@@ -1,3 +0,0 @@
-const api = process.env.REACT_APP_API_URL; // Outputs: https://yourapi.com
-
-export default api;
