3fa4fb3abc33d5b11c096702a4e4beb1b385e569
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-09 15:44:06 +0300

    Changed name of logo

--- client/src/components/utils/logo/logo.tsx
+++ client/src/components/utils/logo/logo.tsx
@@ -4,13 +4,29 @@ export default function Logo() {
     <LogoContainer>
       <LogoImage />
       <LogoText>
+        <span>E</span>
+        <span>M</span>
+        <span>P</span>
+        <span>L</span>
+        <span>O</span>
+        <span>Y</span>
+        <span>E</span>
+        <span>E</span>
+        <span> </span>
+        <span>P</span>
+        <span>A</span>
+        <span>Y</span>
         <span>R</span>
         <span>O</span>
         <span>L</span>
         <span>L</span>
-        <span>P</span>
-        <span>A</span>
+        <span> </span>
+        <span>S</span>
         <span>Y</span>
+        <span>S</span>
+        <span>T</span>
+        <span>E</span>
+        <span>M</span>
       </LogoText>
     </LogoContainer>
   );
