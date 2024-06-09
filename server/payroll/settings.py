8841a62cfd62cd2995aa5b4ca0979d625a5929f2
eziraa <ezratgab@gmail.com>
2024-06-09 20:26:18 +0300

    Adding corsheaders middleware to the Setting MIDDLEWARE list

--- server/payroll/settings.py
+++ server/payroll/settings.py
@@ -47,12 +47,14 @@ INSTALLED_APPS = [
 
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    "django.middleware.csrf.CsrfViewMiddleware",
+    'corsheaders.middleware.CorsMiddleware',  # Ensure this is included
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
