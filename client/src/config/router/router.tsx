46ff264ae064709dffade8248608f05d30c5a777
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-11 01:16:15 +0300

    Created route for home page

--- client/src/config/router/router.tsx
+++ client/src/config/router/router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "../../components/pages/signup/sign-up";
 import UpdateEmployee from "../../components/sections/updateEmployee/UpdateEmployee";
-import { HomePage } from "../../components/pages/home/homepage";
 import { ForgotPassword } from "../../components/pages/forgot_password/forgot_password";
+import { LoginPage } from "../../components/pages/login/login";
+import { HomePage } from "../../components/pages/home/homepage";
 export const RouterConfig = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home-page" element={<HomePage />} />
       <Route path="/forgot_password" element={<ForgotPassword />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/edit-profile" element={<UpdateEmployee />} />
