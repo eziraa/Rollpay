2051e1031f5a5c8189ac11c9ecb932159ce494f2
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-10 12:11:21 +0300

    Updated API

--- client/src/services/employee-api.tsx
+++ client/src/services/employee-api.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { AddEmpParams } from "../typo/employee/params";
-import api from "../config/router/api";
+import { API } from "../config/api";
 import { EmployeeResponse } from "../typo/employee/response";
 
 const addEmp = async (values: AddEmpParams) => {
   const employee = await axios
-    .post<EmployeeResponse>(api + "/employee/add", values)
+    .post<EmployeeResponse>(API + "/employee/add", values)
     .then((res) => res.data);
   return employee;
 };
