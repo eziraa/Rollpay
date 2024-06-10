1488f37a25cb370eadcfe6be396e9ad8edbc705c
eziraa <ezratgab@gmail.com>
2024-06-10 03:31:38 +0300

    Adding Auth provider to tha  root compent App

--- client/src/config/router/router.tsx
+++ client/src/config/router/router.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "../../components/pages/login/login";
 import { ForgotPassword } from "../../components/pages/forgot_password/forgot_password";
-import SignUp from "../../components/pages/signup/SignUp";
+import SignUp from "../../components/pages/signup/sign-up";
 import UpdateEmployee from "../../components/sections/updateEmployee/UpdateEmployee";
 import { HomePage } from "../../components/pages/home/homepage";
 export const RouterConfig = () => (
