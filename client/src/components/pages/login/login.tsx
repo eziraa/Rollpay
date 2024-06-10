8c7a0fa62997911cfe6bd0f5687cd511e509cc64
eziraa <ezratgab@gmail.com>
2024-06-10 11:11:29 +0300

    cleaning code

--- client/src/components/pages/login/login.tsx
+++ client/src/components/pages/login/login.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
-import { Header } from "../../sections/header/header";
 import {
   Form,
   Input,
@@ -11,7 +10,6 @@ import {
   PasswordContainer,
 } from "../../utils/form_elements/form.style";
 import { PasswordVisible } from "../../utils/password_visiblity/password.style";
-import { HomeContainer } from "../home/homepage.style";
 import {
   Text,
   CheckboxContainer,
@@ -29,12 +27,12 @@ import { ErrorMessage } from "../signup/SignUp.style";
 import { useAppDispatch, useAppSelector } from "../../../utils/customHook";
 import { loginRequested } from "../../../store/user/userSLice";
 import { useAuth } from "../../../contexts/authContext";
-import { AddButton } from "../../sections/add_employee/add-employee.style";
 
 export const LoginPage = () => {
   const dispatcher = useAppDispatch();
   const user = useAppSelector((state) => state.user);
   const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+  const { isAuthenticated } = useAuth();
   const togglePasswordVisiblity = () => {
     setPasswordVisible(!passwordVisible);
   };
@@ -45,76 +43,67 @@ export const LoginPage = () => {
       confirm_password: "",
     },
     validationSchema: LogInSchema,
-    onSubmit: (values, _) => {
-      dispatcher(loginRequested(values));
+    onSubmit: async (values, _) => {
+      await dispatcher(loginRequested(values));
     },
   });
-  const { isAuthenticated, setIsAuthenticated } = useAuth();
-  const logout = () => {
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken");
-    setIsAuthenticated(false);
-    // Redirect to login page or perform other actions as needed
-  };
+
   return (
-    <HomeContainer>
-      <Header />
-      {!isAuthenticated || !user.is_login ? (
-        <LoginContainer>
-          <Title>Log In</Title>
-          <Form onSubmit={handleSubmit}>
-            <InputContainer>
-              <Label>User name</Label>
-              <Input
-                name="username"
-                value={values.username}
+    !isAuthenticated && (
+      <LoginContainer>
+        <Title>Log In</Title>
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(e);
+          }}
+        >
+          <InputContainer>
+            <Label>User name</Label>
+            <Input
+              name="username"
+              value={values.username}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            />
+            {errors.username && <ErrorMessage>{errors.username} </ErrorMessage>}
+          </InputContainer>
+          <InputContainer>
+            <Label>Password</Label>
+            <PasswordContainer>
+              <input
+                type={passwordVisible ? "text" : "password"}
+                name="password"
+                value={values.password}
                 onBlur={handleBlur}
                 onChange={handleChange}
               />
-              {errors.username && (
-                <ErrorMessage>{errors.username} </ErrorMessage>
-              )}
-            </InputContainer>
-            <InputContainer>
-              <Label>Password</Label>
-              <PasswordContainer>
-                <input
-                  type={passwordVisible ? "text" : "password"}
-                  name="password"
-                  value={values.password}
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                />
-                <PasswordVisible onClick={togglePasswordVisiblity}>
-                  {passwordVisible ? <RiEyeFill /> : <RiEyeOffFill />}
-                </PasswordVisible>
-              </PasswordContainer>
-              {errors.password && (
-                <ErrorMessage>{errors.password} </ErrorMessage>
-              )}
-            </InputContainer>
-            <ActionsContainer>
-              <CheckboxContainer>
-                <Checkbox type="checkbox" /> <Text> Remember me</Text>
-              </CheckboxContainer>
-              <CustomLink>
-                <Link to="/forgot_password">Frogot Password?</Link>
-              </CustomLink>
-            </ActionsContainer>
-            <Button type="submit" onClick={(e) => e.stopPropagation()}>
-              Login
-            </Button>
-          </Form>
-          <LinkContainer>
-            <Text>Don't have an account? </Text>
+              <PasswordVisible onClick={togglePasswordVisiblity}>
+                {passwordVisible ? <RiEyeFill /> : <RiEyeOffFill />}
+              </PasswordVisible>
+            </PasswordContainer>
+            {errors.password && <ErrorMessage>{errors.password} </ErrorMessage>}
+          </InputContainer>
+          <ActionsContainer>
+            <CheckboxContainer>
+              <Checkbox type="checkbox" /> <Text> Remember me</Text>
+            </CheckboxContainer>
             <CustomLink>
-              <Link to="/signup"> Sign up </Link>
+              <Link to="/forgot_password">Frogot Password?</Link>
             </CustomLink>
-          </LinkContainer>
-        </LoginContainer>
-      ) : (
-        <AddButton onClick={logout}>Log Out</AddButton>
-      )}
-    </HomeContainer>
+          </ActionsContainer>
+          <Button type="submit" onClick={(e) => e.stopPropagation()}>
+            Login
+          </Button>
+        </Form>
+        <LinkContainer>
+          <Text>Don't have an account? </Text>
+          <CustomLink>
+            <Link to="/signup"> Sign up </Link>
+          </CustomLink>
+        </LinkContainer>
+      </LoginContainer>
+    )
   );
 };
+
