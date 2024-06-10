d504bc3ad82e7fa4d43b05aced13ca973d84e496
eziraa <ezratgab@gmail.com>
2024-06-10 11:19:16 +0300

    updating routing for code cleaning

--- client/src/config/router/router.tsx
+++ client/src/config/router/router.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LoginPage } from "../../components/pages/login/login";
-import { ForgotPassword } from "../../components/pages/forgot_password/forgot_password";
+import { ForgotPassword } from "../../components/pages/forgot_password/send_otp";
 import SignUp from "../../components/pages/signup/sign-up";
 import UpdateEmployee from "../../components/sections/updateEmployee/UpdateEmployee";
 import { HomePage } from "../../components/pages/home/homepage";
 export const RouterConfig = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/forgot_password" element={<ForgotPassword />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/add-employee" element={<HomePage />} />
       <Route path="/edit-profile" element={<UpdateEmployee />} />
     </Routes>
   </Router>
