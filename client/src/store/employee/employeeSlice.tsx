826cda17e1d4aa00c955d7bbc27818f77a64560c
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-11 10:12:32 +0300

    Slice for getting employee info

--- client/src/store/employee/employeeSlice.tsx
+++ client/src/store/employee/employeeSlice.tsx
@@ -3,6 +3,7 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { EmployeeState } from "../../typo/employee/states";
 import { AddEmpParams } from "../../typo/employee/params";
+import { EmployeeResponse } from "../../typo/employee/response";
 
 const InitialEmpState: EmployeeState = {
   adding: false,
@@ -21,11 +22,28 @@ const EmployeeSlice = createSlice({
       state.adding = false;
       state.task = undefined;
     },
+    listEmpRequested: (state) => {
+      state.loading = true;
+    },
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    listEmpDone: (state, payload: PayloadAction<EmployeeResponse[]>) => {
+      state.employees = payload.payload;
+      state.adding = false;
+      state.task = undefined;
+      state.loading = false;
+    },
     setTask: (state, task: PayloadAction<string | undefined>) => {
       state.task = task.payload;
     },
   },
 });
-export const { addEmpRequested, addEmpDone, setTask } = EmployeeSlice.actions;
+export const {
+  addEmpRequested,
+  addEmpDone,
+  listEmpRequested,
+  listEmpDone,
+  setTask,
+} = EmployeeSlice.actions;
 
 export default EmployeeSlice.reducer;
