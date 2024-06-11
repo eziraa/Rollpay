18b6820ff39ec71a787da6bb1266b65f7a1254c9
eziraa <ezratgab@gmail.com>
2024-06-11 08:01:04 +0300

    folder restucturing

---
+++ client/src/components/sections/forgot_password/send_otp.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import { ForgotPasswordContainer } from "./forgot_password.style";
+import { Title } from "../signup/SignUp.style";
+import {
+  Button,
+  Form,
+  Input,
+  InputContainer,
+  Label,
+} from "../../utils/form_elements/form.style";
+import { useAppDispatch } from "../../../utils/customHook";
+import { setFlashMessage } from "../../../store/notification/flashMesssageSlice";
+function generateOTP() {
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  return otp.toString();
+}
+export const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const dispatcher = useAppDispatch();
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+    emailjs
+      .send(
+        "service_tfrx1er",
+        "template_3ol2yro",
+        {
+          to_email: email,
+          otp: generateOTP(),
+        },
+        "k3GlkvUNSNjbu-6bp"
+      )
+      .then(
+        () => {
+          dispatcher(
+            setFlashMessage({
+              color: "green",
+              status: true,
+              title: "Send OTP",
+              desc: "OTP sent successfully",
+              duration: 3,
+            })
+          );
+        },
+        () => {
+          dispatcher(
+            setFlashMessage({
+              color: "red",
+              status: true,
+              title: "Send OTP",
+              desc: "Failed to send OTP",
+              duration: 3,
+            })
+          );
+        }
+      );
+  };
+
+  return (
+    <ForgotPasswordContainer>
+      <Title>Reset Your Password</Title>
+      <Form onSubmit={sendEmail}>
+        <InputContainer>
+          <Label>Enter your email</Label>
+          <Input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </InputContainer>
+        <Button type="submit">Send OTP</Button>
+      </Form>
+    </ForgotPasswordContainer>
+  );
+};
