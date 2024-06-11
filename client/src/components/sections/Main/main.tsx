a7a92aedc61f7421cb6e9e9914fbc2981c065730
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-11 10:07:58 +0300

    Added table and search for main section

--- client/src/components/sections/Main/main.tsx
+++ client/src/components/sections/Main/main.tsx
@@ -1,26 +1,32 @@
-import { AddButton, MainContainer, MainHeader, Title } from "./main.style";
-// import { Search } from "../../utils/search/search";
+import {
+  AddButton,
+  ListBody,
+  MainContainer,
+  MainHeader,
+  Title,
+} from "./main.style";
+import { Search } from "../../utils/search/search";
 // import Table from "../table/table";
-import { useAppDispatch, useAppSelector } from "../../../utils/customHook";
+import { useAppDispatch } from "../../../utils/customHook";
 import { setTask } from "../../../store/employee/employeeSlice";
 import { ADD_EMP } from "../../../utils/constants/tasks";
-import { AddEmployee } from "../add_employee/add-employee";
+import Table from "../table/table";
 
 const Main = () => {
   const dispatcher = useAppDispatch();
-  const employee = useAppSelector((state) => state.employee);
 
   return (
     <MainContainer>
       <MainHeader>
         <Title>All Employees</Title>
         <AddButton onClick={() => dispatcher(setTask(ADD_EMP))}>Add</AddButton>
-        {employee.task === ADD_EMP && <AddEmployee />}
       </MainHeader>
-      {/* <ListBody>
+      <ListBody>
         <Search />
+      </ListBody>
+      <ListBody>
         <Table />
-      </ListBody> */}
+      </ListBody>
     </MainContainer>
   );
 };
