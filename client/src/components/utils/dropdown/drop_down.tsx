41a6b4d10989cae58e04e6628f0b9963f6351dd7
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-11 10:09:49 +0300

    Created a drop down component

---
+++ client/src/components/utils/dropdown/drop_down.tsx
@@ -0,0 +1,15 @@
+import { DropDownContainer, DropDownItem } from "./drop__down.style";
+
+function DropDown() {
+  return (
+    <DropDownContainer name="page">
+      <DropDownItem value="10">10</DropDownItem>
+      <DropDownItem value="20">20</DropDownItem>
+      <DropDownItem value="30">30</DropDownItem>
+      <DropDownItem value="40">40</DropDownItem>
+      <DropDownItem value="50">50</DropDownItem>
+    </DropDownContainer>
+  );
+}
+
+export default DropDown;
