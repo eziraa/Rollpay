a218721f953d02b3f0c211d883b07b320a068fed
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-11 10:07:08 +0300

    Added a salary menu item

--- client/src/components/sections/left-menu/left-menu.tsx
+++ client/src/components/sections/left-menu/left-menu.tsx
@@ -1,30 +1,29 @@
 import {
-    AddUserIcon,
-    HomeIcon,
-    LeftMenuContainer,
-    MenuItem,
-    MenuItemText,
-    UsersIcon,
-  } from "./left-menu.style";
-  
-  function LeftMenu() {
-    return (
-      <LeftMenuContainer>
-        <MenuItem>
-          <HomeIcon />
-          <MenuItemText>Home</MenuItemText>
-        </MenuItem>
-        <MenuItem>
-          <UsersIcon />
-          <MenuItemText>All Employees</MenuItemText>
-        </MenuItem>
-        <MenuItem>
-          <AddUserIcon />
-          <MenuItemText>Add Employee</MenuItemText>
-        </MenuItem>
-      </LeftMenuContainer>
-    );
-  }
-  
-  export default LeftMenu;
-  
+  SalaryIcon,
+  HomeIcon,
+  LeftMenuContainer,
+  MenuItem,
+  MenuItemText,
+  UsersIcon,
+} from "./left-menu.style";
+
+function LeftMenu() {
+  return (
+    <LeftMenuContainer>
+      <MenuItem>
+        <HomeIcon />
+        <MenuItemText>Home</MenuItemText>
+      </MenuItem>
+      <MenuItem>
+        <UsersIcon />
+        <MenuItemText>All Employees</MenuItemText>
+      </MenuItem>
+      <MenuItem>
+        <SalaryIcon />
+        <MenuItemText>Salary</MenuItemText>
+      </MenuItem>
+    </LeftMenuContainer>
+  );
+}
+
+export default LeftMenu;
