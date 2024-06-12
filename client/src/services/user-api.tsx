08b3e2ca3f66a9f5a13e0d891069e2c4104e4d10
eziraa <ezratgab@gmail.com>
2024-06-12 13:22:59 +0300

    updating remote api operation for user register , login and logout oprations

--- client/src/services/user-api.tsx
+++ client/src/services/user-api.tsx
@@ -4,33 +4,6 @@ import axios, { AxiosError } from "axios";
 import { LoginParams, SignUpParams } from "../typo/user/params";
 import { API } from "../config/api";
 import { SignUpResponse } from "../typo/user/response";
-axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
-  "accessToken"
-)}`;
-axios.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      const refreshToken = localStorage.getItem("refreshToken");
-      if (refreshToken) {
-        try {
-          const response = await axios.post(API + "/refresh-token", {
-            refreshToken,
-          });
-          const { accessToken } = response.data;
-          localStorage.setItem("accessToken", accessToken);
-          originalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
-          return axios(originalRequest);
-        } catch (error) {
-          console.error("Token refresh failed", error);
-        }
-      }
-    }
-    return Promise.reject(error);
-  }
-);
 const signUp = async (values: SignUpParams) => {
   const response = await axios
     .post<SignUpResponse>(API + "/user/register", values)
@@ -51,25 +24,24 @@ const signUp = async (values: SignUpParams) => {
 };
 
 const login = async (values: LoginParams) => {
-  const response = await axios
-    .post(API + "/user/login", values)
-    .then((res) => {
-      const { accessToken, refreshToken } = res.data;
-      localStorage.setItem("accessToken", accessToken);
-      localStorage.setItem("refreshToken", refreshToken);
-      return {
-        success: "User Logged in successfully",
-        code: res.status,
-      };
-    })
-    .catch((err: AxiosError) => {
-      const { error } = err.response?.data as { error: string };
-      return {
-        error: error,
-        code: err.response?.status,
-      } as { error: string; code: number };
+  try {
+    const { username, password } = values;
+    const response = await axios.post("http://localhost:8000/user/login/", {
+      username,
+      password,
     });
-  return response;
+    const token = response.data.token;
+    localStorage.setItem("token", token);
+    console.log(token); // Save the token locally
+    axios.defaults.headers.common["Authorization"] = `Token ${token}`; // Set default header
+    console.log("Login successful");
+    return {
+      success: "User Logged in successfully",
+      code: response.status,
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const logout = async () => {
