7039aa822cdc18105830c8221e65266789d799e0
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-12 13:03:18 +0300

    Updated serializer

--- server/employee/serializer.py
+++ server/employee/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Employee
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)  
     class Meta:
         model = Employee
         fields = ('id','first_name', 'last_name', 'gender', 'email', 'phone_number','date_of_birth','date_of_hire', 'position' )
