b272ef19633c32fadded0a8d8b46f20eb502dec0
eziraa <ezratgab@gmail.com>
2024-06-12 21:46:29 +0300

    removing app does not exist

--- server/payroll/settings.py
+++ server/payroll/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'employee',
-    'users',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
