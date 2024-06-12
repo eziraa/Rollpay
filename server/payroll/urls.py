0109c19d2527a794f00bad9fc1f2c05c2466cd0a
eziraa <ezratgab@gmail.com>
2024-06-12 21:46:51 +0300

    updating user login endpoint

--- server/payroll/urls.py
+++ server/payroll/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("user/login/", TokenObtainPairView.as_view(), name="get_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path('employee/', include('employee.urls')),
