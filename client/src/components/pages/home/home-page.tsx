ad3fd74bafebcce8e6eb6e0eed38a0ac344f34e8
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-12 10:18:42 +0300

    Import error resolved

--- client/src/components/pages/home/home-page.tsx
+++ client/src/components/pages/home/home-page.tsx
@@ -4,7 +4,7 @@ import { useAppSelector } from "../../../utils/custom-hook";
 import { ADD_EMP } from "../../../utils/constants/tasks";
 import LeftMenu from "../../sections/left-menu/left-menu";
 import { AddEmployee } from "../../sections/add_employee/add-employee";
-import Main from "../../sections/Main/main";
+import Main from "../../sections/main/main";
 
 export const HomePage = () => {
   const employee = useAppSelector((state) => state.employee);
