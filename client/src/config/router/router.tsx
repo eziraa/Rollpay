80b6b1c0766f9450c13cc341fe1e99654c757f8f
eziraa <ezratgab@gmail.com>
2024-06-12 22:50:40 +0300

    Adding 404 page to routing

--- client/src/config/router/router.tsx
+++ client/src/config/router/router.tsx
@@ -4,6 +4,7 @@ import SignUp from "../../components/pages/sign-up/sign-up";
 import { HomePage } from "../../components/pages/home/home-page";
 import { LoginPage } from "../../components/pages/login/login";
 import ProtectedRoute from "../utils/protected_route";
+import NotFoundPage from "../../components/pages/4_0_4/404";
 export const RouterConfig = () => (
   <Router>
     <Routes>
@@ -17,6 +18,7 @@ export const RouterConfig = () => (
         }
       />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </Router>
 );
