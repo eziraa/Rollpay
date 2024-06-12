b34d4490c43c58fa02d869cf42644e779930e521
eziraa <ezratgab@gmail.com>
2024-06-12 22:40:14 +0300

    wrappign the homepage with protected route to prevent unauthorised access

--- client/src/config/router/router.tsx
+++ client/src/config/router/router.tsx
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "../../components/pages/sign-up/sign-up";
-import UpdateEmployee from "../../components/sections/update-employee/update-employee";
 
 import { HomePage } from "../../components/pages/home/home-page";
 import { LoginPage } from "../../components/pages/login/login";
+import ProtectedRoute from "../utils/protected_route";
 export const RouterConfig = () => (
   <Router>
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/home-page" element={<HomePage />} />
+      <Route
+        path="/home-page"
+        element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/edit-profile" element={<UpdateEmployee />} />
     </Routes>
   </Router>
 );
