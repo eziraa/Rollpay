5aaf8468bf191a495c162af591aee66fa91b2d23
eziraa <ezratgab@gmail.com>
2024-06-13 10:56:49 +0300

    making here to load employee list

--- client/src/components/pages/home/home-page.tsx
+++ client/src/components/pages/home/home-page.tsx
@@ -1,13 +1,19 @@
 import { Header } from "../../sections/header/header";
 import { HomeBody, HomeContainer } from "./home-page.style";
-import { useAppSelector } from "../../../utils/custom-hook";
+import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
 import { ADD_EMP } from "../../../utils/constants/tasks";
 import LeftMenu from "../../sections/left-menu/left-menu";
 import { AddEmployee } from "../../sections/add_employee/add-employee";
 import Main from "../../sections/main/main";
+import { useEffect } from "react";
+import { listEmpRequested } from "../../../store/employee/employee-slice";
 
 export const HomePage = () => {
   const employee = useAppSelector((state) => state.employee);
+  const dispacher = useAppDispatch();
+  useEffect(() => {
+    dispacher(listEmpRequested());
+  }, [dispacher]);
   return (
     <HomeContainer>
       <Header />
