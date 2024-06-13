b6785894a37c1b9e76c3166857cf595c0e1eb6ce
eziraa <ezratgab@gmail.com>
2024-06-13 16:19:26 +0300

    Adding more task constants

--- client/src/constants/tasks.tsx
+++ client/src/constants/tasks.tsx
@@ -1,3 +1,6 @@
 export const ADD_EMP = "add_employee";
 export const EDIT_EMP = "edit_employee";
 export const LIST_EMP_S = "list_employees";
+export const ADD_ALLOWANCE = "add_allowance";
+export const ADD_DEDUCTION = "add_deduction";
+export const ADD_OVERTIME = "add_overtime";
