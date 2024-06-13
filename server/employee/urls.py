cffc12fb0be972c90e5bb253b03335d2bbeb7400
eziraa <ezratgab@gmail.com>
2024-06-13 21:57:55 +0300

    creating a url to add salary

--- server/employee/urls.py
+++ server/employee/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-
+from . import salary
 
 # Defining  url patterns to use it as end point
 urlpatterns = [
     path('list', views.EmployeeView.as_view(), name="__list_emplyees__"),
     path('add', views.EmployeeView.as_view(), name='__add_emoployee__'),
+    path('salary/add/<employee_id>', salary.AddSalaryToEmployeeAPIView.as_view(),
+         name='__add_salary__'),
     path('get/<str:id>', views.EmployeeView.as_view(), name='__get_emoploye__'),
     path('update/<str:id>/', views.EmployeeView.as_view(),
          name='__update_emoployee__'),
