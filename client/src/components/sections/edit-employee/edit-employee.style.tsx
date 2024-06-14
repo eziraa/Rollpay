0a9f9401cbe65d53b5b469aad6856f4bcd5a931a
eziraa <ezratgab@gmail.com>
2024-06-14 19:44:59 +0300

    clode refactoring

--- client/src/components/sections/edit-employee/edit-employee.style.tsx
+++ client/src/components/sections/edit-employee/edit-employee.style.tsx
@@ -1,111 +1,30 @@
 import styled from "styled-components";
-import Profile from "../../../assets/profile.png";
 import { addOpacityToColor } from "../../utils/convertor/add-opacity-color";
-export const EditEmployeeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  padding: 3rem;
-  position: relative;
-`;
-
-export const EditEmployeeContent = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: start;
-  width: 100%;
-  height: 80%;
-  gap: 10rem;
-`;
-
-export const Title = styled.h1`
-  font-size: 2rem;
-  font-weight: 500;
-  margin-bottom: 1rem;
-  color: ${({ theme }) => theme.colors.primary};
-`;
-
-export const EmployeeeProfileContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  width: 22%;
-  border: 1px solid ${({ theme }) => theme.colors.secondary};
-  border-radius: 10px;
-  height: 90%;
-  padding: 1rem;
-  gap: 2rem;
-`;
-
-export const ProfileImage = styled.img`
-  width: 10rem;
-  height: 10rem;
-  border-radius: 50%;
-  padding: 0rem;
-  background: url(${Profile}), linear-gradient(90deg, #60565596, #6866619d);
-  background-blend-mode: multiply;
-  background-size: cover;
-  background-position: center;
-  border: 2px solid ${({ theme }) => theme.colors.primary};
-  cursor: pointer;
-`;
-
-export const EmployeeInfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: start;
-  width: 100%;
-  height: 60%;
-  padding: 1rem;
-  gap: 1rem;
-`;
 
 export const EditEmployeeBody = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 60%;
+  width: 100%;
   height: 100%;
   padding: 1rem;
   gap: 1rem;
+  position: relative;
 `;
-export const EmployeeData = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: start;
+export const Field = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
   gap: 1rem;
   width: 100%;
 `;
 
-export const DataLabel = styled.h5`
+export const Label = styled.h5`
   font-size: 1.4rem;
   font-weight: 500;
   color: ${({ theme }) => addOpacityToColor(0.7, theme.colors.primary)};
-  flex: 1;
   text-align: left;
-`;
-
-export const DataValue = styled.h5`
-  font-size: 1.4rem;
-  font-weight: 500;
-  color: ${({ theme }) => addOpacityToColor(0.5, theme.colors.primary)};
-  flex: 1;
-`;
-
-export const EditButton = styled.div`
-  font-size: 2rem;
-  color: ${({ theme }) => theme.colors.primary};
-  cursor: pointer;
-  width: 5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  padding: 1rem 2rem;
 `;
 
 export const SaveButton = styled.button`
@@ -116,6 +35,11 @@ export const SaveButton = styled.button`
   border: 1px solid ${({ theme }) => theme.colors.primary};
   background-color: ${({ theme }) => theme.colors.primary};
   border-radius: 5px;
+  &:hover {
+    background-color: ${({ theme }) =>
+      addOpacityToColor(0.75, theme.colors.primary)};
+    color: ${({ theme }) => theme.backgrounds.primary};
+  }
 `;
 
 export const CancelButton = styled.button`
@@ -156,4 +80,12 @@ export const ActionBtn = styled.div`
       addOpacityToColor(0.75, theme.colors.primary)};
     color: ${({ theme }) => theme.backgrounds.primary};
   }
+`;
+
+export const SaveBtn = styled(SaveButton)`
+  width: 10rem;
+  bottom: 1rem;
+  position: absolute;
+  left: 53.5%;
+  bottom: -4rem;
 `;
