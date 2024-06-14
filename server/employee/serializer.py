0eb36993693de50a4975f657319a8637ae8c828d
eziraa <ezratgab@gmail.com>
2024-06-14 09:33:25 +0300

    updating serilazer to include calculated salary as one field of serilazed employee

--- server/employee/serializer.py
+++ server/employee/serializer.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 from .models import Employee
 
+
 class EmployeeSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(read_only=True)  
+    id = serializers.CharField(read_only=True)
+    # Change salary to a SerializerMethodField
+    salary = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Employee
-        fields = ('id','first_name', 'last_name', 'gender', 'email', 'phone_number','date_of_birth','date_of_hire', 'position' )
+        fields = ('id', 'first_name', 'last_name', 'gender', 'email',
+                  'phone_number', 'date_of_birth', 'date_of_hire', 'position', 'salary')
+
+    def get_salary(self, obj: Employee):
+        base_salary = int(obj.salary.basic_salary) if obj.salary else 0
+        bonus = 5000  # Assuming a fixed bonus
+        calculated_salary = base_salary + bonus
+        return calculated_salary
 
 class ProfilePicSerializer(serializers.ModelSerializer):
     class Meta:
