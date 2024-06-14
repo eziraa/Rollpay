c2910511af41ff9ed5568da6b477419e106511c5
eziraa <ezratgab@gmail.com>
2024-06-14 09:37:49 +0300

    fix login and sign up permition

--- server/employee/user_views.py
+++ server/employee/user_views.py
@@ -12,9 +12,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 
 class UserView(APIView):
+    permission_classes = [AllowAny]
     def get(self, request, *args, **kwargs):
         logout(request)
         return Response("Logged out", status=status.HTTP_200_OK)
@@ -45,6 +47,7 @@ class UserView(APIView):
 
 
 class AccountView(APIView):
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         try:
