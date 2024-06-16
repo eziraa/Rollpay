082bdb55d420ed2f3a8da174deb980dc9546c7a5
eziraa <ezratgab@gmail.com>
2024-06-16 20:19:30 +0300

    updating flash message middleware to hide the flash message according to their duration

--- client/src/store/notification/flash-message-saga.tsx
+++ client/src/store/notification/flash-message-saga.tsx
@@ -1,14 +1,14 @@
 import { takeEvery, put, delay } from "redux-saga/effects";
 import { PayloadAction } from "@reduxjs/toolkit";
 import {
-  FlashMessageState,
+  FlashMessageItem,
   hideFlashMessage,
   setFlashMessage,
 } from "./flash-messsage-slice";
 
-function* handleFlashMessage(action: PayloadAction<FlashMessageState>) {
+function* handleFlashMessage(action: PayloadAction<FlashMessageItem>) {
   yield delay(action.payload.duration * 1000);
-  yield put(hideFlashMessage());
+  yield put(hideFlashMessage(action.payload));
 }
 
 export function* watchSetFlashMessage() {
