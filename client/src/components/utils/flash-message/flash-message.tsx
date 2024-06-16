487a7e0d0220df14a107f58c7bb748cf79f6b4c5
eziraa <ezratgab@gmail.com>
2024-06-16 20:17:43 +0300

    updating flash message to queue list of flash messge and hide the flash message according to their duration

--- client/src/components/utils/flash-message/flash-message.tsx
+++ client/src/components/utils/flash-message/flash-message.tsx
@@ -1,55 +1,64 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import {
   CloseIcon,
   FlashMessageBody,
   FlashMessageIcon,
   FlashMessageText,
   FlashMessageTitle,
-  FlasheMessageContainr,
+  FlashMessageItem,
+  FlasheMessageContainer,
 } from "./flash-message.style";
-import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
 import { hideFlashMessage } from "../../../store/notification/flash-messsage-slice";
 import { FaCheckCircle } from "react-icons/fa";
 import { IoIosWarning } from "react-icons/io";
+import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
 import { addOpacityToColor } from "../convertor/add-opacity-color";
-
+import { FlashMessageItem as FlashMsg } from "../../../store/notification/flash-messsage-slice";
 export const FlashMessage = () => {
+  const { flashMessages } = useAppSelector((state) => state.flashMessage);
   const dispatcher = useAppDispatch();
-  const flashMessage = useAppSelector((state) => state.flashMessage);
-  if (!flashMessage.status) return;
+  if (flashMessages.length < 1) return;
   return (
-    <FlasheMessageContainr
-      style={{
-        borderColor: flashMessage.type === "success" ? "#04b97f" : "#f9ba46",
-        backgroundColor:
-          flashMessage.type === "success"
-            ? addOpacityToColor(0.1, "#04b97f")
-            : addOpacityToColor(0.2, "#a55e25"),
-      }}
-    >
-      <FlashMessageIcon>
-        {flashMessage.type === "success" ? (
-          <FaCheckCircle
-            style={{
-              color: "#04b97f",
+    <FlasheMessageContainer>
+      {flashMessages.map((flashMessage: FlashMsg, index: number) => (
+        <FlashMessageItem
+          key={index}
+          style={{
+            borderColor:
+              flashMessage.type === "success" ? "#04b97f" : "#f9ba46",
+            backgroundColor:
+              flashMessage.type === "success"
+                ? addOpacityToColor(0.1, "#04b97f")
+                : addOpacityToColor(0.2, "#a55e25"),
+          }}
+        >
+          <FlashMessageIcon>
+            {flashMessage.type === "success" ? (
+              <FaCheckCircle
+                style={{
+                  color: "#04b97f",
+                }}
+              />
+            ) : (
+              <IoIosWarning
+                style={{
+                  color: "#f9ba46",
+                }}
+              />
+            )}
+          </FlashMessageIcon>
+          <FlashMessageBody>
+            <FlashMessageTitle>{flashMessage.title}</FlashMessageTitle>
+            <FlashMessageText>{flashMessage.desc}</FlashMessageText>
+          </FlashMessageBody>
+          <CloseIcon
+            onClick={(e) => {
+              e.stopPropagation();
+              dispatcher(hideFlashMessage(flashMessage));
             }}
           />
-        ) : (
-          <IoIosWarning
-            style={{
-              color: "#f9ba46",
-            }}
-          />
-        )}
-      </FlashMessageIcon>
-      <FlashMessageBody>
-        <FlashMessageTitle>{flashMessage.title}</FlashMessageTitle>
-        <FlashMessageText> {flashMessage.desc} </FlashMessageText>
-      </FlashMessageBody>
-      <CloseIcon
-        onClick={() => {
-          dispatcher(hideFlashMessage());
-        }}
-      />
-    </FlasheMessageContainr>
+        </FlashMessageItem>
+      ))}
+    </FlasheMessageContainer>
   );
 };
