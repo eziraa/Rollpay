a4e45d56d174ca6740cfdcec912e4ee0ed4e19e6
eziraa <ezratgab@gmail.com>
2024-06-16 16:42:34 +0300

    silcensing waring

--- client/src/components/sections/list-displayer/list-displayer.tsx
+++ client/src/components/sections/list-displayer/list-displayer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
 import { useEffect, useState } from "react";
 import {
