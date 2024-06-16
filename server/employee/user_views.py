71143abf7d8564bd88fa49f85b3efffc15afec71
eziraa <ezratgab@gmail.com>
2024-06-17 01:31:44 +0300

    updating user register view to add to group when user registed

--- server/employee/user_views.py
+++ server/employee/user_views.py
@@ -1,7 +1,8 @@
 
 from django.http import JsonResponse
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import User, Permission, Group
 from django.contrib.auth import authenticate, logout, login
+from django.shortcuts import get_object_or_404
 from django.contrib.contenttypes.models import ContentType
 import json
 from employee.models import Employee
@@ -48,17 +49,30 @@ class UserView(APIView):
 
 class AccountView(APIView):
     permission_classes = [AllowAny]
-
     def post(self, request, *args, **kwargs):
         try:
             data = json.loads(request.body)
-            if User.objects.filter(username=data['username']).exists():
-                return JsonResponse({'error': 'Username already exists'}, status=400)
-            user = User.objects.create_user(
-                username=data['username'], password=data['password'],
-            )
-            content_type = ContentType.objects.get_for_model(Employee)
-            user.save()
-            return JsonResponse({'message': 'User registered successfully'}, status=201)
+            if Employee.objects.filter(id=data['empID']).exists():
+                if User.objects.filter(username=data['username']).exists():
+                    return JsonResponse({'error': 'Username already exists'}, status=400)
+                user = User.objects.create_user(
+                    username=data['username'], password=data['password'],
+                )
+                group, created = Group.objects.get_or_create(name="employee")
+                group, created = Group.objects.get_or_create(
+                    name='add_emplyee')
+                employee_permissions = Permission.objects.filter(
+                    codename__in=['add_user', 'change_user, view_user'])
+
+                # Assign permissions to the group
+                permissions = Permission.objects.filter(
+                    codename__in=['add_employee', 'change_employee'])
+                group.permissions.set(permissions)
+                group.save()
+                user.groups.add(group)
+                user.save()
+                return JsonResponse({'message': 'User registered successfully'}, status=201)
+            else:
+                return JsonResponse({'error': 'Employee does not exist \n Check Your ID'}, status=400)
         except KeyError as e:
             return JsonResponse({'error': f'Missing field: {str(e)}'}, status=400)
