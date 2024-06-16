1e72e09c9952dc2fcd83f22a498653546eb2125a
eziraa <ezratgab@gmail.com>
2024-06-16 22:35:39 +0300

    code refactoring  by using reusable components

--- client/src/components/sections/allowance/allowance.style.tsx
+++ client/src/components/sections/allowance/allowance.style.tsx
@@ -1,61 +1,28 @@
 import styled from "styled-components";
-import { addOpacityToColor } from "../../utils/convertor/add-opacity-color";
-import { custom_scroll_bar } from "../../utils/scroll-bar/scroll-bar";
+import {
+  body_css,
+  btn_css,
+  container_css,
+  header_css,
+  title_css,
+} from "../../utils/containers/containers.style";
 
 export const AllowanceContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  box-shadow: 0rem 0rem 1rem ${({ theme }) => theme.colors.secondary};
-  border-radius: 0.5rem;
-  height: 100%;
-  width: 100%;
-  gap: 2rem;
+  ${container_css}
 `;
 
 export const AllowanceHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0.5rem 1rem;
-  gap: 1rem;
-  color: ${({ theme }) => theme.colors.primary};
-  letter-spacing: 0.1rem;
+  ${header_css}
 `;
 
 export const AllowanceTitle = styled.h1`
-  font-size: 2rem;
-  font-weight: 500;
-  color: ${({ theme }) => addOpacityToColor(0.6, theme.colors.primary)};
-  letter-spacing: 0.1rem;
+  ${title_css}
 `;
 
 export const AddButton = styled.button`
-  font-size: 2rem;
-  color: ${({ theme }) => theme.backgrounds.primary};
-  cursor: pointer;
-  padding: 1rem 2rem;
-  border-radius: 0.5rem;
-  background-color: ${({ theme }) => theme.colors.primary};
-  border: none;
-  &:hover {
-    background-color: ${({ theme }) =>
-      addOpacityToColor(0.75, theme.colors.primary)};
-    color: ${({ theme }) => theme.backgrounds.primary};
-  }
+  ${btn_css}
 `;
 
 export const AllowanceBody = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 3rem;
-  width: 100%;
-  color: ${({ theme }) => theme.colors.primary};
-  letter-spacing: 0.1rem;
-  flex-direction: column;
-  padding: 0 1rem;
-  background-color: ${({ theme }) => theme.backgrounds.primary};
-  ${custom_scroll_bar};
+  ${body_css}
 `;
