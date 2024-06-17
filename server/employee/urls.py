84f838789493ff29665b4deb957722fc7048d1d0
eziraa <ezratgab@gmail.com>
2024-06-17 04:10:15 +0300

    defining end point to delete employee

--- server/employee/urls.py
+++ server/employee/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('add', views.EmployeeView.as_view(), name='__add_emoployee__'),
     path('edit/<employee_id>', views.EmployeeView.as_view(),
          name='__edit_emoployee__'),
+    path('delete/<employee_id>', views.EmployeeView.as_view(),
+         name='__delete_emoployee__'),
     path('salary/add/<employee_id>', salary.AddSalaryToEmployeeAPIView.as_view(),
          name='__add_salary__'),
     path('get/<str:id>', views.EmployeeView.as_view(), name='__get_emoploye__'),
