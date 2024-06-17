027e85a403e885a9e1b9b138e4508becc27e5be1
eziraa <ezratgab@gmail.com>
2024-06-17 03:36:57 +0300

    Adding delete button to employee page

--- client/src/components/sections/see-employee/see-employee.tsx
+++ client/src/components/sections/see-employee/see-employee.tsx
@@ -16,6 +16,8 @@ import {
   Title,
   SeeEmployeeHeader,
   TitleContainer,
+  ActionBtnsContainer,
+  DeleteButton,
 } from "./see-employee.style";
 import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
 import { IoChevronBackCircleOutline } from "react-icons/io5";
@@ -36,6 +38,7 @@ import {
 import { EditEmployee } from "../edit-employee/edit-employee";
 import { EmployeeOvertime } from "../overtime/overtime";
 import { EmployeeDeduction } from "../deduction/deduction";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 export const SeeEmployee = () => {
   const { curr_emp: current_employee } = useAppSelector(
@@ -127,17 +130,34 @@ export const SeeEmployee = () => {
               <DataValue>{current_employee?.date_of_hire}</DataValue>
             </EmployeeData>
           </EmployeeInfoContainer>
-          {major_task !== EDIT_EMP && (
+          <ActionBtnsContainer
+            style={{
+              gap: "2rem",
+            }}
+          >
+            <DeleteButton
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                // dispatcher(setMajorTask(EDIT_EMP));
+              }}
+            >
+              <RiDeleteBin6Line /> Delete
+            </DeleteButton>
             <Button
               onClick={(e) => {
                 e.preventDefault();
                 e.stopPropagation();
                 dispatcher(setMajorTask(EDIT_EMP));
               }}
+              disabled={major_task === EDIT_EMP}
+              style={{
+                cursor: major_task === EDIT_EMP ? "not-allowed" : "pointer",
+              }}
             >
               <MdModeEditOutline /> Edit
             </Button>
-          )}
+          </ActionBtnsContainer>
         </EmployeeeProfileContainer>
         {major_task == EDIT_EMP && <EditEmployee />}
         {major_task == SEE_EMP_ALLOWANCE && <EmployeeAllowance />}
