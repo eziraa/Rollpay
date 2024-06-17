6f582e3b0ad850db4f9a4bc57a992c24764c741c
eziraa <ezratgab@gmail.com>
2024-06-17 16:56:41 +0300

    styling salary components

---
+++ client/src/components/sections/salary/salary.style.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { custom_scroll_bar } from "../../utils/scroll-bar/scroll-bar";
+import { addOpacityToColor } from "../../utils/convertor/add-opacity-color";
+
+export const SalaryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  height: 90vh;
+  ${custom_scroll_bar}
+`;
+
+export const SalaryTitle = styled.h1`
+  font-size: 2.5rem;
+  font-weight: bold;
+  letter-spacing: 1px;
+  color: ${({ theme }) => addOpacityToColor(0.65, theme.colors.primary)};
+`;
