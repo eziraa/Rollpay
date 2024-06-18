ee284469b7c8d0897af571c6e2fcf57d8a681939
eziraa <ezratgab@gmail.com>
2024-06-18 10:09:36 +0300

    adding left menu navigation

--- client/src/components/sections/left-menu/left-menu.tsx
+++ client/src/components/sections/left-menu/left-menu.tsx
@@ -1,3 +1,6 @@
+import { LIST_EMP_S, SEE_EMP_SALARY } from "../../../constants/tasks";
+import { setLongTask } from "../../../store/user/user-slice";
+import { useAppDispatch } from "../../../utils/custom-hook";
 import {
   SalaryIcon,
   HomeIcon,
@@ -8,17 +11,30 @@ import {
 } from "./left-menu.style";
 
 function LeftMenu() {
+  const dispatcher = useAppDispatch();
   return (
     <LeftMenuContainer>
       <MenuItem>
         <HomeIcon />
         <MenuItemText>Home</MenuItemText>
       </MenuItem>
-      <MenuItem>
+      <MenuItem
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          dispatcher(setLongTask(LIST_EMP_S));
+        }}
+      >
         <UsersIcon />
         <MenuItemText>All Employees</MenuItemText>
       </MenuItem>
-      <MenuItem>
+      <MenuItem
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          dispatcher(setLongTask(SEE_EMP_SALARY));
+        }}
+      >
         <SalaryIcon />
         <MenuItemText>Salary</MenuItemText>
       </MenuItem>
