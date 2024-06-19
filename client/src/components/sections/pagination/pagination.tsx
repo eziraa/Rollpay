79e76f574c96151dc95313674c3eca45be234108
eziraa <ezratgab@gmail.com>
2024-06-19 23:23:53 +0300

    handiling previous page event

--- client/src/components/sections/pagination/pagination.tsx
+++ client/src/components/sections/pagination/pagination.tsx
@@ -13,8 +13,12 @@ import {
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import DropDown from "../../utils/drop-down/drop-down";
-import { loadNextPageRequested } from "../../../store/employee/employee-slice";
+import {
+  loadNextPageRequested,
+  loadPrevPageRequested,
+} from "../../../store/employee/employee-slice";
 import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
+import { setFlashMessage } from "../../../store/notification/flash-messsage-slice";
 function Pagination() {
   const dispatcher = useAppDispatch();
   const { pagination } = useAppSelector((state) => state.employee);
@@ -22,13 +26,37 @@ function Pagination() {
   const loadNextPage = async () => {
     if (pagination?.next) {
       dispatcher(loadNextPageRequested(pagination?.next));
-    }
+    } else
+      dispatcher(
+        setFlashMessage({
+          desc: "No more pages to load",
+          type: "error",
+          duration: 3,
+          status: true,
+          title: "Loading next page...",
+        })
+      );
   };
+  const loadPrevPage = async () => {
+    if (pagination?.previous) {
+      dispatcher(loadPrevPageRequested(pagination?.previous));
+    } else
+      dispatcher(
+        setFlashMessage({
+          desc: "No more pages to load",
+          type: "error",
+          duration: 3,
+          status: true,
+          title: "Loading previous page...",
+        })
+      );
+  };
+
   return (
     <PaginationContainer>
       <BottomContainer>
         <NavButton>
-          <ButtonName>
+          <ButtonName onClick={loadPrevPage}>
             <MdOutlineKeyboardArrowLeft />
             <ButtonText>Prev</ButtonText>
           </ButtonName>
