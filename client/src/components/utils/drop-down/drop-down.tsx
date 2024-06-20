9c373d6a04a190b27c2002eaa5737f97981a1c42
eziraa <ezratgab@gmail.com>
2024-06-20 23:17:04 +0300

    updating the drop down to be able to select and load employees

--- client/src/components/utils/drop-down/drop-down.tsx
+++ client/src/components/utils/drop-down/drop-down.tsx
@@ -1,31 +1,37 @@
-import { setPagesize } from "../../../store/employee/employee-slice";
-import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
+import { useContext } from "react";
 import { DropDownContainer, DropDownItem } from "./drop-down.style";
+import { PaginationContext } from "../../../contexts/pagination-context";
+import { useAppDispatch } from "../../../utils/custom-hook";
+import { loadNextPageRequested } from "../../../store/employee/employee-slice";
 
 function DropDown() {
+  const { pagination, setPageSize } = useContext(PaginationContext);
   const dispatcher = useAppDispatch();
-  const { pagination } = useAppSelector((state) => state.employee);
-
   return (
     <DropDownContainer
       name="page"
       onChange={(e) => {
-        dispatcher(setPagesize(Number.parseInt(e.currentTarget.value)));
+        setPageSize(parseInt(e.currentTarget.value));
+        dispatcher(
+          loadNextPageRequested(
+            "/employee/list" + `?page=${1}&page_size=${e.currentTarget.value}`
+          )
+        );
       }}
     >
-      <DropDownItem value="10" selected={pagination?.page_size === 10}>
+      <DropDownItem value="10" selected={pagination?.per_page === 10}>
         10
       </DropDownItem>
-      <DropDownItem value="20" selected={pagination?.page_size === 20}>
+      <DropDownItem value="20" selected={pagination?.per_page === 20}>
         20
       </DropDownItem>
-      <DropDownItem value="30" selected={pagination?.page_size === 30}>
+      <DropDownItem value="30" selected={pagination?.per_page === 30}>
         30
       </DropDownItem>
-      <DropDownItem value="40" selected={pagination?.page_size === 40}>
+      <DropDownItem value="40" selected={pagination?.per_page === 40}>
         40
       </DropDownItem>
-      <DropDownItem value="50" selected={pagination?.page_size === 50}>
+      <DropDownItem value="50" selected={pagination?.per_page === 50}>
         50
       </DropDownItem>
     </DropDownContainer>
