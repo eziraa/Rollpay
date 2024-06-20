826f56f5f55c6afdd4667bc7cbb5d4c3d7ae34ae
eziraa <ezratgab@gmail.com>
2024-06-20 23:14:55 +0300

    code cleaning

--- client/src/components/sections/pagination/pagination.tsx
+++ client/src/components/sections/pagination/pagination.tsx
@@ -18,9 +18,10 @@ import {
   loadNextPageRequested,
   loadPrevPageRequested,
 } from "../../../store/employee/employee-slice";
-import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
+import { useAppDispatch } from "../../../utils/custom-hook";
 import { setFlashMessage } from "../../../store/notification/flash-messsage-slice";
-import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { PaginationContext } from "../../../contexts/pagination-context";
 
 export interface PageInfo {
   currentPage: number;
@@ -30,25 +31,11 @@ export interface PageInfo {
 }
 function Pagination() {
   const dispatcher = useAppDispatch();
-  const { pagination } = useAppSelector((state) => state.employee);
+  const { pagination } = useContext(PaginationContext);
 
   const loadNextPage = async () => {
     if (pagination?.next) {
-      if (pagination.next.includes("page_size=")) {
-        const url = pagination.next.substring(
-          0,
-          pagination.next.indexOf("&page_size=")
-        );
-        if (url.length > 0)
-          dispatcher(
-            loadPrevPageRequested(url + `&page_size=${pagination.page_size}`)
-          );
-      } else
-        dispatcher(
-          loadNextPageRequested(
-            pagination?.next + `&page_size=${pagination.page_size}`
-          )
-        );
+      dispatcher(loadNextPageRequested(pagination.next));
     } else
       dispatcher(
         setFlashMessage({
@@ -61,22 +48,8 @@ function Pagination() {
       );
   };
   const loadPrevPage = async () => {
-    if (pagination?.previous) {
-      if (pagination.previous.includes("page_size=")) {
-        const url = pagination.previous.substring(
-          0,
-          pagination.previous.indexOf("&page_size=")
-        );
-        if (url.length > 0)
-          dispatcher(
-            loadPrevPageRequested(url + `&page_size=${pagination.page_size}`)
-          );
-      }
-      dispatcher(
-        loadPrevPageRequested(
-          pagination.previous + `&page_size=${pagination.page_size}`
-        )
-      );
+    if (pagination?.prev) {
+      dispatcher(loadPrevPageRequested(pagination.prev));
     } else
       dispatcher(
         setFlashMessage({
@@ -88,52 +61,6 @@ function Pagination() {
         })
       );
   };
-  const [page, setPage] = useState<PageInfo>({
-    currentPage: 0,
-    totalPages: 0,
-    totalRecords: 0,
-    pageSize: 0,
-  });
-  const getPageInfo = (prev: string) => {
-    if (prev.includes("page=")) {
-      prev = prev.substring(prev.indexOf("page=") + 5, prev.indexOf("&"));
-      console.log(prev);
-      setPage({
-        currentPage: Number.parseFloat(prev) + 1,
-        totalPages:
-          Math.floor(pagination ? pagination.count / pagination.page_size : 0) +
-          1,
-        totalRecords: pagination?.count ?? 0,
-        pageSize: 10,
-      });
-    } else if (pagination?.next?.includes("page=")) {
-      prev = pagination?.next?.substring(
-        pagination?.next?.indexOf("page=") + 5,
-        pagination?.next?.indexOf("&")
-      );
-      setPage({
-        currentPage: Number.parseFloat(prev),
-        totalPages:
-          Math.floor(pagination ? pagination.count / pagination.page_size : 0) +
-          1,
-        totalRecords: pagination?.count ?? 0,
-        pageSize: 10,
-      });
-    } else {
-      setPage({
-        currentPage: page.currentPage + 1,
-        totalPages:
-          Math.floor(pagination ? pagination.count / pagination.page_size : 0) +
-          1,
-        totalRecords: pagination?.count ?? 0,
-        pageSize: 10,
-      });
-    }
-  };
-
-  useEffect(() => {
-    getPageInfo(pagination?.previous ?? "");
-  }, [pagination]);
 
   return (
     <PaginationContainer>
@@ -161,10 +88,10 @@ function Pagination() {
           </ButtonName>
         </NavButton>
         <Paragraph>Page:</Paragraph>
-        <CurrentPageNumber type="number" value={pagination?.current_page} />
+        <CurrentPageNumber type="number" value={pagination.current_page} />
         <TextContainer>
           <Text>of</Text>
-          <Paragraph2> {pagination?.number_of_pages} </Paragraph2>
+          <Paragraph2> {pagination.total_pages} </Paragraph2>
         </TextContainer>
         <Paragraph>Per Page:</Paragraph>
         <DropDown />
