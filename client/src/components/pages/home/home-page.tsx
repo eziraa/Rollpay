c32d815a656aaab7f94ec5760024128bda398670
eziraa <ezratgab@gmail.com>
2024-06-20 23:14:29 +0300

    updating home page to set pagination based on the response

--- client/src/components/pages/home/home-page.tsx
+++ client/src/components/pages/home/home-page.tsx
@@ -5,7 +5,7 @@ import { ADD_EMP } from "../../../utils/constants/tasks";
 import LeftMenu from "../../sections/left-menu/left-menu";
 import { AddEmployee } from "../../sections/add_employee/add-employee";
 import Main from "../../sections/main/main";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { listEmpRequested } from "../../../store/employee/employee-slice";
 import {
   ADD_ALLOWANCE,
@@ -18,16 +18,21 @@ import { AddSalaryComponent } from "../../sections/salary-components/salary-comp
 import LoadingSpinner from "../../utils/spinner/spinner";
 import { CheckFlashMessage } from "../../sections/confirm-flash-message/confirm-flash-message";
 import { getSalariesRequested } from "../../../store/salary/salary-slice";
+import { PaginationContext } from "../../../contexts/pagination-context";
 export const HomePage = () => {
   const employee = useAppSelector((state) => state.employee);
   const { long_task, short_task } = useAppSelector((state) => state.user);
   const salary = useAppSelector((state) => state.salary);
   const dispacher = useAppDispatch();
+  const { setPagination } = useContext(PaginationContext);
   useEffect(() => {
     if (long_task === SEE_EMP_SALARY) dispacher(getSalariesRequested());
     else if (long_task === undefined || long_task === LIST_EMP_S)
       dispacher(listEmpRequested());
   }, [dispacher, long_task]);
+  useEffect(() => {
+    employee.pagination && setPagination(employee.pagination);
+  }, [employee.pagination]);
   return (
     <HomeContainer>
       <Header />
