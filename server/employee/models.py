1a6c71c6c43adb171375cb69c1f77e21752dc5dc
eziraa <ezratgab@gmail.com>
2024-06-20 14:09:20 +0300

    removing unnecessary field of salary model

--- server/employee/models.py
+++ server/employee/models.py
@@ -43,12 +43,6 @@ class Salary(models.Model):
         Overtime,  blank=True)
     deductions = models.ManyToManyField(
         Deduction, blank=True)
-    net_salary = models.DecimalField(
-        max_digits=7, decimal_places=2, blank=True, null=True)
-    gross_salary = models.DecimalField(
-        max_digits=7, decimal_places=2, blank=True, null=True)
-    total_deduction = models.DecimalField(
-        max_digits=7, decimal_places=2, blank=True, null=True)
 
 
 class Position(models.Model):
