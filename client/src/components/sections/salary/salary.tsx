3f3289aec145b176bc831a5250a0dc9bb7f291c8
eziraa <ezratgab@gmail.com>
2024-06-21 20:57:09 +0300

    updating search method to make it server side

--- client/src/components/sections/salary/salary.tsx
+++ client/src/components/sections/salary/salary.tsx
@@ -16,25 +16,33 @@ import {
 import { SearchIcon } from "../../utils/search/search.style";
 import { Header, Title } from "../display-employee/display-employee.style";
 import { Select, SelectOption } from "../../utils/form-elements/form.style";
-import { setLongTask } from "../../../store/user/user-slice";
-import { SEARCH_EMPLOYEE_SALARY } from "../../../constants/tasks";
-import {
-  noSearchResult,
-  startSearchPaymentEmployee,
-} from "../../../store/salary/salary-slice";
+import { searchEmployeeRequested } from "../../../store/salary/salary-slice";
 import { Employee } from "../../../typo/salary/response";
 import Pagination from "../pagination/pagination";
+import LoadingSpinner from "../../utils/spinner/spinner";
 
 export const Salary = () => {
   const dispatcher = useAppDispatch();
   const salary = useAppSelector((state) => state.salary);
-  const { long_task } = useAppSelector((state) => state.user);
   const [searchBy, setSearchBy] = useState("first_name");
 
   const [allowanceTypes, setAllowanceTypes] = useState<string[]>([]);
   const [deductionTypes, setDeductionTypes] = useState<string[]>([]);
   const { response } = useAppSelector((state) => state.salary);
+  const [search_val, setSearchVal] = useState<string>("");
 
+  useEffect(() => {
+    const loadEmployee = setTimeout(() => {
+      dispatcher(
+        searchEmployeeRequested({
+          search_by: searchBy,
+          search_value: search_val,
+        })
+      );
+    }, 500);
+
+    return () => clearTimeout(loadEmployee);
+  }, [search_val]);
   useEffect(() => {
     const tempAllowanceTypes = new Set<string>();
     const tempDeductionTypes = new Set<string>();
@@ -80,11 +88,9 @@ export const Salary = () => {
   const [employeeSalary, setEmployeeSalary] = useState<Employee[]>([]);
 
   useEffect(() => {
-    if (long_task === SEARCH_EMPLOYEE_SALARY) {
+    if (salary.searching && salary.search_response)
       setEmployeeSalary(salary.search_response || []);
-    } else {
-      setEmployeeSalary(salary.response?.employees || []);
-    }
+    else setEmployeeSalary(salary.response?.employees || []);
   }, [salary.response, salary.search_response]);
 
   return (
@@ -95,23 +101,8 @@ export const Salary = () => {
           <SearchIcon />
           <SearchInput
             onChange={(e) => {
-              dispatcher(setLongTask(SEARCH_EMPLOYEE_SALARY));
-              const result = startSearchPaymentEmployee(
-                salary.response?.employees.filter((employee) =>
-                  Object.entries(employee).find(([key, value]) => {
-                    return (
-                      key === searchBy &&
-                      value
-                        .toString()
-                        .toLowerCase()
-                        .startsWith(e.target.value.toString().toLowerCase())
-                    );
-                  })
-                ) || []
-              );
-              if (result) {
-                dispatcher(startSearchPaymentEmployee(result.payload));
-              } else dispatcher(noSearchResult());
+              // dispatcher(setLongTask(SEARCH_EMPLOYEE_SALARY));
+              setSearchVal(e.currentTarget.value);
             }}
           />
         </SearchContainer>
@@ -128,81 +119,86 @@ export const Salary = () => {
           <SelectOption value="id">ID</SelectOption>
         </Select>
       </Header>
-      <SalaryTable>
-        <TableHeader>
-          <HeaderTitle rowSpan={2}>Employee ID</HeaderTitle>
-          <HeaderTitle rowSpan={2}>Employee Name</HeaderTitle>
-          <HeaderTitle rowSpan={2}>Basic Salary</HeaderTitle>
-          <HeaderTitle
-            style={{
-              textAlign: "center",
-            }}
-            colSpan={allowanceTypes.length}
-          >
-            Allowance
-          </HeaderTitle>
-          <HeaderTitle rowSpan={2}>Gross Sallary</HeaderTitle>
-          <HeaderTitle
-            style={{
-              textAlign: "center",
-            }}
-            colSpan={deductionTypes.length + 1}
-          >
-            Deduction
-          </HeaderTitle>
-          <HeaderTitle rowSpan={2}>Total Deduction</HeaderTitle>
-          <HeaderTitle rowSpan={2}>Net Pay</HeaderTitle>
-          <HeaderTitle rowSpan={2}>Payment</HeaderTitle>
-        </TableHeader>
-        <TableHeader>
-          {allowanceTypes.map((allowanceType) => {
-            return <HeaderTitle> {allowanceType} </HeaderTitle>;
-          })}
-          <HeaderTitle>Income Tax</HeaderTitle>
-          {deductionTypes.map((deductionType) => {
-            return <HeaderTitle> {deductionType} </HeaderTitle>;
-          })}
-        </TableHeader>
-        {employeeSalary
-          .filter((employee) => employee.salary)
-          .map((employee) => (
-            <TableRow key={employee.id}>
-              <TableData>{employee.id}</TableData>
-              <TableData>
-                {employee.first_name + " " + employee.last_name}
-              </TableData>
-              <TableData>{employee.salary.basic_salary}</TableData>
-              {allowanceTypes.map((allowanceType) => {
-                return (
-                  <TableData>
-                    {getRate(
-                      employee.salary.allowances.find(
-                        (alowance) => alowance.allowance_type === allowanceType
-                      )?.allowance_rate
-                    )}
-                  </TableData>
-                );
-              })}
-              <TableData>{getSalary(employee.salary.gross_salary)}</TableData>
-              <TableData>{getSalary(employee.salary.income_tax)}</TableData>
-              {deductionTypes.map((deductionType) => {
-                return (
-                  <TableData>
-                    {getRate(
-                      employee.salary.deductions.find(
-                        (deduction) =>
-                          deduction.deduction_type === deductionType
-                      )?.deduction_rate
-                    )}
-                  </TableData>
-                );
-              })}
-              <TableData>{employee.salary.total_deduction}</TableData>
-              <TableData>{employee.salary.net_salary}</TableData>
-              <TableData> Not Paid </TableData>
-            </TableRow>
-          ))}
-      </SalaryTable>
+      {salary.loading ? (
+        <LoadingSpinner />
+      ) : (
+        <SalaryTable>
+          <TableHeader>
+            <HeaderTitle rowSpan={2}>Employee ID</HeaderTitle>
+            <HeaderTitle rowSpan={2}>Employee Name</HeaderTitle>
+            <HeaderTitle rowSpan={2}>Basic Salary</HeaderTitle>
+            <HeaderTitle
+              style={{
+                textAlign: "center",
+              }}
+              colSpan={allowanceTypes.length}
+            >
+              Allowance
+            </HeaderTitle>
+            <HeaderTitle rowSpan={2}>Gross Sallary</HeaderTitle>
+            <HeaderTitle
+              style={{
+                textAlign: "center",
+              }}
+              colSpan={deductionTypes.length + 1}
+            >
+              Deduction
+            </HeaderTitle>
+            <HeaderTitle rowSpan={2}>Total Deduction</HeaderTitle>
+            <HeaderTitle rowSpan={2}>Net Pay</HeaderTitle>
+            <HeaderTitle rowSpan={2}>Payment</HeaderTitle>
+          </TableHeader>
+          <TableHeader>
+            {allowanceTypes.map((allowanceType) => {
+              return <HeaderTitle> {allowanceType} </HeaderTitle>;
+            })}
+            <HeaderTitle>Income Tax</HeaderTitle>
+            {deductionTypes.map((deductionType) => {
+              return <HeaderTitle> {deductionType} </HeaderTitle>;
+            })}
+          </TableHeader>
+          {employeeSalary
+            .filter((employee) => employee.salary)
+            .map((employee) => (
+              <TableRow key={employee.id}>
+                <TableData>{employee.id}</TableData>
+                <TableData>
+                  {employee.first_name + " " + employee.last_name}
+                </TableData>
+                <TableData>{employee.salary.basic_salary}</TableData>
+                {allowanceTypes.map((allowanceType) => {
+                  return (
+                    <TableData>
+                      {getRate(
+                        employee.salary.allowances.find(
+                          (alowance) =>
+                            alowance.allowance_type === allowanceType
+                        )?.allowance_rate
+                      )}
+                    </TableData>
+                  );
+                })}
+                <TableData>{getSalary(employee.salary.gross_salary)}</TableData>
+                <TableData>{getSalary(employee.salary.income_tax)}</TableData>
+                {deductionTypes.map((deductionType) => {
+                  return (
+                    <TableData>
+                      {getRate(
+                        employee.salary.deductions.find(
+                          (deduction) =>
+                            deduction.deduction_type === deductionType
+                        )?.deduction_rate
+                      )}
+                    </TableData>
+                  );
+                })}
+                <TableData>{employee.salary.total_deduction}</TableData>
+                <TableData>{employee.salary.net_salary}</TableData>
+                <TableData> Not Paid </TableData>
+              </TableRow>
+            ))}
+        </SalaryTable>
+      )}
       <Pagination />
     </SalaryContainer>
   );
