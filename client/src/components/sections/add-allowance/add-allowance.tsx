421b3ff28ac723e1a59b5a39b648745e102e531c
eziraa <ezratgab@gmail.com>
2024-06-23 17:17:32 +0300

    creating components of add allowance to add allowance to employee

---
+++ client/src/components/sections/add-allowance/add-allowance.tsx
@@ -0,0 +1,35 @@
+import {
+  Input,
+  InputContainer,
+  Label,
+} from "../../utils/form-elements/form.style";
+import { Modal } from "../../utils/modal/modal";
+import { Title } from "../add_employee/add-employee.style";
+import {
+  AddBtn,
+  AllowanceBody,
+  AllowanceContainer,
+  AllowanceForm,
+} from "./add-allowance.style";
+export const AddAllowance = () => {
+  return (
+    <Modal>
+      <AllowanceContainer>
+        <AllowanceBody>
+          <Title>Add Allowance</Title>
+          <AllowanceForm>
+            <InputContainer>
+              <Label>Allowance Name</Label>
+              <Input type="text" />
+            </InputContainer>
+            <InputContainer>
+              <Label>Allowance rate</Label>
+              <Input type="text" />
+            </InputContainer>
+            <AddBtn>Add</AddBtn>
+          </AllowanceForm>
+        </AllowanceBody>
+      </AllowanceContainer>
+    </Modal>
+  );
+};
