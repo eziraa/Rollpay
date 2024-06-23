c6035e40505853af5633c56048d98bf19133ff9d
eziraa <ezratgab@gmail.com>
2024-06-23 20:35:24 +0300

    displaying  modal store and mode cleaniing

--- client/src/components/pages/home/home-page.tsx
+++ client/src/components/pages/home/home-page.tsx
@@ -2,16 +2,15 @@ import { Header } from "../../sections/header/header";
 import { HomeBody, HomeContainer } from "./home-page.style";
 import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
 import LeftMenu from "../../sections/left-menu/left-menu";
-import { AddEmployee } from "../../sections/add_employee/add-employee";
 import Main from "../../sections/main/main";
 import { useContext, useEffect } from "react";
 import { listEmpRequested } from "../../../store/employee/employee-slice";
-import { AddSalaryComponent } from "../../sections/salary-components/salary-components";
 import LoadingSpinner from "../../utils/spinner/spinner";
 import { CheckFlashMessage } from "../../sections/confirm-flash-message/confirm-flash-message";
 import { getSalariesRequested } from "../../../store/salary/salary-slice";
 import { PaginationContext } from "../../../contexts/pagination-context";
 import { DisplayContext } from "../../../contexts/display-context";
+import { ModalStore } from "../../utils/modal-store/modal-store";
 export const HomePage = () => {
   const employee = useAppSelector((state) => state.employee);
   const dispacher = useAppDispatch();
@@ -33,10 +32,7 @@ export const HomePage = () => {
         <CheckFlashMessage />
         {employee.loading ? <LoadingSpinner /> : <Main />}
       </HomeBody>
-      {display.add_employee && <AddEmployee />}
-      {(display.add_allowance ||
-        display.add_deduction ||
-        display.add_overtime) && <AddSalaryComponent />}
+      <ModalStore />
     </HomeContainer>
   );
 };
