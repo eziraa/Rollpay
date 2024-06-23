a9811e6418f1fb6e14786b7a8b82603d7459cf4b
eziraa <ezratgab@gmail.com>
2024-06-23 17:16:12 +0300

    fix color issue to the user profile page

--- client/src/components/pages/user-profile/user-profile.style.tsx
+++ client/src/components/pages/user-profile/user-profile.style.tsx
@@ -5,6 +5,7 @@ export const UserProfileContainer = styled.div`
   display: flex;
   height: 90vh;
   width: 100vw;
+  background-color: ${({ theme }) => theme.backgrounds.primary};
 `;
 export const Item = styled(ItemContainer)`
   align-items: baseline;
