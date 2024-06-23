3ef507e51f7ed427cb832e95ec38268b65c19b63
eziraa <ezratgab@gmail.com>
2024-06-23 20:32:10 +0300

    updating the modal to close the modal

--- client/src/components/utils/modal/modal.tsx
+++ client/src/components/utils/modal/modal.tsx
@@ -1,33 +1,21 @@
+import { CLOSE_MODAL } from "../../../constants/tasks";
+import { useModal } from "../../../hooks/modal-hook";
 import { CloseIcon } from "../buttons/close";
 import { ModalContainer, ModalContent } from "./modal.style";
-import { useContext } from "react";
-import { DisplayContext } from "../../../contexts/display-context";
 
 export const Modal = ({ children }: { children: React.ReactNode }) => {
-  const { display, setDisplay } = useContext(DisplayContext);
+  const { openModal } = useModal();
   return (
     <ModalContainer
       onClick={(e) => {
         e.stopPropagation();
-        setDisplay({
-          ...display,
-          add_employee: false,
-          add_allowance: false,
-          add_overtime: false,
-          add_deduction: false,
-        });
+        openModal(CLOSE_MODAL);
       }}
     >
       <ModalContent onClick={(e) => e.stopPropagation()}>
         <CloseIcon
           onClick={() => {
-            setDisplay({
-              ...display,
-              add_employee: false,
-              add_allowance: false,
-              add_overtime: false,
-              add_deduction: false,
-            });
+            openModal(CLOSE_MODAL);
           }}
         />
         {children}
