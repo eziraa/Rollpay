6655f8f2fa2bf3b62cdf17db7de2b2e36b25f036
eziraa <ezratgab@gmail.com>
2024-06-23 17:12:53 +0300

    creating reusable styles for adding item components

---
+++ client/src/components/utils/add-item/add.styl.tsx
@@ -0,0 +1,39 @@
+import { css } from "styled-components";
+import { addOpacityToColor } from "../../utils/convertor/add-opacity-color";
+
+export const add_container = css`
+  display: flex;
+  flex-direction: column;
+  padding: 4rem;
+  box-shadow: 0 0 1.5rem ${({ theme }) => theme.colors.primary};
+  border-radius: 1rem;
+  height: auto;
+  width: 100%;
+  margin: 0;
+  gap: 3rem;
+`;
+
+export const add_body = css`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  position: relative;
+  padding: 1rem;
+  gap: 1rem;
+`;
+
+export const add_btn = css`
+  padding: 1rem 2rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  font-size: 1.3rem;
+  text-align: center;
+  background-color: ${({ theme }) => theme.buttons.primary};
+  color: #ffffff;
+  &:hover {
+    background-color: ${({ theme }) =>
+      addOpacityToColor(0.75, theme.buttons.primary)};
+    cursor: pointer;
+  }
+`;
