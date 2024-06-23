1b7e32e1065cb3632cde5dc4f4a9181e099de779
eziraa <ezratgab@gmail.com>
2024-06-23 17:18:28 +0300

    creating compoents to add overtime to employee

---
+++ client/src/components/sections/add-overtime/add-overtime.tsx
@@ -0,0 +1,35 @@
+import {
+  Input,
+  InputContainer,
+  Label,
+} from "../../utils/form-elements/form.style";
+import { Modal } from "../../utils/modal/modal";
+import { Title } from "../add_employee/add-employee.style";
+import {
+  AddBtn,
+  OvertimeBody,
+  OvertimeContainer,
+  OvertimeForm,
+} from "./add-overtime.style";
+export const AddOvertimes = () => {
+  return (
+    <Modal>
+      <OvertimeContainer>
+        <OvertimeBody>
+          <Title>Add Overtime</Title>
+          <OvertimeForm>
+            <InputContainer>
+              <Label>Overtime Name</Label>
+              <Input type="text" />
+            </InputContainer>
+            <InputContainer>
+              <Label>Overtime rate</Label>
+              <Input type="text" />
+            </InputContainer>
+            <AddBtn>Add</AddBtn>
+          </OvertimeForm>
+        </OvertimeBody>
+      </OvertimeContainer>
+    </Modal>
+  );
+};
