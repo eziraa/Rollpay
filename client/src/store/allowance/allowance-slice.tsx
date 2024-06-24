58f6e446395175312c298b4ca93480f0cbea685c
eziraa <ezratgab@gmail.com>
2024-06-24 19:33:09 +0300

    creating  allowance slice and reducers to add allowance

---
+++ client/src/store/allowance/allowance-slice.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-refresh/only-export-components */
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { AllowanceState } from "../../typo/allowance/states";
+import { Allowance } from "../../typo/allowance/response";
+import {
+  AddAllowanceParams,
+  EditAllowanceParams,
+} from "../../typo/allowance/params";
+import { PaginatedAllowanceResponse } from "../../services/allowance-api";
+
+const InitialEmpState: AllowanceState = {
+  adding: false,
+  allowances: [],
+  loading: false,
+  curr_allowance: undefined,
+  editing: false,
+  deleting: false,
+  query_set: [],
+  searching: false,
+  pagination: undefined,
+  adding_allowance_error: undefined,
+};
+const AllowanceSlice = createSlice({
+  name: "allowance",
+  initialState: InitialEmpState,
+  reducers: {
+    addAllowanceRequested: (state, _: PayloadAction<AddAllowanceParams>) => {
+      state.adding = true;
+    },
+    addAllowanceDone: (state, action: PayloadAction<Allowance>) => {
+      state.adding = false;
+      state.adding_allowance_error = undefined;
+      state.allowances.push(action.payload);
+      state.curr_allowance = action.payload;
+    },
+   
