f948453334667a1d52150ed1d792a2763104b277
eziraa <ezratgab@gmail.com>
2024-06-24 22:16:37 +0300

    updating the position api view class to handle all request methods

--- server/employee/views/position.py
+++ server/employee/views/position.py
@@ -1,14 +1,66 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from employee.serializers.serializers import PositionSerializer
+from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.views import APIView
 from ..models import Position
+from employee.serializers.serializers import PositionSerializer
+from django.http import JsonResponse
+import json
+from utils.pagination import StandardResultsSetPagination
 
 
-class PosittionView(APIView):
-    def post(self, request, *args, **kwargs):
-        if Position.objects.filter(position_name=request.data['position_name']).exists():
-            return Response({'error': 'Position already exists'}, status=400)
-        position = Position.objects.create(**request.data)
-        position.save()
-        serializer = PositionSerializer(position)
-        return Response(serializer.data, status=201)
+class PositionView (APIView):
+
+    def get(self, request: Request, format=None):
+        try:
+            queryset = Position.objects.all().order_by("pk")
+            paginator = StandardResultsSetPagination()
+            paginator.page_size = request.query_params.get("page_size", 10)
+            page = paginator.paginate_queryset(queryset, request)
+            if page is not None:
+                serializer = PositionSerializer(page, many=True)
+                return paginator.get_paginated_response(serializer.data)
+            serializer = PositionSerializer(queryset, many=True)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            if Position.objects.filter(email=data['position_name']).exists():
+                return JsonResponse({'error': 'Position already exists'}, status=status.HTTP_400_BAD_REQUEST)
+            position = Position.objects.create(**data)
+            serializer = PositionSerializer(position, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except KeyError:
+            return JsonResponse({'error': 'Required field(s) missing in request data'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, position_id):
+        try:
+            position = Position.objects.get(pk=position_id)
+        except Position.DoesNotExist:
+            return Response({"error": "Position not found"}, status=status.HTTP_404_NOT_FOUND)
+        try:
+            serializer = PositionSerializer(position)
+            position.delete()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, position_id):
+        try:
+            position = Position.objects.get(pk=position_id)
+        except Position.DoesNotExist:
+            return Response({"error": "Position not found"}, status=status.HTTP_404_NOT_FOUND)
+        data = json.loads(request.body)
+        serializer = PositionSerializer(position, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
