b3cf92fedefabe61ddfd67d22279be471b50a9b6
eziraa <ezratgab@gmail.com>
2024-06-25 00:53:10 +0300

    updating modal provider to open and close modal properly

--- client/src/providers/modal-provider.tsx
+++ client/src/providers/modal-provider.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { ModalContext, ModalInterface } from "../contexts/modal-context";
-import { ADD_POSITION, CLOSE_MODAL } from "../constants/tasks";
 
 export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
   const [modal, setModal] = useState<ModalInterface>({
@@ -9,33 +8,27 @@ export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
     add_deduction: false,
     add_overtime: false,
     add_position: false,
+    add_employee_to_emp: false,
+    add_allowance_to_emp: false,
+    add_deduction_to_emp: false,
+    add_overtime_to_emp: false,
+    add_position_to_emp: false,
   });
 
   const openModal = (name: string) => {
-    Object.entries(modal).map(([key, isOpened]) => {
-      if (name === CLOSE_MODAL) {
-        if (isOpened) {
-          if (isOpened && key === ADD_POSITION)
-            setModal({ ...modal, [key]: false, add_employee: true });
-          else setModal({ ...modal, [key]: false });
-        }
-      } else if (key !== name) {
-        setModal({
-          ...modal,
-          [key]: false,
-        });
-      }
+    setModal({
+      ...modal,
+      [name]: true,
     });
-
-    name !== CLOSE_MODAL &&
-      setModal({
-        ...modal,
-        [name]: true,
-      });
   };
-
+  const closeModal = (name: string) => {
+    setModal({
+      ...modal,
+      [name]: false,
+    });
+  };
   return (
-    <ModalContext.Provider value={{ modal, openModal }}>
+    <ModalContext.Provider value={{ modal, openModal, closeModal }}>
       {children}
     </ModalContext.Provider>
   );
