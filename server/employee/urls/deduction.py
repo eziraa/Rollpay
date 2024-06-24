8e0428e3b820ec45646eebf9bd9e5e323e8b8ef5
eziraa <ezratgab@gmail.com>
2024-06-24 22:11:35 +0300

    creating endpoints for deduction

---
+++ server/employee/urls/deduction.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from employee.views import deduction
+
+urlpatterns = [
+    path('list', deduction.DeductionView.as_view(), name="__list_deductions__"),
+    path('add', deduction.DeductionView.as_view(), name='__add_deduction__'),
+    path('edit/<deduction_id>', deduction.DeductionView.as_view(),
+         name='__edit_deduction__'),
+    path('delete/<deduction_id>', deduction.DeductionView.as_view(),
+         name='__delete_deduction__'),
+    path('get/<str:id>', deduction.DeductionView.as_view(),
+         name='__get_deduction__'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
