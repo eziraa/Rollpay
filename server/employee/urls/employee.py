39c991750a889680f48e72a971ac6adea53fd3c7
NetsanetAlemu <netsialemu1907@gmail.com>
2024-06-26 13:15:24 +0300

    Added route to add allowance

--- server/employee/urls/employee.py
+++ server/employee/urls/employee.py
@@ -7,6 +7,7 @@ from employee.views import views, salary_view
 urlpatterns = [
     path('list', views.EmployeeView.as_view(), name="__list_emplyees__"),
     path('add', views.EmployeeView.as_view(), name='__add_emoployee__'),
+    path('allowance/add/<employee_id>/<allowance_type>', views.EmployeeView.as_view(), name='__add_allowance_to_employee__'),
     path('edit/<employee_id>', views.EmployeeView.as_view(),
          name='__edit_emoployee__'),
     path('delete/<employee_id>', views.EmployeeView.as_view(),
