cbbbec2f57b26a3d7f029bc055de9897f2a03c69
eziraa <ezratgab@gmail.com>
2024-06-27 16:00:35 +0300

    creating a custom filter context to filter employees

---
+++ client/src/contexts/filter-context.tsx
@@ -0,0 +1,47 @@
+import { createContext } from "react";
+
+export interface SalaryRange {
+  min: number;
+  max: number;
+}
+
+export interface DateRange {
+  from: string;
+  to: string;
+}
+
+export interface EmployeeFilter {
+  filter_by: string;
+  order_by: string;
+  order: string;
+  position: string;
+  salary_range: SalaryRange | undefined;
+  date_of_hire_range: DateRange | undefined;
+  date_of_birth_range: DateRange | undefined;
+}
+
+export interface FilterContextType {
+  filter: EmployeeFilter;
+  setFilter: (filter: EmployeeFilter) => void;
+}
+export const FilterContext = createContext<FilterContextType>({
+  filter: {
+    filter_by: "",
+    order_by: "",
+    order: "",
+    position: "",
+    salary_range: {
+      min: 0,
+      max: 0,
+    },
+    date_of_hire_range: {
+      from: "",
+      to: "",
+    },
+    date_of_birth_range: {
+      from: "",
+      to: "",
+    },
+  },
+  setFilter: () => {},
+});
