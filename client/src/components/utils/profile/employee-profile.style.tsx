f7522eb87a74a8853ae02b888aced469fbce9404
eziraa <ezratgab@gmail.com>
2024-06-27 10:33:23 +0300

    creating a reusable employee profile section to for reusablity

---
+++ client/src/components/utils/profile/employee-profile.style.tsx
@@ -0,0 +1,165 @@
+import styled from "styled-components";
+import { addOpacityToColor } from "../convertor/add-opacity-color";
+import Profile from "../../../assets/profile.png";
+import { ThemeProps } from "../../../typo/theme/theme";
+
+export const EmployeeeProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  min-width: 30rem;
+  border: 1px solid ${({ theme }) => theme.colors.secondary};
+  border-radius: 10px;
+  height: 100%;
+  padding: 1rem;
+  gap: 2rem;
+`;
+
+export const ProfileImage = styled.img`
+  width: 10rem;
+  height: 10rem;
+  border-radius: 50%;
+  padding: 0rem;
+  background: url(${Profile}), linear-gradient(90deg, #60565596, #6866619d);
+  background-blend-mode: multiply;
+  background-size: cover;
+  background-position: center;
+  border: 2px solid ${({ theme }) => theme.colors.primary};
+  cursor: pointer;
+`;
+
+export const EmployeeInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  width: 100%;
+  height: 60%;
+  padding: 1rem;
+  gap: 1.5rem;
+`;
+
+export const EmployeeData = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 0.5rem;
+  width: 100%;
+`;
+
+export const DataLabel = styled.h5`
+  font-size: 1.4rem;
+  font-weight: 500;
+  color: ${({ theme }) => addOpacityToColor(0.7, theme.colors.primary)};
+  flex: 1;
+  text-align: left;
+`;
+
+export const DataValue = styled.h5`
+  font-size: 1.4rem;
+  font-weight: 500;
+  color: ${({ theme }) => addOpacityToColor(0.5, theme.colors.primary)};
+  flex: 1.5;
+`;
+
+export const EditButton = styled.div`
+  font-size: 2rem;
+  color: ${({ theme }) => theme.colors.primary};
+  cursor: pointer;
+  width: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Button = styled.button`
+  font-size: 1.2rem;
+  color: #ffffff;
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+  border: none;
+  background-color: ${({ theme }) => theme.buttons.primary};
+  border-radius: 5px;
+  width: 10rem;
+  display: flex;
+  gap: 1rem;
+  font-size: medium;
+  &:hover {
+    background-color: ${({ theme }) =>
+      addOpacityToColor(0.75, theme.buttons.primary)};
+  }
+`;
+
+export const BackButton = styled.div`
+  font-size: 3rem;
+  display: inline-block;
+  color: ${({ theme }) => theme.buttons.primary};
+  cursor: pointer;
+  border-radius: 5px;
+  padding-bottom: -0.4rem;
+  text-align: center;
+  &:hover {
+    color: ${({ theme }) => addOpacityToColor(0.75, theme.buttons.primary)};
+  }
+`;
+
+export const ActionBtnsContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
+  width: 100%;
+  padding: 2rem 0;
+`;
+
+export const ActionBtn = styled.div`
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 1.6rem;
+  padding: 1rem 2rem;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.backgrounds.primary};
+  &:hover {
+    background-color: ${({ theme }) =>
+      addOpacityToColor(0.75, theme.colors.primary)};
+    color: ${({ theme }) => theme.backgrounds.primary};
+  }
+`;
+
+export const NavBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: start;
+  flex: 3.8;
+  gap: 0rem;
+  background-color: ${({ theme }) => theme.backgrounds.primary};
+`;
+
+interface NavItemProps extends ThemeProps {
+  active?: boolean;
+}
+
+export const NavItem = styled.div<NavItemProps>`
+  padding: 1rem 2rem;
+  display: inline-block;
+  font-size: 1.7rem;
+  letter-spacing: 1px;
+  cursor: pointer;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.primary};
+  border-bottom: 0.5rem solid;
+  border-bottom-color: ${({ active, theme }) =>
+    active ? theme.buttons.primary : "transparent"};
+`;
+
+export const DeleteButton = styled(Button)`
+  background-color: ${({ theme }) =>
+    addOpacityToColor(0.6, theme.buttons.primary)};
+  border: none;
+`;
---
+++ client/src/components/utils/profile/employee-profile.tsx
@@ -0,0 +1,92 @@
+import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
+import {
+  ActionBtnsContainer,
+  Button,
+  DataLabel,
+  DataValue,
+  DeleteButton,
+  EmployeeData,
+  EmployeeInfoContainer,
+  EmployeeeProfileContainer,
+  ProfileImage,
+} from "./employee-profile.style";
+import { MdModeEditOutline } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { tryingToDelete } from "../../../store/employee/employee-slice";
+import { getCurrEmpPaymentInfo } from "../../../store/salary/salary-slice";
+import { useEffect } from "react";
+
+export const EmployeeProfile = () => {
+  const { curr_emp } = useAppSelector((state) => state.salary);
+  const dispatcher = useAppDispatch();
+  useEffect(() => {
+    const curr_emp_id = localStorage.getItem("curr_emp_id");
+    curr_emp_id && dispatcher(getCurrEmpPaymentInfo(curr_emp_id));
+  }, [curr_emp]);
+  return (
+    <EmployeeeProfileContainer>
+      <ProfileImage />
+      <EmployeeInfoContainer>
+        <EmployeeData>
+          <DataLabel>Full Name</DataLabel>
+          <DataValue>
+            {curr_emp?.employee?.first_name +
+              " " +
+              curr_emp?.employee?.last_name}
+          </DataValue>
+        </EmployeeData>
+        <EmployeeData>
+          <DataLabel>Gender</DataLabel>
+          <DataValue>{curr_emp?.employee?.gender}</DataValue>
+        </EmployeeData>
+        <EmployeeData>
+          <DataLabel>Email</DataLabel>
+          <DataValue>{curr_emp?.employee?.email}</DataValue>
+        </EmployeeData>
+        <EmployeeData>
+          <DataLabel>Phone Number</DataLabel>
+          <DataValue>{curr_emp?.employee?.phone_number}</DataValue>
+        </EmployeeData>
+        <EmployeeData>
+          <DataLabel>Role</DataLabel>
+          <DataValue>{curr_emp?.employee?.position}</DataValue>
+        </EmployeeData>
+        <EmployeeData>
+          <DataLabel>Salary</DataLabel>
+          <DataValue>{curr_emp?.employee?.salary}</DataValue>
+        </EmployeeData>
+        <EmployeeData>
+          <DataLabel>Birth Date</DataLabel>
+          <DataValue>{curr_emp?.employee?.date_of_birth}</DataValue>
+        </EmployeeData>
+        <EmployeeData>
+          <DataLabel>Date of Hire</DataLabel>
+          <DataValue>{curr_emp?.employee?.date_of_hire}</DataValue>
+        </EmployeeData>
+      </EmployeeInfoContainer>
+      <ActionBtnsContainer
+        style={{
+          gap: "2rem",
+        }}
+      >
+        <DeleteButton
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            dispatcher(tryingToDelete());
+          }}
+        >
+          <RiDeleteBin6Line /> Delete
+        </DeleteButton>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }}
+        >
+          <MdModeEditOutline /> Edit
+        </Button>
+      </ActionBtnsContainer>
+    </EmployeeeProfileContainer>
+  );
+};
