eba286042f094277b4b1925e462cc70ecffaa4fa
eziraa <ezratgab@gmail.com>
2024-06-27 08:27:39 +0300

    fix import issue

--- client/src/components/utils/modal-store/modal-store.tsx
+++ client/src/components/utils/modal-store/modal-store.tsx
@@ -4,8 +4,8 @@ import { AddDeduction } from "../../sections/add-deduction/add-deduction";
 import { AddOvertime } from "../../sections/add-overtime/add-overtime";
 import { AddPosition } from "../../sections/add-position/add-position";
 import { AddEmployee } from "../../sections/add_employee/add-employee";
-import { AddAllowanceToEmp } from "../../sections/see-employee/add-allowance";
-import { AddDeductionToEmp } from "../../sections/see-employee/add-deduction";
+import { AddAllowanceToEmp } from "../../pages/see-employee/add-allowance";
+import { AddDeductionToEmp } from "../../pages/see-employee/add-deduction";
 
 export const ModalStore = () => {
   const { modal } = useModal();
