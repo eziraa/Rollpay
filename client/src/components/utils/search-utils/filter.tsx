aeb905575db5a735d7fc7dcb41cb9adb23f5b260
eziraa <ezratgab@gmail.com>
2024-06-27 22:31:58 +0300

    separting the style and the component

--- client/src/components/utils/search-utils/filter.tsx
+++ client/src/components/utils/search-utils/filter.tsx
@@ -1,98 +1,18 @@
-import styled from "styled-components";
-import { Input, Select, SelectOption } from "../form-elements/form.style";
-import { addOpacityToColor } from "../convertor/add-opacity-color";
+import { SelectOption } from "../form-elements/form.style";
 import { useFilter } from "../../../hooks/filter-hook";
 import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
 import { setFlashMessage } from "../../../store/notification/flash-messsage-slice";
+import {
+  ButtonContainer,
+  ClearButton,
+  FilterButton,
+  FilterContainer,
+  FilterInput,
+  FilterLabel,
+  FilterRow,
+  FilterSelect,
+} from "./filter.style";
 
-export const FilterContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: fit-content;
-  width: 40vw;
-  gap: 1rem;
-  padding: 2rem;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 5rem;
-  border-radius: 0.5rem;
-  background-color: ${({ theme }) => theme.backgrounds.primary};
-  box-shadow: 0 0 0.5rem 0 ${({ theme }) => theme.colors.primary};
-  z-index: 100;
-`;
-
-export const FilterRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  gap: 1rem;
-`;
-
-export const FilterLabel = styled.h3`
-  font-size: 1.4rem;
-  color: ${({ theme }) => addOpacityToColor(0.7, theme.colors.primary)};
-  flex: 0.9;
-  &:not(:first-child) {
-    margin-left: 1rem;
-  }
-`;
-export const FilterSelect = styled(Select)`
-  padding: 1rem;
-  font-size: 1.2rem;
-  flex: 1;
-  max-width: 30%;
-`;
-
-export const FilterInput = styled(Input)`
-  width: fit-content;
-  padding: 1rem;
-  font-size: 1.2rem;
-  outline: none;
-  border-radius: 0;
-  border: none;
-  border-bottom: 1px solid
-    ${({ theme }) => addOpacityToColor(0.5, theme.colors.primary)};
-  &:focus {
-    border: none;
-  }
-`;
-
-export const FilterButton = styled.button`
-  padding: 1rem 2.5rem;
-  font-size: 1.2rem;
-  background-color: ${({ theme }) => theme.buttons.primary};
-  color: #ffffff;
-  border-radius: 0.5rem;
-  border: none;
-  &:hover {
-    background-color: ${({ theme }) =>
-      addOpacityToColor(0.75, theme.buttons.primary)};
-    color: #ffffff;
-  }
-  width: fit-content;
-  justify-self: end;
-  position: relative;
-`;
-
-export const ClearButton = styled(FilterButton)`
-  background-color: ${({ theme }) =>
-    addOpacityToColor(0.7, theme.buttons.primary)};
-`;
-
-export const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: end;
-  width: 100%;
-  gap: 1rem;
-  padding: 1rem;
-`;
 export const Filter = () => {
   const { filter, setFilter } = useFilter();
   const { positions } = useAppSelector((state) => state.position);
