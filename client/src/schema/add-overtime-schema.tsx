20b985c71d182f9b9145e79b1f319fc7d25fac6c
eziraa <ezratgab@gmail.com>
2024-06-28 10:52:41 +0300

    updating  overtime schemas

--- client/src/schema/add-overtime-schema.tsx
+++ client/src/schema/add-overtime-schema.tsx
@@ -1,12 +1,14 @@
 import * as Yup from "yup";
 
-export const AddOvertime = Yup.object({
+export const AddOvertimeSchema = Yup.object({
   overtime_type: Yup.string().required("Please enter overtime type"),
   overtime_rate: Yup.number().required("Please enter overtime rate"),
-  overtime_length: Yup.number().required(
-    "Please enter overtime length of timeline"
-  ),
-  overtime_date: Yup.date().required(
-    "Please enter datetime of overtime timeline"
-  ),
+});
+
+export const AddAllowanceToEmpSchema = Yup.object({
+  allowance_type: Yup.string().required("Please enter allowance type"),
+  allowance_rate: Yup.number().required("Please enter allowance rate"),
+  employee_id: Yup.number().required("Please enter employee ID"),
+  overtime: Yup.date().required("Please enter start date"),
+  end_date: Yup.date().required("Please enter end date"),
 });
