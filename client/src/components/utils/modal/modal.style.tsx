c64984d5173127d41515fd70fc755081fc1a565b
eziraa <ezratgab@gmail.com>
2024-06-29 17:17:45 +0300

    updating modal styles

--- client/src/components/utils/modal/modal.style.tsx
+++ client/src/components/utils/modal/modal.style.tsx
@@ -5,7 +5,7 @@ export const ModalContainer = styled.div<ThemeProps>`
   position: fixed; /* Changed from absolute to fixed */
   width: 100vw;
   height: 100vh;
-  background-color: #322c2ccd;
+  background-color: #322c2c9b;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -14,15 +14,19 @@ export const ModalContainer = styled.div<ThemeProps>`
   padding: 0;
   top: 0;
   left: 0;
+  top: 50%; /* Center vertically */
+  left: 50%; /* Center horizontally */
+  transform: translate(-50%, -50%);
 `;
 
 export const ModalContent = styled.div`
   width: fit-content;
   height: fit-content;
+  min-width: max-content;
   background-color: ${({ theme }) => theme.backgrounds.primary};
   display: flex;
   border-radius: 1rem;
-  position: absolute;
+  position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
