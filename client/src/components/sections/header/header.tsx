63615de63694aa9dab9e95756899f64697c9e253
eziraa <ezratgab@gmail.com>
2024-07-01 15:55:10 +0300

    code cleaning

--- client/src/components/sections/header/header.tsx
+++ client/src/components/sections/header/header.tsx
@@ -1,15 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Toggle } from "../../utils/buttons/toggle";
-// import { Button } from "../../utils/form_elements/form.style";
 import {
   HeaderContainer,
   ProfileContainer,
   ProfileImage,
   WelcomeMessageContainer,
 } from "./header.style";
-import { useEffect, useState } from "react";
-
-import Profile from "../profile/profile";
+import { useEffect } from "react";
 
 import { CURRENT_USER } from "../../../constants/token-constants";
 import { useAppDispatch } from "../../../utils/custom-hook";
@@ -18,21 +14,19 @@ import { getCurrentUserRequest } from "../../../store/user/user-slice";
 import { useUser } from "../../../hooks/user-hook";
 import { BlurredText, MidBlurredText } from "../../utils/titles/titles";
 import { stringDay } from "../../utils/day/string-day";
+import { Notification } from "./notification";
+import { DropDown } from "./profile-drop-down";
 
 export const Header = () => {
   const curr_user = localStorage.getItem(CURRENT_USER);
   const employee_id = JSON.parse(curr_user || "[]")?.id;
   const { user } = useUser();
   const dispatcher = useAppDispatch();
-  const [openProfileMenu, setProfileMenu] = useState<boolean>();
 
   useEffect(() => {
     employee_id && dispatcher(getCurrentUserRequest(employee_id));
   }, []);
 
-  const closeAction = () => {
-    setProfileMenu(false);
-  };
   return (
     <>
       <HeaderContainer>
@@ -43,14 +37,9 @@ export const Header = () => {
           </MidBlurredText>
         </WelcomeMessageContainer>
         <ProfileContainer>
-          <Toggle />
-          {openProfileMenu && <Profile close={closeAction} />}
-          <ProfileImage
-            profile={baseURL + user?.employee.profile_picture}
-            onClick={() => {
-              setProfileMenu(!openProfileMenu);
-            }}
-          />
+          <Notification />
+          <ProfileImage profile={baseURL + user?.employee.profile_picture} />
+          <DropDown />
         </ProfileContainer>
       </HeaderContainer>
     </>
