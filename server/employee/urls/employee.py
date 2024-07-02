63bd7c2612156c53afa50d51c59a14362f8c726e
eziraa <ezratgab@gmail.com>
2024-07-02 13:50:38 +0300

    adding end point to filter employee

--- server/employee/urls/employee.py
+++ server/employee/urls/employee.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('add', views.EmployeeView.as_view(), name='__add_emoployee__'),
     path('filter', filter_employee.FilterEmployeeView.as_view(),
          name='__filter_emoployee__'),
+    path('filter/<filter_by>', filter_employee.FilterEmployeeView.as_view(),
+         name='__filter_emoployee__'),
     path('allowance/add/<employee_id>/<allowance_type>', views.EmployeeView.as_view(), name='__add_allowance_to_employee__'),
     path('edit/<employee_id>', views.EmployeeView.as_view(),
          name='__edit_emoployee__'),
