98a50de5f5de18762e9dc88c817446c2f161be62
eziraa <ezratgab@gmail.com>
2024-07-02 11:08:49 +0300

    creating endpoint to able to filter employee

--- server/employee/urls/employee.py
+++ server/employee/urls/employee.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from employee.views import views, salary_view
+from employee.views import views, salary_view, filter_employee
 from employee.views.user_views import ProfilePicture
 
 # Defining  url patterns to use it as end point
 urlpatterns = [
     path('list', views.EmployeeView.as_view(), name="__list_emplyees__"),
     path('add', views.EmployeeView.as_view(), name='__add_emoployee__'),
+    path('filter', filter_employee.FilterEmployeeView.as_view(),
+         name='__filter_emoployee__'),
     path('allowance/add/<employee_id>/<allowance_type>', views.EmployeeView.as_view(), name='__add_allowance_to_employee__'),
     path('edit/<employee_id>', views.EmployeeView.as_view(),
          name='__edit_emoployee__'),
