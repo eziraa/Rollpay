657fef6087a547ee0b4ce64835bbb60144303cf0
eziraa <ezratgab@gmail.com>
2024-07-05 07:15:54 +0300

    updating get method of PositionView

--- server/employee/views/position.py
+++ server/employee/views/position.py
@@ -12,8 +12,14 @@ import datetime
 
 class PositionView (APIView):
 
-    def get(self, request: Request, format=None):
+    def get(self, request: Request, position_id=None, format=None):
         try:
+
+            if position_id:
+                position = Position.objects.get(id=position_id)
+                serializer = PositionSerializer(position)
+                return Response(serializer.data, status=200)
+
             queryset = Position.objects.all().order_by("pk")
             paginator = StandardResultsSetPagination()
             paginator.page_size = request.query_params.get("page_size", 10)
