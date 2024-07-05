317c2127c8c7d27c294dfa4766e53c19366a2694
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-05 13:56:09 +0300

    url to get number of employees

--- server/employee/urls/employee.py
+++ server/employee/urls/employee.py
@@ -34,4 +34,8 @@ urlpatterns = [
          name='__update_emoployee__'),
     path("contract/<str:employee_id>",
          EmployementContract.as_view(), name='_employement_contract__'),
+     path('employee-number', views.EmployeeNumber.as_view(), name='__get_employee_number__'),
+     
+
+
 ] 
