d48a135e590030fadb5e593b7fd95715d9625d8f
eziraa <ezratgab@gmail.com>
2024-07-05 07:12:27 +0300

    update add position to able to use this for efit position also

--- client/src/components/sections/add-position/add-position.tsx
+++ client/src/components/sections/add-position/add-position.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useFormik } from "formik";
 import {
   FormError,
@@ -17,30 +19,55 @@ import { useAppDispatch } from "../../../utils/custom-hook";
 import {
   addPositionRequested,
   closePositionTask,
+  getPositionRequest,
   resetPositionState,
 } from "../../../store/position/position-slice";
 import { AddPositionSchema } from "../../../schema/add-position-schema";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { usePosition } from "../../../hooks/position-hook";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 export const AddPosition = () => {
   // Calling hooks and getting necessary information
   const dispatcher = useAppDispatch();
   const { task_error, task_finished } = usePosition();
   const position = usePosition();
   const navigate = useNavigate();
+  const { position_id } = useParams();
+
+  const [initial, _] = useState({
+    position_name: position.curr_position?.position_name as string,
+    basic_salary: (position.curr_position?.basic_salary || "") as string,
+  });
+
+  //Getting current position
+
+  useEffect(() => {
+    position_id && dispatcher(getPositionRequest(position_id));
+  }, [position_id]);
+
   // Creating formik instance
-  const { touched, errors, values, handleChange, handleSubmit, isSubmitting } =
-    useFormik({
-      initialValues: {
-        position_name: "",
-        basic_salary: "",
-      },
-      validationSchema: AddPositionSchema,
-      onSubmit: (values) => {
-        dispatcher(addPositionRequested(values));
-      },
-    });
+  const {
+    touched,
+    errors,
+    values,
+    handleChange,
+    handleSubmit,
+    isSubmitting,
+    initialValues,
+  } = useFormik({
+    initialValues: initial,
+    validationSchema: AddPositionSchema,
+    onSubmit: (values) => {
+      dispatcher(
+        resetPositionState({
+          ...position,
+          task_error: undefined,
+          task_finished: false,
+        })
+      );
+      dispatcher(addPositionRequested(values));
+    },
+  });
 
   //Defining hook to close the modal
   useEffect(() => {
@@ -76,7 +103,7 @@ export const AddPosition = () => {
                 type="text"
                 onChange={handleChange}
                 name="position_name"
-                value={values.position_name}
+                value={initial.position_name || values.position_name}
               />
               {touched.position_name && errors.position_name && (
                 <FormError> {errors.position_name} </FormError>
@@ -86,7 +113,7 @@ export const AddPosition = () => {
               <Label>Base Salary</Label>
               <Input
                 type="number"
-                value={values.basic_salary}
+                value={initial.basic_salary || values.basic_salary}
                 name="basic_salary"
                 onChange={handleChange}
               />
@@ -100,7 +127,6 @@ export const AddPosition = () => {
                   fontSize: "1.5rem",
                 }}
               >
-                {" "}
                 {task_error}
               </FormError>
             )}
