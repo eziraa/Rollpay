f33a854b44e20a4817974669580dcbf17c585e59
eziraa <ezratgab@gmail.com>
2024-07-07 23:46:12 +0300

    updating the dashboard to display fetched data from the backend

--- client/src/components/pages/dashboard/dashboard.tsx
+++ client/src/components/pages/dashboard/dashboard.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useStatistics } from "../../../hooks/statistics-hook";
 import { MonthCard } from "../../utils/curr-month-card/curr-month-card";
 import { PaymentCard } from "../../utils/payment-card/payment-card";
 import { LargeText, NormalBlurredText } from "../../utils/titles/titles";
@@ -11,26 +13,34 @@ import {
   StatCard,
   StatContainer,
 } from "./dashboard.style";
-import { mockData } from "./stat";
+import { useAppDispatch } from "../../../utils/custom-hook";
+import { getStatRequest } from "../../../store/statistics/statistics-slice";
+import { FaUser } from "react-icons/fa";
 
 export const DashBoard = () => {
+  const { stat } = useStatistics();
+  const dispatcher = useAppDispatch();
+  useEffect(() => {
+    dispatcher(getStatRequest());
+    console.log(stat);
+  }, []);
   return (
     <DashboardContainer>
       <StatContainer className="state-card-container">
-        {mockData.map((data, index) => {
+        {Object.entries(stat).map(([key, value], index) => {
           return (
             <StatCard>
               <CardRowTemplate>
                 <CardColumnTemplate>
-                  <LargeText>{data.total}</LargeText>
-                  <p>{data.name}</p>
+                  <LargeText>{value}</LargeText>
+                  <p>{key.toLocaleUpperCase()}</p>
                 </CardColumnTemplate>
-                {<data.icon />}
+                {<FaUser />}
               </CardRowTemplate>
               <CardRowTemplate>
                 {index % 2 === 0 ? <IncreaseIcon /> : <DecreaseIcon />}
                 <CardColumnTemplate>
-                  <NormalBlurredText>{data.percentage}%</NormalBlurredText>
+                  <NormalBlurredText>{87}%</NormalBlurredText>
                 </CardColumnTemplate>
               </CardRowTemplate>
             </StatCard>
