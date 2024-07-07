09b61e5d79d75454d5e0e6454620cdc00ab7bd7a
eziraa <ezratgab@gmail.com>
2024-07-07 08:03:16 +0300

    adding method to close the left menu bar to close btn

--- client/src/components/sections/header/header.tsx
+++ client/src/components/sections/header/header.tsx
@@ -16,21 +16,33 @@ import { BlurredText, MidBlurredText } from "../../utils/titles/titles";
 import { stringDay } from "../../utils/day/string-day";
 import { Notification } from "./notification";
 import { DropDown } from "./profile-drop-down";
+import { HamburgerMenu } from "../../pages/home/home-page.style";
+import { HiMenu } from "react-icons/hi";
+import { useRefs } from "../../../hooks/refs-hook";
 
 export const Header = () => {
   const curr_user = localStorage.getItem(CURRENT_USER);
   const employee_id = JSON.parse(curr_user || "[]")?.id;
   const { user } = useUser();
   const dispatcher = useAppDispatch();
-
+  const leftMenuRef = useRefs().refs?.leftMenuRef;
   useEffect(() => {
     employee_id && dispatcher(getCurrentUserRequest(employee_id));
   }, []);
 
-
   return (
     <>
       <HeaderContainer>
+        <HamburgerMenu
+          className="hamburger-menu"
+          onClick={() => {
+            leftMenuRef &&
+              leftMenuRef.current &&
+              leftMenuRef.current.classList.toggle("collapsed");
+          }}
+        >
+          <HiMenu />
+        </HamburgerMenu>
         <WelcomeMessageContainer>
           <BlurredText>Welcome, Mr. {user?.employee.first_name}</BlurredText>
           <MidBlurredText>
