ae40437a51e8679a3ce3136f2b8aa19e18e41ee7
eziraa <ezratgab@gmail.com>
2024-07-08 11:02:52 +0300

    fix type initialization issue

--- client/src/store/statistics/statistics-slice.tsx
+++ client/src/store/statistics/statistics-slice.tsx
@@ -5,11 +5,11 @@ const initialState: StatisticsState = {
   task_error: undefined,
   task_finished: true,
   stat: {
-    total_employees: 39,
-    curr_month_tax: 360233.0,
-    curr_month_allowances: 36380.0,
-    curr_month_deductions: 23460.0,
-    curr_month_payment_amount: 812687.0,
+    total_employees: "39",
+    curr_month_tax: "360233.0",
+    curr_month_allowances: "36380.0",
+    curr_month_deductions: "23460.0",
+    curr_month_payment_amount: "812687.0",
   },
 };
 
