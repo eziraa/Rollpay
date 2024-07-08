1a76ddbe9aa86f1cc6a2d9f801cba5540dcbf55a
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-08 12:02:11 +0300

    Added avg_basic_salary to StatisticsResponse interface

--- client/src/typo/statistics/response.tsx
+++ client/src/typo/statistics/response.tsx
@@ -7,6 +7,7 @@ export interface StatisticsResponse {
   curr_month_allowances: number;
   curr_month_deductions: number;
   curr_month_payment_amount: number;
+  avg_basic_salary: number;
 }
 
 export interface StatResponse extends BaseResponse {
