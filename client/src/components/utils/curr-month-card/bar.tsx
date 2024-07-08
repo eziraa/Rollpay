ec550f9dc1bdb0773d68b2daa77edc5477bc3bf2
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-08 16:11:48 +0300

    Statisctics of allowance of current month

--- client/src/components/utils/curr-month-card/bar.tsx
+++ client/src/components/utils/curr-month-card/bar.tsx
@@ -1,17 +1,50 @@
 // please install npm install react-apexcharts apexcharts
 import React, { useEffect, useState } from "react";
 import Chart from "react-apexcharts"; // Import the Chart component from react-apexcharts
-
+import { useStatistics } from "../../../hooks/statistics-hook";
+import { useAppDispatch } from "../../../utils/custom-hook";
+import { getStatRequest } from "../../../store/statistics/statistics-slice";
+interface ChartData {
+  series: number[];
+  labels: string[];
+}
 function Piechart() {
-  const [labels, setLabels] = useState<string[]>([]);
-  const [values, setValues] = useState<number[]>([]);
+  const { stat } = useStatistics();
+  const dispatcher = useAppDispatch();
+  const [chartData, setChartData] = useState<ChartData>({
+    series: [],
+    labels: [],
+  });
 
-  const label_list = ["Rejected", "Approved", "Pending"];
-  const mark_list = [40, 360, 79];
   useEffect(() => {
-    setValues(mark_list);
-    setLabels(label_list);
-  }, []);
+    dispatcher(getStatRequest());
+  }, [dispatcher]);
+  const colors = [
+    "#c24949",
+    "#00df7f",
+    "#faee03",
+    "#3b8ad9",
+    "#f7a35c",
+    "#9567e2",
+    "#49a9c2",
+    "#f15c80",
+  ];
+
+  
+  useEffect(() => {
+    if (stat.curr_month_allowance) {
+      const series = Object.values(stat.curr_month_allowance).map(
+        (allowance) => allowance || 0
+      );
+      const labels = Object.keys(stat.curr_month_allowance);
+
+      setChartData({
+        series: series,
+        labels: labels,
+      });
+    }
+  }, [stat.curr_month_allowance]);
+
 
   return (
     <React.Fragment>
@@ -20,12 +53,12 @@ function Piechart() {
           type="pie"
           width={400}
           height={400}
-          series={values}
+          series={chartData.series}
           options={{
-            title: { text: "Employees PieChart" },
+            title: { text: "Allowance PieChart" },
             noData: { text: "Empty Data" },
-            colors: ["#c24949", "#00df7f", "#faee03"],
-            labels: labels,
+            colors: colors.slice(0, chartData.series.length),
+            labels: chartData.labels,
           }}
         ></Chart>
       </div>
@@ -33,3 +66,4 @@ function Piechart() {
   );
 }
 export default Piechart;
+
