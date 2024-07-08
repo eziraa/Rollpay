744b7228218197b35da5fac87dc1c607b0371e5a
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-08 08:44:46 +0300

    Renamed url

--- server/employee/urls/employee.py
+++ server/employee/urls/employee.py
@@ -34,7 +34,7 @@ urlpatterns = [
          name='__update_emoployee__'),
     path("contract/<str:employee_id>",
          EmployementContract.as_view(), name='_employement_contract__'),
-     path('employee-number', views.EmployeeNumber.as_view(), name='__get_employee_number__'),
+     path('total', views.EmployeeNumber.as_view(), name='__get_total_employee__'),
      path('average-salary', salary_view.BasicSalaryAverage.as_view(), name='__average_basic_salary__'),
 
 
