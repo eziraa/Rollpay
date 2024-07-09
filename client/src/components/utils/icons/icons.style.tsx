77076c952a5f35cf51b118e121b89bd61fb1c05f
eziraa <ezratgab@gmail.com>
2024-07-09 23:16:29 +0300

    creating different icons and styling them

---
+++ client/src/components/utils/icons/icons.style.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { place_center } from "../flexbox/place-center.style";
+
+export const SmallIcon = styled.div`
+  ${place_center}
+  padding: 1rem;
+  font-size: 1.6rem;
+`;
+
+export const RoundedSmallIcon = styled(SmallIcon)`
+  border-radius: 50%;
+`;
+
+export const MediumIcon = styled.div`
+  ${place_center}
+  padding: 1rem;
+  font-size: 2rem;
+`;
+
+export const RoundedMediumIcon = styled(MediumIcon)`
+  border-radius: 50%;
+`;
+
+export const LargeIcon = styled.div`
+  ${place_center}
+  padding: 1rem;
+  font-size: 2.5rem;
+`;
+
+export const RoundedLargeIcon = styled(LargeIcon)`
+  border-radius: 50%;
+`;
+
+export const ExtraLargeIcon = styled(LargeIcon)`
+  font-size: 3.5rem;
+`;
+
+export const RoundedExtraLargeIcon = styled(ExtraLargeIcon)`
+  border-radius: 50%;
+`;
