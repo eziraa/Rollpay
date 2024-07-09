ec3d9002f9017b48a87b8a0679f5213e51aed702
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-09 12:10:43 +0300

    changed initial state of current month allowance

--- client/src/store/statistics/statistics-slice.tsx
+++ client/src/store/statistics/statistics-slice.tsx
@@ -12,7 +12,7 @@ const initialState: StatisticsState = {
     curr_month_deductions: 0,
     curr_month_payment_amount: 0,
     avg_basic_salary: 0,
-    curr_month_allowance: {},
+    curr_month_allowance: [],
   },
 };
 
