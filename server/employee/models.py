fef207cf5d6434c7dac8494551b8f5445cf5566e
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-10 14:40:36 +0300

    Changed property of start_at to auto_now_add

--- server/employee/models.py
+++ server/employee/models.py
@@ -59,7 +59,7 @@ class Allowance(models.Model):
     allowance_type = models.CharField(max_length=255, null=False)
     allowance_rate = models.DecimalField(
         max_digits=6, decimal_places=2, null=False)
-    start_at = models.DateTimeField(auto_now=True)
+    start_at = models.DateTimeField(auto_now_add=True)
     end_at = models.DateTimeField(null=True, blank=True)
 
 
