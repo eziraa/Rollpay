48872210ffd4e0b3a7bf8b0445f36ebdeb16a01f
eziraa <ezratgab@gmail.com>
2024-07-10 23:00:01 +0300

    creating componenet to indcate the bar graph limit value

---
+++ client/src/components/pages/user/dashboard/cross.tsx
@@ -0,0 +1,14 @@
+import { BrokenLine, LimitContainer } from "./dashboard.style";
+
+export const Cross = () => {
+  return (
+    <LimitContainer>
+      <BrokenLine />
+      <BrokenLine />
+      <BrokenLine />
+      <BrokenLine />
+      <BrokenLine />
+      <BrokenLine />
+    </LimitContainer>
+  );
+};
