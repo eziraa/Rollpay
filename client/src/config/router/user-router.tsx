d21fe9ad25b9e789b4da23ac8378661b32c778b0
eziraa <ezratgab@gmail.com>
2024-07-11 11:42:41 +0300

    code refatoring and code cleaning to diplay user allowances , decuctions and overtimes

--- client/src/config/router/user-router.tsx
+++ client/src/config/router/user-router.tsx
@@ -2,17 +2,20 @@ import { Route, Navigate } from "react-router-dom";
 import { UserHomePage } from "../../components/pages/user/home/home";
 import { UserDashboard } from "../../components/pages/user/dashboard/dashboard";
 import UserProfile from "../../components/pages/user-profile/user-profile";
-import UserAllowance from "../../components/pages/user-profile/user-allowance";
-import UserOvertime from "../../components/pages/user-profile/user-overtime";
-import UserDeductions from "../../components/pages/user-profile/user-deductions";
+import { EmployeeAllowance } from "../../components/sections/employee-allowance/allowance";
+import { EmployeeOvertime } from "../../components/sections/employee-overtime/overtime";
+import { EmployeeDeduction } from "../../components/sections/employee-deduction/deduction";
 export const UserRouterConfig = () => (
   <Route path="/" element={<UserHomePage />}>
     <Route path="/" element={<UserDashboard />} />
     <Route path="/user-profile/:employee_id" element={<UserProfile />}>
-      <Route path="allowances" element={<UserAllowance />} />
+      <Route path="allowances" element={<EmployeeAllowance />} />
       <Route path="" element={<Navigate to="allowances" replace />} />
-      <Route path="overtimes" element={<UserOvertime />} />
-      <Route path="deductions" element={<UserDeductions />} />
+      <Route path="allowances/:year/:month" element={<EmployeeAllowance />} />
+      <Route path="overtimes" element={<EmployeeOvertime />} />
+      <Route path="overtimes/:year/:month" element={<EmployeeOvertime />} />
+      <Route path="deductions" element={<EmployeeDeduction />} />
+      <Route path="deductions/:year/:month" element={<EmployeeDeduction />} />
     </Route>
   </Route>
 );
