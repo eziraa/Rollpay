7eb1001e09083107d2cf279dbf3e82d892067b23
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-11 10:13:01 +0300

    changed basic_salary type to string

--- client/src/typo/position/params.tsx
+++ client/src/typo/position/params.tsx
@@ -1,6 +1,6 @@
 export interface AddPositionParams {
   position_name: string;
-  basic_salary: number;
+  basic_salary: string;
 }
 export interface EditPositionParams extends AddPositionParams {
   id: string;
