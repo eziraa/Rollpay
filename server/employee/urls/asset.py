b652c30a28a1c050e497410d3a5398cfdb080472
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-12 15:45:19 +0300

    modified url of assets

--- server/employee/urls/asset.py
+++ server/employee/urls/asset.py
@@ -4,6 +4,8 @@ from employee.views.assets import AddAssetToEmployee, AssetDetail, AssetList
 
 urlpatterns = [
     path('<employee_id>/add', AddAssetToEmployee.as_view(), name='add-asset-to-employee'),
-    path('list', AssetList.as_view(), name='asset-list'),
-    path('<asset_id>', AssetDetail.as_view(), name='asset-detail'),
+    path('<employee_id>', AssetList.as_view(), name='asset-list'),
+    path('delete/<asset_id>', AssetDetail.as_view(), name='asset-detail'),
+    path('edit/<asset_id>', AssetDetail.as_view(), name='asset-detail'),
+
 ]
