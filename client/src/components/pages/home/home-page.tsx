42c85d2730f8f8044ca749356875802fbdb44943
eziraa <ezratgab@gmail.com>
2024-07-12 22:06:41 +0300

    defineing the left menu items list and their naviagation paths to able to navigate properly

--- client/src/components/pages/home/home-page.tsx
+++ client/src/components/pages/home/home-page.tsx
@@ -3,11 +3,78 @@ import { HomeBody, HomeContainer } from "./home-page.style";
 import LeftMenu from "../../sections/left-menu/left-menu";
 
 import { Outlet } from "react-router";
+import { HomeIcon, SalaryIcon } from "../../sections/left-menu/left-menu.style";
+import { MdExpandMore } from "react-icons/md";
+import { MenuItemInterface } from "../../../typo/menu/props";
+import { useState } from "react";
+import { FaUsers } from "react-icons/fa6";
 
 export const HomePage = () => {
+  const [colapseEmployees, setColapseEmployees] = useState(true);
+  const [colapseSalary, setColapseSalary] = useState(true);
+
+  const menuItems: MenuItemInterface[] = [
+    {
+      title: "Home",
+      path: "/",
+      icon: <HomeIcon />,
+      sub_menu_items: [],
+    },
+    {
+      title: "Employees",
+      path: "/employees",
+      icon: <FaUsers />,
+      exapandItems: setColapseEmployees,
+      items_expanded: colapseEmployees,
+      sub_menu_items: [
+        {
+          title: "Employees",
+          path: "/employees",
+          icon: <MdExpandMore />,
+        },
+        {
+          title: "Positions",
+          path: "/positions",
+          icon: <SalaryIcon />,
+        },
+      ],
+    },
+    {
+      title: "Payroll",
+      icon: <SalaryIcon />,
+      path: "/payroll",
+      items_expanded: colapseSalary,
+      exapandItems: setColapseSalary,
+      sub_menu_items: [
+        {
+          title: "Employees Payroll",
+          path: "/employees-salary",
+          icon: <SalaryIcon />,
+        },
+        {
+          title: "Deductions",
+          path: "/deductions",
+          icon: <SalaryIcon />,
+        },
+        {
+          title: "Allowances",
+          path: "/allowances",
+          icon: <SalaryIcon />,
+        },
+        {
+          title: "Overtimes",
+          path: "/overtimes",
+          icon: <SalaryIcon />,
+        },
+      ],
+    },
+
+    // Add more menu items here...
+  ];
+
   return (
     <HomeContainer>
-      <LeftMenu />
+      <LeftMenu menu_items={menuItems} />
       <HomeBody>
         <Header />
         <Outlet />
