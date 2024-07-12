b4c2c848056b18fb735563d4311328664896cd65
eziraa <ezratgab@gmail.com>
2024-07-12 22:09:13 +0300

    adding more props to the left menu item interface

--- client/src/typo/menu/props.tsx
+++ client/src/typo/menu/props.tsx
@@ -2,5 +2,7 @@ export interface MenuItemInterface {
   title: string;
   path: string;
   icon: JSX.Element;
+  items_expanded?: boolean;
+  exapandItems?: (bol: boolean) => void;
   sub_menu_items?: MenuItemInterface[];
 }
