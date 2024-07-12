8df4dcc71633f4f41fe6d99175486336cc80b773
eziraa <ezratgab@gmail.com>
2024-07-12 16:16:45 +0300

    updating the login page to fix login issue

--- client/src/components/pages/login/login.tsx
+++ client/src/components/pages/login/login.tsx
@@ -23,19 +23,18 @@ import { Link } from "react-router-dom";
 import { useFormik } from "formik";
 import { LogInSchema } from "../../../schema/log-in-schema";
 import { ErrorMessage } from "../sign-up/sign-up.style";
-import { useAppDispatch, useAppSelector } from "../../../utils/custom-hook";
+import { useAppDispatch } from "../../../utils/custom-hook";
 import { loginRequested } from "../../../store/user/user-slice";
 import { IoEyeOutline } from "react-icons/io5";
 import { FaRegEyeSlash } from "react-icons/fa";
 import { SmallSpinner } from "../../utils/spinner/spinner";
 import { Toggle } from "../../utils/buttons/toggle";
 import { EmpsDisplayerHeader as Header } from "../display-employee/display-employee.style";
+import { useUser } from "../../../hooks/user-hook";
 
 export const LoginPage = () => {
   const dispatcher = useAppDispatch();
-  const { is_login, login_error, logging_in } = useAppSelector(
-    (state) => state.user
-  );
+  const { is_login, login_error, logging_in } = useUser();
   const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
   const togglePasswordVisiblity = () => {
     setPasswordVisible(!passwordVisible);
@@ -46,6 +45,7 @@ export const LoginPage = () => {
       window.location.href = "/";
     }
   }, [is_login]);
+
   const { touched, values, handleBlur, handleChange, handleSubmit, errors } =
     useFormik({
       initialValues: {
