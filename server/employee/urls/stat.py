94b6551c2385b6b5898001d1d8ca36f0571b2354
eziraa <ezratgab@gmail.com>
2024-07-14 19:36:34 +0300

    adding end point for paymnet statisticss

--- server/employee/urls/stat.py
+++ server/employee/urls/stat.py
@@ -6,4 +6,5 @@ from employee.views.statistics import StatisticsView
 
 urlpatterns = [
     path('get', StatisticsView.as_view(), name="__get_statistics__"),
+    path('payment', StatisticsView.as_view(), name="__payment_statistics__"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
