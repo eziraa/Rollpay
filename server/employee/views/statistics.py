dcb4b40be7c647b1abc2221c15380b3dd445a6eb
eziraa <ezratgab@gmail.com>
2024-07-14 19:36:59 +0300

    creating a method for payment statistics

--- server/employee/views/statistics.py
+++ server/employee/views/statistics.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import *
 from employee.serializers.statistics import StatisticsSerializer
-
+from employee.serializers.payment_statistics import PaymentStatisticsSerilizer
+from month import Month
 
 class StatisticsView(APIView):
     """
@@ -14,3 +15,10 @@ class StatisticsView(APIView):
         serializer = StatisticsSerializer(payment)
   
         return Response(data=serializer.data, status=200)
+
+    def post(self, *args, **kwargs):
+        now = datetime.datetime.now()
+        current_month = Month(year=now.year, month=now.month-1)
+        serializer = PaymentStatisticsSerilizer.get_stat(month=current_month)
+        print(serializer)
+        return Response(data=serializer, status=200)
