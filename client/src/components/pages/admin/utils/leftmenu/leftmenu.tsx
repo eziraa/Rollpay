6191b9d4ebbd41e7dccb9ed37dc3dc3d82892cac
eziraa <ezratgab@gmail.com>
2024-07-16 01:01:48 +0300

    code refactoring

--- client/src/components/pages/admin/utils/leftmenu/leftmenu.tsx
+++ client/src/components/pages/admin/utils/leftmenu/leftmenu.tsx
@@ -1,14 +1,43 @@
+import { useLocation, useNavigate } from "react-router";
 import { MenuItem } from "../../../../sections/left-menu/left-menu.style";
 import { LeftMenuContainer } from "./leftmenu.style";
 
+const menu_items = [
+  {
+    title: "Dashboard",
+    path: "",
+  },
+  {
+    title: "Groups",
+    path: "/groups",
+  },
+  {
+    title: "Users",
+    path: "/users",
+  },
+  {
+    title: "Roles",
+    path: "/roles",
+  },
+  {
+    title: "Employees",
+    path: "/employees",
+  },
+];
+
 export const LeftMenu = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
   return (
     <LeftMenuContainer>
-      <MenuItem active={true}>DashBoard</MenuItem>
-      <MenuItem active={false}>Groups</MenuItem>
-      <MenuItem active={false}>Users</MenuItem>
-      <MenuItem active={false}>Roles</MenuItem>
-      <MenuItem active={false}>Employees</MenuItem>
+      {menu_items.map((menuItem) => (
+        <MenuItem
+          active={menuItem.path === pathname}
+          onClick={() => navigate(menuItem.path)}
+        >
+          {menuItem.title}
+        </MenuItem>
+      ))}
     </LeftMenuContainer>
   );
 };
