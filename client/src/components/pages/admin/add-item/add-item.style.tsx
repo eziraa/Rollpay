747a3b33f273e8f1ec10a459dd769f0de03b3ac1
eziraa <ezratgab@gmail.com>
2024-07-15 15:06:18 +0300

    creating adding item components and styling them

---
+++ client/src/components/pages/admin/add-item/add-item.style.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { addOpacityToColor } from "../../../utils/convertor/add-opacity-color";
+
+export const AddItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: start;
+  height: 100%;
+  flex: 1;
+  gap: 1rem;
+`;
+
+export const AddItemTitle = styled.p`
+  font-size: 2.1rem;
+  height: fit-content;
+  letter-spacing: 0.15rem;
+  color: ${({ theme }) => addOpacityToColor(0.6, theme.colors.primary)};
+`;
+
+export const AddItemForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 35%;
+  height: fit-content;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  width: 100%;
+`;
+export const AddItemInput = styled.input`
+  padding: 0.71rem;
+  border: 1px solid
+    ${({ theme }) => addOpacityToColor(0.7, theme.colors.primary)};
+  border-radius: 5px;
+  flex: 1.2;
+`;
+
+export const AddItemLabel = styled.label`
+  font-size: 1.3rem;
+  color: ${({ theme }) => addOpacityToColor(0.7, theme.colors.primary)};
+  flex: 0.8;
+`;
+
+export const ActionContainer = styled.div`
+  background-color: aliceblue;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  padding: 1rem;
+  width: 100%;
+  height: fit-content;
+`;
