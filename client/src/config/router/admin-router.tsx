4fd04b7a7cd8ac33b0c31b8b802aab6aeae7208b
eziraa <ezratgab@gmail.com>
2024-07-16 01:03:23 +0300

    updating the admin router confiuration

--- client/src/config/router/admin-router.tsx
+++ client/src/config/router/admin-router.tsx
@@ -1,10 +1,24 @@
-import { Route } from "react-router-dom";
+import { Navigate, Route } from "react-router-dom";
 import { AdminDashBoard } from "../../components/pages/admin/dashboard/dashbord";
-import { ModelItem } from "../../components/pages/admin/utils/model/item";
-import { AddGroup } from "../../components/pages/admin/add-item/add-item";
+import { DisplayUsers } from "../../components/pages/admin/users/users";
+import { DisplayRoles } from "../../components/pages/admin/roles/roles";
+import { DisplayGroups } from "../../components/pages/admin/groups/display-groups";
+import { AddGroup } from "../../components/pages/admin/groups/add-group";
+import { GroupsPage } from "../../components/pages/admin/groups/groups";
 export const AdminRouterConfig = () => (
   <Route path="/" element={<AdminDashBoard />}>
-    <Route path="" element={<ModelItem />} />
-    <Route path="add-group" element={<AddGroup />} />
+    <Route path="/users" element={<DisplayUsers />}>
+      <Route path="" element={<Navigate to="users" replace={true} />} />
+      <Route path="add-user" element={<AddGroup />} />
+    </Route>
+    <Route path="" element={<Navigate to="users" replace={true} />} />
+    <Route path="/groups" element={<GroupsPage />}>
+      <Route path="" element={<DisplayGroups />} />
+      <Route path="add-group" element={<AddGroup />} />
+    </Route>
+    <Route path="/roles" element={<DisplayRoles />}>
+      <Route path="" element={<Navigate to="roles" replace={true} />} />
+      <Route path="add-role" element={<AddGroup />} />
+    </Route>
   </Route>
 );
