d25968227bbec55508839421e473513fa5e25bd3
eziraa <ezratgab@gmail.com>
2024-07-15 15:11:37 +0300

    styling admin left menu components

---
+++ client/src/components/pages/admin/utils/leftmenu/leftmenu.style.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { addOpacityToColor } from "../../../../utils/convertor/add-opacity-color";
+
+export const LeftMenuContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  padding: 1rem;
+  height: 100%;
+  border: 0.2rem solid
+    ${({ theme }) => addOpacityToColor(0.1, theme.colors.primary)};
+`;
