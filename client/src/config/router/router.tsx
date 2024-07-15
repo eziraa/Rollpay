4310dc5a9d946575dbdbdcfc648f5c561342c451
eziraa <ezratgab@gmail.com>
2024-07-15 15:21:39 +0300

    change the router config to diaplay the admin interface

--- client/src/config/router/router.tsx
+++ client/src/config/router/router.tsx
@@ -2,12 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "../../components/pages/sign-up/sign-up";
 
 import { LoginPage } from "../../components/pages/login/login";
-import ProtectedRoute from "../utils/protected_route";
 import NotFoundPage from "../../components/pages/4_0_4/404";
 import AccessDenied from "../../components/pages/access-denied/access-denied";
 import { ChangePassword } from "../../components/pages/change-password/change-password";
 import { MainPage } from "../../components/pages/main/main";
-export const RouterConfig = ({ pathname }: { pathname: string }) => {
+import { AdminRouterConfig } from "./admin-router";
+export const RouterConfig = () => {
   return (
     <Router>
       <Routes>
@@ -16,7 +16,7 @@ export const RouterConfig = ({ pathname }: { pathname: string }) => {
         <Route path="/access-denied" element={<AccessDenied />} />
         <Route path="/change-password" element={<ChangePassword />} />
         <Route path="/" element={<MainPage />}>
-          {ProtectedRoute(pathname)}
+          {AdminRouterConfig()}
         </Route>
         {<Route path="*" element={<NotFoundPage />} />}
       </Routes>
