6d6b5262edf60f191c96faccb1043aea786c024d
eziraa <ezratgab@gmail.com>
2024-07-16 23:42:20 +0300

    fix endpoint issue

--- server/employee/urls/employee.py
+++ server/employee/urls/employee.py
@@ -44,7 +44,7 @@ urlpatterns = [
          employee.EmployeeView.as_view(), name='__add_overtime_to_emp__'),
     path("salary/get", salary_view.SalaryView.as_view(), name='__get_salary__'),
     path("salary/raise", salary_view.SalaryView.as_view(), name='__raise_salary__'),
-    path("salary/raise/<employee_id>",
+    path("salary/raise/<rate>",
          salary_view.SalaryView.as_view(), name='__raise_salary__'),
     path("salary/raise/<employee_id>/<rate>",
          salary_view.SalaryView.as_view(), name='__raise_salary__'),
