30d543e3137ce10b768c2f1b7e9e69c524dd8c51
eziraa <ezratgab@gmail.com>
2024-07-16 09:33:59 +0300

    creating a remote service method to able fetch permissions

--- client/src/services/admin-api.tsx
+++ client/src/services/admin-api.tsx
@@ -67,10 +67,54 @@ const getGroups = async () => {
   return groups;
 };
 
+const getPermissions = async () => {
+  const groups = await api
+    .get("permission/list")
+    .then((res) => {
+      return {
+        permissions: res.data,
+        code: res.status,
+        success: "Success returned overtimes",
+      };
+    })
+    .catch((err: AxiosError) => {
+      const { error } = err.response?.data as { error: string };
+      return {
+        error: error,
+        code: err.response?.status,
+      } as { error: string; code: number };
+    });
+
+  return groups;
+};
+
+const addGroup = async () => {
+  const groups = await api
+    .get("group/add")
+    .then((res) => {
+      return {
+        group: res.data,
+        code: res.status,
+        success: "Success returned overtimes",
+      };
+    })
+    .catch((err: AxiosError) => {
+      const { error } = err.response?.data as { error: string };
+      return {
+        error: error,
+        code: err.response?.status,
+      } as { error: string; code: number };
+    });
+
+  return groups;
+};
+
 const AdminAPI = {
   getRoles,
   getUsers,
   getGroups,
+  getPermissions,
+  addGroup,
 };
 
 export default AdminAPI;
