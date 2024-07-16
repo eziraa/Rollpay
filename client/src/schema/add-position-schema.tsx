dfa638c2933a799c04d8efb890ac3c4cd51c5f38
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-16 16:50:47 +0300

    modified validation schema for adding position

--- client/src/schema/add-position-schema.tsx
+++ client/src/schema/add-position-schema.tsx
@@ -1,6 +1,8 @@
 import * as Yup from "yup";
 
 export const AddPositionSchema = Yup.object({
-  position_name: Yup.string().required("Please Enter position name"),
-  basic_salary: Yup.number().required("Please Enter position inital salary"),
+  position_name: Yup.string()
+    .required("Please enter position name")
+    .min(3, "Position name should be atleast 3 characters"),
+  basic_salary: Yup.number().required("Please enter position inital salary"),
 });
