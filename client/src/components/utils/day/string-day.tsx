c2c36b1b771bf17d60f0173b7a0a11b68af72872
NetsanetAlemu <netsialemu1907@gmail.com>
2024-07-17 15:39:33 +0300

    Custom function to display date

--- client/src/components/utils/day/string-day.tsx
+++ client/src/components/utils/day/string-day.tsx
@@ -13,3 +13,20 @@ export const stringDay = (date: Date) => {
 
   return rearrangedString;
 };
+export const stringDatetine = (date: Date) => {
+  const dayString = date.toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+  });
+  const words = dayString.split(" ");
+  const extention =
+    date.getDate() === 1 ? "st" : date.getDate() === 2 ? "nd"  : date.getDate() === 3 ? "rd": "th";
+  const rearrangedString = `${words[2]}, ${date.getDate() + extention} ${
+    words[0]
+  } ${words[1]} ${words[3]}: ${words[4]}`;
+
+  return rearrangedString;
+};
