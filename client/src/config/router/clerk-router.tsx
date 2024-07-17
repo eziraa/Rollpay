891f25865d6ba72c3cd39c1fd52ee09750e20555
eziraa <ezratgab@gmail.com>
2024-07-17 13:46:58 +0300

    updating clrek router

--- client/src/config/router/clerk-router.tsx
+++ client/src/config/router/clerk-router.tsx
@@ -1,6 +1,6 @@
 import { Route, Navigate } from "react-router-dom";
 
-import { HomePage } from "../../components/pages/home/home-page";
+import { ClerkPage, HomePage } from "../../components/pages/home/home-page";
 import { EmployeesListPage } from "../../components/pages/display-employee/display-employee";
 import { SeeEmployee } from "../../components/pages/see-employee/see-employee";
 import { EmployeesSalaryPage } from "../../components/pages/salary/salary";
@@ -25,77 +25,111 @@ import { OvertimePage } from "../../components/pages/overtimes/overtime";
 import { AddDocument } from "../../components/sections/add-document/add-document";
 import { EmployeeAsset } from "../../components/sections/employee-asset/asset";
 import { RaiseSalary } from "../../components/sections/raise-salary/raise-salary";
+import { UserRouterConfig } from "./user-router";
 // import UserProfile from "../../components/pages/user-profile/user-profile";
-export const ClerkRouterConfig = () => (
-  <Route path="/" element={<HomePage />}>
-    <Route path="/" element={<DashBoard />} />
-    <Route path="/employees" element={<EmployeesListPage />}>
-      <Route path="add-employee" element={<AddEmployee />}>
-        <Route path="add-position" element={<AddPosition />} />
-      </Route>
-      <Route path="upload-document" element={<AddDocument />} />
-    </Route>
-    <Route path="/positions" element={<PositionPage />}>
-      <Route path="add-position" element={<AddPosition />} />
-      <Route path="edit-position/:position_id" element={<AddPosition />} />
-    </Route>
-    <Route path="/allowances" element={<AllowancePage />}>
-      <Route path="add-allowance" element={<AddAllowance />} />
-      <Route path="edit-allowance/:allowance_id" element={<AddAllowance />} />
-    </Route>
-    <Route path="/deductions" element={<DeductionPage />}>
-      <Route path="add-deduction" element={<AddDeduction />} />
-      <Route path="edit-deduction/:deduction_id" element={<AddDeduction />} />
-    </Route>
-    <Route path="/overtimes" element={<OvertimePage />}>
-      <Route path="add-overtime" element={<AddOvertime />} />
-      <Route path="edit-overtime/:overtime_id" element={<AddOvertime />} />
-    </Route>
-    <Route path="/employees/employee/delete" element={<CheckFlashMessage />} />
+export const ClerkRouterConfig = () => {
+  const deleteRoute = <Route path="delete" element={<CheckFlashMessage />} />;
+  return (
+    <Route path="/" element={<ClerkPage />}>
+      <Route path="/" element={<HomePage />}>
+        <Route path="/" element={<DashBoard />} />
+        <Route path="/employees" element={<EmployeesListPage />}>
+          <Route path="add-employee" element={<AddEmployee />}>
+            <Route path="add-position" element={<AddPosition />} />
+          </Route>
+          <Route path="upload-document" element={<AddDocument />} />
+        </Route>
+        <Route path="/positions" element={<PositionPage />}>
+          <Route path="add-position" element={<AddPosition />} />
+          <Route path="edit-position/:position_id" element={<AddPosition />} />
+        </Route>
+        <Route path="/allowances" element={<AllowancePage />}>
+          <Route path="add-allowance" element={<AddAllowance />} />
+          <Route
+            path="edit-allowance/:allowance_id"
+            element={<AddAllowance />}
+          />
+        </Route>
+        <Route path="/deductions" element={<DeductionPage />}>
+          <Route path="add-deduction" element={<AddDeduction />} />
+          <Route
+            path="edit-deduction/:deduction_id"
+            element={<AddDeduction />}
+          />
+        </Route>
+        <Route path="/overtimes" element={<OvertimePage />}>
+          <Route path="add-overtime" element={<AddOvertime />} />
+          <Route path="edit-overtime/:overtime_id" element={<AddOvertime />} />
+        </Route>
+        <Route
+          path="/employees/employee/delete"
+          element={<CheckFlashMessage />}
+        />
 
-    <Route path="/employees-salary" element={<EmployeesSalaryPage />}>
-      <Route path="raise" element={<RaiseSalary />} />
+        <Route path="/employees-salary" element={<EmployeesSalaryPage />}>
+          <Route path="raise" element={<RaiseSalary />} />
+        </Route>
+        <Route
+          path="/employees-salary/:year/:month"
+          element={<EmployeesSalaryPage />}
+        />
+        <Route
+          path="/employees/employee/:employee_id"
+          element={<SeeEmployee />}
+        >
+          <Route path="" element={<Navigate to="allowances" replace />} />
+
+          <Route path="allowances/:year/:month" element={<EmployeeAllowance />}>
+            {deleteRoute}
+
+            <Route path="add-allowance" element={<AddAllowanceToEmp />}>
+              <Route path="add-new-allowance" element={<AddAllowance />} />
+            </Route>
+          </Route>
+          <Route path="allowances" element={<EmployeeAllowance />}>
+            {deleteRoute}
+            <Route path="add-allowance" element={<AddAllowanceToEmp />}>
+              <Route path="add-new-allowance" element={<AddAllowance />} />
+            </Route>
+          </Route>
+          <Route path="assets" element={<EmployeeAsset />}>
+            {deleteRoute}
+
+            <Route path="add-asset" element={<AddDocument />} />
+          </Route>
+          <Route path="overtimes/:year/:month" element={<EmployeeOvertime />}>
+            {deleteRoute}
+
+            <Route path="add-overtime" element={<AddOvertimeToEmp />}>
+              <Route path="add-new-overtime" element={<AddOvertime />} />
+            </Route>
+          </Route>
+          <Route path="overtimes" element={<EmployeeOvertime />}>
+            {deleteRoute}
+
+            <Route path="add-overtime" element={<AddOvertimeToEmp />}>
+              <Route path="add-new-overtime" element={<AddOvertime />} />
+            </Route>
+          </Route>
+          <Route path="deductions/:year/:month" element={<EmployeeDeduction />}>
+            {deleteRoute}
+
+            <Route path="add-deduction" element={<AddDeductionToEmp />}>
+              <Route path="add-new-deduction" element={<AddDeduction />} />
+            </Route>
+          </Route>
+          <Route path="deductions" element={<EmployeeDeduction />}>
+            {deleteRoute}
+
+            <Route path="add-deduction" element={<AddDeductionToEmp />}>
+              <Route path="add-new-deduction" element={<AddDeduction />} />
+            </Route>
+          </Route>
+          <Route path="edit" element={<EditEmployee />} />
+          {deleteRoute}
+        </Route>
+      </Route>
+      {UserRouterConfig({ base_end_point: "me" })}
     </Route>
-    <Route
-      path="/employees-salary/:year/:month"
-      element={<EmployeesSalaryPage />}
-    />
-    <Route path="/employees/employee/:employee_id" element={<SeeEmployee />}>
-      <Route path="" element={<Navigate to="allowances" replace />} />
-      <Route path="allowances/:year/:month" element={<EmployeeAllowance />}>
-        <Route path="add-allowance" element={<AddAllowanceToEmp />}>
-          <Route path="add-new-allowance" element={<AddAllowance />} />
-        </Route>
-      </Route>
-      <Route path="allowances" element={<EmployeeAllowance />}>
-        <Route path="add-allowance" element={<AddAllowanceToEmp />}>
-          <Route path="add-new-allowance" element={<AddAllowance />} />
-        </Route>
-      </Route>
-      <Route path="assets" element={<EmployeeAsset />}>
-        <Route path="add-asset" element={<AddDocument />} />
-      </Route>
-      <Route path="overtimes/:year/:month" element={<EmployeeOvertime />}>
-        <Route path="add-overtime" element={<AddOvertimeToEmp />}>
-          <Route path="add-new-overtime" element={<AddOvertime />} />
-        </Route>
-      </Route>
-      <Route path="overtimes" element={<EmployeeOvertime />}>
-        <Route path="add-overtime" element={<AddOvertimeToEmp />}>
-          <Route path="add-new-overtime" element={<AddOvertime />} />
-        </Route>
-      </Route>
-      <Route path="deductions/:year/:month" element={<EmployeeDeduction />}>
-        <Route path="add-deduction" element={<AddDeductionToEmp />}>
-          <Route path="add-new-deduction" element={<AddDeduction />} />
-        </Route>
-      </Route>
-      <Route path="deductions" element={<EmployeeDeduction />}>
-        <Route path="add-deduction" element={<AddDeductionToEmp />}>
-          <Route path="add-new-deduction" element={<AddDeduction />} />
-        </Route>
-      </Route>
-      <Route path="edit" element={<EditEmployee />} />
-    </Route>
-  </Route>
-);
+  );
+};
