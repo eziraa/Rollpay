380b9b88441e1f6898d708db4c75c9888e9e7403
eziraa <ezratgab@gmail.com>
2024-07-17 13:47:16 +0300

    updating user routing issue

--- client/src/config/router/user-router.tsx
+++ client/src/config/router/user-router.tsx
@@ -5,10 +5,14 @@ import UserProfile from "../../components/pages/user-profile/user-profile";
 import { EmployeeAllowance } from "../../components/sections/employee-allowance/allowance";
 import { EmployeeOvertime } from "../../components/sections/employee-overtime/overtime";
 import { EmployeeDeduction } from "../../components/sections/employee-deduction/deduction";
-export const UserRouterConfig = () => (
-  <Route path="/" element={<UserHomePage />}>
-    <Route path="/" element={<UserDashboard />} />
-    <Route path="/user-profile/:employee_id" element={<UserProfile />}>
+export const UserRouterConfig = ({
+  base_end_point,
+}: {
+  base_end_point: string | undefined;
+}) => (
+  <Route path={base_end_point || ""} element={<UserHomePage />}>
+    <Route path={""} element={<UserDashboard />} />
+    <Route path="user-profile/:employee_id" element={<UserProfile />}>
       <Route path="allowances" element={<EmployeeAllowance />} />
       <Route path="" element={<Navigate to="allowances" replace />} />
       <Route path="allowances/:year/:month" element={<EmployeeAllowance />} />
