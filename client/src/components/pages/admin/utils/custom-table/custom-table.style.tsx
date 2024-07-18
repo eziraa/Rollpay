ce1a0c3b09d22d4b0b19b5b353e3cb011de8fab8
eziraa <ezratgab@gmail.com>
2024-07-18 15:55:04 +0300

    creating a check box

--- client/src/components/pages/admin/utils/custom-table/custom-table.style.tsx
+++ client/src/components/pages/admin/utils/custom-table/custom-table.style.tsx
@@ -32,7 +32,7 @@ export const CustomTable = styled.table<TableProps>`
   }
   tr {
     display: grid;
-    grid-template-columns: repeat(${({ keys }) => keys - 1}, 1fr) 1fr;
+    grid-template-columns: 0.5fr repeat(${({ keys }) => keys - 1}, 1fr) 1fr;
   }
   thead tr {
     background-color: ${({ theme }) =>
