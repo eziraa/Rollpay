22e93d9431920034854c2d235552d1ae94613b97
eziraa <ezratgab@gmail.com>
2024-07-18 15:53:51 +0300

    updating the add group to  able to use it for edit also

--- client/src/components/pages/admin/groups/add-group.tsx
+++ client/src/components/pages/admin/groups/add-group.tsx
@@ -14,22 +14,33 @@ import { useAdmin } from "../../../../hooks/admin-hook";
 import { useEffect, useState } from "react";
 import {
   addGroupRequest,
+  editGroupRequest,
   getPermissionsRequest,
 } from "../../../../store/admin/admin-slice";
 import { Permission } from "../../../../typo/admin/response";
 import { FormError } from "../../../utils/form-elements/form.style";
+import { useParams } from "react-router";
 
 export const AddGroup = () => {
   const dispatcher = useAppDispatch();
-  const { permissions, task_error } = useAdmin();
+  const { permissions, task_error, groups } = useAdmin();
   const [selectedOptions, setSelectedOptions] = useState<Permission[]>([]);
   const [name, setName] = useState("");
+  const { group_id } = useParams();
+
   const permission = {
     all_permissions: permissions,
     selected_permissions: selectedOptions,
     selectHandler: setSelectedOptions,
   };
-
+  const group = groups.find((g) => g.id == group_id);
+  useEffect(() => {
+    if (group_id && group) {
+      setName(group.name);
+      setSelectedOptions(group.permissions);
+    }
+    // Fetch permissions on component mount or group_id change
+  }, [group_id]);
   useEffect(() => {
     dispatcher(getPermissionsRequest());
   }, []);
@@ -54,18 +65,29 @@ export const AddGroup = () => {
         <AddBtn
           onClick={(e) => {
             e.preventDefault();
-            dispatcher(
-              addGroupRequest({
-                name: name,
-                permissions: selectedOptions.map(
-                  (permission) => permission.codename
-                ), // Step 3: Pass selected permissions to the request action creator.
-                onSuccess: () => {},
-                onError: () => {
-                  // Handle error if request fails
-                },
-              })
-            );
+            if (group_id)
+              dispatcher(
+                editGroupRequest({
+                  id: group_id || "",
+                  name: name,
+                  permissions: selectedOptions.map(
+                    (permission) => permission.codename
+                  ),
+                })
+              );
+            else
+              dispatcher(
+                addGroupRequest({
+                  name: name,
+                  permissions: selectedOptions.map(
+                    (permission) => permission.codename
+                  ), // Step 3: Pass selected permissions to the request action creator.
+                  onSuccess: () => {},
+                  onError: () => {
+                    // Handle error if request fails
+                  },
+                })
+              );
           }}
         >
           Save
