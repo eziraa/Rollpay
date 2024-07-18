84f4fa1acf0ae4fee8e455b0dfe6a60a1e84a7b1
eziraa <ezratgab@gmail.com>
2024-07-18 15:57:12 +0300

    creating a saga middlewate to able to edit and delte group

--- client/src/store/admin/admin-saga.tsx
+++ client/src/store/admin/admin-saga.tsx
@@ -4,6 +4,9 @@ import AdminAPI from "../../services/admin-api";
 import {
   addGroupDone,
   addGroupRequest,
+  deleteGroupRequest,
+  editGroupDone,
+  editGroupRequest,
   getGroupsDone,
   getGroupsRequest,
   getPermissionDone,
@@ -15,7 +18,7 @@ import {
   raiseError,
 } from "./admin-slice";
 import { setFlashMessage } from "../notification/flash-messsage-slice";
-import { AddGroupParams } from "../../typo/admin/params";
+import { AddGroupParams, EditGroupParams } from "../../typo/admin/params";
 import { PayloadAction } from "@reduxjs/toolkit";
 
 function* getUsers() {
@@ -216,7 +219,7 @@ function* addGroup(action: PayloadAction<AddGroupParams>) {
           type: "error",
           status: true,
           title: "Forbidden",
-          desc: "Not allowed to add allowance",
+          desc: "Not allowed to add groups",
           duration: 3,
         })
       );
@@ -236,10 +239,116 @@ function* addGroup(action: PayloadAction<AddGroupParams>) {
     );
   }
 }
+
+function* editGroup(action: PayloadAction<EditGroupParams>) {
+  try {
+    const response: AddGroupResponse = yield call(
+      AdminAPI.editGroup,
+      action.payload
+    );
+
+    if (response.code === 200) {
+      yield put(editGroupDone(response.group));
+      yield put(
+        setFlashMessage({
+          type: "success",
+          status: true,
+          title: "Editing Group",
+          desc: response.success,
+          duration: 3,
+        })
+      );
+    } else if (response.code === 401) {
+      yield put(raiseError(response.error));
+    } else if (response.code === 403) {
+      yield put(raiseError(response.error));
+      yield put(
+        setFlashMessage({
+          type: "error",
+          status: true,
+          title: "Forbidden",
+          desc: "Not allowed to add groups",
+          duration: 3,
+        })
+      );
+    } else {
+      yield put(raiseError(response.error));
+    }
+  } catch (_) {
+    yield put(raiseError("Failed please try again later"));
+    yield put(
+      setFlashMessage({
+        type: "error",
+        status: true,
+        title: "Add Group",
+        desc: "Failed please try again later",
+        duration: 3,
+      })
+    );
+  }
+}
+
+function* deleteGroup(action: PayloadAction<string[]>) {
+  try {
+    const response: AdminResponse = yield call(
+      AdminAPI.deleteGroup,
+      action.payload
+    );
+    if (response.code === 200) {
+      yield put(getGroupsDone(response.groups));
+      yield put(
+        setFlashMessage({
+          type: "success",
+          status: true,
+          title: "Deleting Group",
+          desc: "Group deleted successfully",
+          duration: 3,
+        })
+      );
+    } else if (response.code === 401) {
+      window.location.href = "/access-denied";
+      yield put(
+        setFlashMessage({
+          type: "error",
+          status: true,
+          title: "Unauthorized",
+          desc: "Please check your credentials",
+          duration: 3,
+        })
+      );
+    } else if (response.code === 403) {
+      window.location.href = "/access-denied";
+      yield put(
+        setFlashMessage({
+          type: "error",
+          status: true,
+          title: "Access Denied",
+          desc: "Not allowed to view groups",
+          duration: 3,
+        })
+      );
+    } else {
+      yield put(
+        setFlashMessage({
+          type: "error",
+          status: true,
+          title: "List Group",
+          desc: response.error,
+          duration: 3,
+        })
+      );
+    }
+  } catch (error) {
+    // TODO:
+  }
+}
+
 export function* watchAdminRequest() {
   yield takeEvery(getUsersRequest.type, getUsers);
   yield takeEvery(getGroupsRequest.type, getGroups);
   yield takeEvery(getRolesRequest.type, getRoles);
   yield takeEvery(getPermissionsRequest.type, getPermissions);
   yield takeEvery(addGroupRequest.type, addGroup);
+  yield takeEvery(deleteGroupRequest.type, deleteGroup);
+  yield takeEvery(editGroupRequest.type, editGroup);
 }
