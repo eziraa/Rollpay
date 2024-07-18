c402915d37b230427a63c4e5a13cd17a6b0735e3
eziraa <ezratgab@gmail.com>
2024-07-18 10:32:29 +0300

    code cleaning

--- client/src/config/utils/protected_route.tsx
+++ client/src/config/utils/protected_route.tsx
@@ -7,74 +7,29 @@ import { useState, useEffect } from "react";
 import { useAppDispatch } from "../../utils/custom-hook";
 import { getCurrentUserRequest } from "../../store/user/user-slice";
 import { useUser } from "../../hooks/user-hook";
-import { ClerkRouterConfig } from "../router/clerk-router";
-import { UserRouterConfig } from "../router/user-router";
 import { Navigate, Route } from "react-router-dom";
-import { AdminRouterConfig } from "../router/admin-router";
+import {
+  adminRoutes,
+  clerk_routes,
+  getRoutes,
+  userRoute,
+} from "../router/back";
+import { UserResponse } from "../../typo/user/response";
 
-function ProtectedRoute() {
-  const [isAuthorised, setIsAuthorized] = useState(false);
-  const dispatcher = useAppDispatch();
-  const { user } = useUser();
+function ProtectedRoute(isAuthorised: boolean, user: UserResponse) {
   const path = window.location.pathname;
-  useEffect(() => {
-    auth().catch(() => {
-      setIsAuthorized(false);
-    });
-  }, []);
-
-  const refreshToken = async () => {
-    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-    try {
-      const res = await api.post("/token/refresh/", {
-        refresh: refreshToken,
-      });
-      if (res.status === 200) {
-        localStorage.setItem(ACCESS_TOKEN, res.data.access);
-        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-
-        setIsAuthorized(true);
-      } else {
-        setIsAuthorized(false);
-      }
-    } catch (error) {
-      setIsAuthorized(false);
-    }
-  };
-
-  const auth = async () => {
-    const token = localStorage.getItem(ACCESS_TOKEN);
-
-    let decoded: {
-      exp: number;
-      user_id: string;
-    };
-    if (!token) {
-      setIsAuthorized(false);
-      return <Route path={path} element={<Navigate to="/login" replace />} />;
-    } else {
-      decoded = jwtDecode(token);
-      const tokenExpiration = decoded.exp;
-      const now = Date.now() / 1000;
-      if (tokenExpiration)
-        if (tokenExpiration < now) {
-          await refreshToken();
-        } else {
-          setIsAuthorized(true);
-          dispatcher(getCurrentUserRequest(decoded.user_id));
-        }
-    }
-  };
   if (isAuthorised) {
-    return user?.role === "Clerk"
-      ? ClerkRouterConfig()
-      : user?.role === "sys_admin"
-      ? AdminRouterConfig()
-      : user?.role === "user"
-      ? UserRouterConfig()
-      : undefined;
+    return user?.role === "Clerk" ? (
+      getRoutes(clerk_routes)
+    ) : user?.role === "sys_admin" ? (
+      getRoutes(adminRoutes)
+    ) : user?.role === "user" ? (
+      getRoutes(userRoute("/"))
+    ) : (
+      <Route path={path} element={<Navigate to={"/access-denied"} />} />
+    );
   } else {
-    return;
+    return <Route path={path} element={<Navigate to={"/login"} />} />;
   }
 }
 
