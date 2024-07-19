15d3d16b18252fe71d6be3c202d25b54313e5b06
eziraa <ezratgab@gmail.com>
2024-07-19 07:12:15 +0300

    dispalying employees

---
+++ client/src/components/pages/admin/employees/add-employee.tsx
@@ -0,0 +1,162 @@
+import { AddBtn } from "../../../sections/add_employee/add-employee.style";
+import {
+  ActionContainer,
+  AddItemContainer,
+  AddItemForm,
+  AddItemInput,
+  AddItemTitle,
+  InputContainer,
+  AddItemLabel,
+} from "../utils/add-item/add-item.style";
+import { useAppDispatch } from "../../../../utils/custom-hook";
+import { useAdmin } from "../../../../hooks/admin-hook";
+import { useState } from "react";
+import { addUserRequest } from "../../../../store/admin/admin-slice";
+import {
+  FormError,
+  PasswordContainer,
+} from "../../../utils/form-elements/form.style";
+import { useFormik } from "formik";
+import { SignUpSchema } from "../../../../schema/sign-up-schema";
+import { ErrorMessage } from "../../sign-up/sign-up.style";
+import { PasswordVisible } from "../../../utils/password-visiblity/password.style";
+import { IoEyeOutline } from "react-icons/io5";
+import { FaRegEyeSlash } from "react-icons/fa";
+
+export const AddUser = () => {
+  const dispatcher = useAppDispatch();
+  const { adding, task_error } = useAdmin();
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [password_C_Visible, setPassword_C_Visible] = useState<boolean>(false);
+
+  const togglePassword_C_Visiblity = () => {
+    setPassword_C_Visible(!password_C_Visible);
+  };
+  const togglePasswordVisiblity = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const initialValues = {
+    username: "",
+    empID: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const { touched, values, handleBlur, handleChange, handleSubmit, errors } =
+    useFormik({
+      initialValues,
+      validationSchema: SignUpSchema,
+      onSubmit: (values) => {
+        if (!adding) {
+          dispatcher(addUserRequest(values));
+        }
+      },
+    });
+  return (
+    <AddItemContainer>
+      <AddItemTitle>Add User </AddItemTitle>
+      <AddItemForm
+        style={{
+          width: "100%",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <InputContainer
+          style={{
+            display: "grid",
+            gridTemplateColumns: "0.7fr 1fr 1fr 1fr",
+          }}
+        >
+          <AddItemLabel htmlFor="username">Username: </AddItemLabel>
+          <AddItemInput
+            type="text"
+            name="username"
+            value={values.username}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          {touched.username && errors.username && (
+            <ErrorMessage>{errors.username} </ErrorMessage>
+          )}
+        </InputContainer>
+
+        <InputContainer
+          style={{
+            display: "grid",
+            gridTemplateColumns: "0.7fr 1fr 1fr 1fr",
+          }}
+        >
+          <AddItemLabel htmlFor="empID">Employee ID: </AddItemLabel>
+          <AddItemInput
+            type="text"
+            name="empID"
+            value={values.empID}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          {touched.empID && errors.empID && (
+            <ErrorMessage>{errors.empID} </ErrorMessage>
+          )}
+        </InputContainer>
+        <InputContainer
+          style={{
+            display: "grid",
+            gridTemplateColumns: "0.7fr 1fr 1fr 1fr",
+          }}
+        >
+          <AddItemLabel htmlFor="password">Password: </AddItemLabel>
+          <PasswordContainer>
+            <PasswordVisible onClick={togglePasswordVisiblity}>
+              {passwordVisible ? <IoEyeOutline /> : <FaRegEyeSlash />}
+            </PasswordVisible>
+
+            <AddItemInput
+              type={passwordVisible ? "text" : "password"}
+              name="password"
+              value={values.password}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            />
+          </PasswordContainer>
+
+          {touched.password && errors.password && (
+            <ErrorMessage>{errors.password}</ErrorMessage>
+          )}
+        </InputContainer>
+
+        <InputContainer
+          style={{
+            display: "grid",
+            gridTemplateColumns: "0.7fr 1fr 1fr 1fr",
+          }}
+        >
+          <AddItemLabel htmlFor="confirmPassword">
+            Confirm Password:{" "}
+          </AddItemLabel>
+          <PasswordContainer>
+            <PasswordVisible onClick={togglePassword_C_Visiblity}>
+              {password_C_Visible ? <IoEyeOutline /> : <FaRegEyeSlash />}
+            </PasswordVisible>
+
+            <input
+              type={password_C_Visible ? "text" : "password"}
+              name="confirmPassword"
+              value={values.confirmPassword}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            />
+          </PasswordContainer>
+
+          {touched.confirmPassword && errors.confirmPassword && (
+            <ErrorMessage>{errors.confirmPassword} </ErrorMessage>
+          )}
+        </InputContainer>
+        {task_error && <ErrorMessage>{task_error}</ErrorMessage>}
+        <ActionContainer>
+          <AddBtn type="submit">Save</AddBtn>
+          {task_error && <FormError> {task_error} </FormError>}
+        </ActionContainer>
+      </AddItemForm>
+    </AddItemContainer>
+  );
+};
