54db4961aabbfc11f6a8fe4bf4da78bd1e8c5e7b
eziraa <ezratgab@gmail.com>
2024-10-03 09:51:45 +0300

    change the auth provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { AuthProvider } from "./providers/auth-provider.tsx";
+import { Provider } from "react-redux";
+import { store } from "./utils/store.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Provider>
+);
