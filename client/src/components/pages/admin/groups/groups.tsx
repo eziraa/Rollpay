41a8384d7c2844a01efcaf6fcbc70312908ceb6d
eziraa <ezratgab@gmail.com>
2024-10-05 21:12:20 +0300

    adding loader indicator

--- client/src/components/pages/admin/groups/groups.tsx
+++ client/src/components/pages/admin/groups/groups.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router";
 import { PageBody } from "../utils/body/page-body";
-
+import { useNavigation } from "react-router-dom";
+import BodyLoader from "../../../utils/loading/body-loading";
 export const GroupsPage = () => {
-  return (
-    <PageBody>
-      <Outlet />
-    </PageBody>
-  );
+  const isLoading = useNavigation().state === "loading";
+  return <PageBody>{isLoading ? <BodyLoader /> : <Outlet />}</PageBody>;
 };
