ab50287fd149e87156e4fca8d08b4b164951d911
eziraa <ezratgab@gmail.com>
2024-10-07 19:15:38 +0300

    updating roles displayer to able to edit and delete roles

--- client/src/components/pages/admin/roles/display-roles.tsx
+++ client/src/components/pages/admin/roles/display-roles.tsx
@@ -1,9 +1,12 @@
 // export const CustomTable =
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAdmin } from "../../../../hooks/admin-hook";
 import { useAppDispatch } from "../../../../utils/custom-hook";
-import { getRolesRequest } from "../../../../store/admin/admin-slice";
+import {
+  deleteRoleRequest,
+  getRolesRequest,
+} from "../../../../store/admin/admin-slice";
 import { ThreeDots } from "../../../utils/loading/dots";
 import { NoResult } from "../../../utils/containers/containers.style";
 import { CustomTable } from "../utils/custom-table/custom-table.style";
@@ -22,10 +25,13 @@ import { AddBtn } from "../../../sections/add_employee/add-employee.style";
 import { useNavigate } from "react-router";
 import { BlurredIcon } from "../utils/icons/icons.style";
 import { FaSearch } from "react-icons/fa";
-import { Label, Select } from "../utils/dropdown/dropdown.style";
+import { Label, Option, Select } from "../utils/dropdown/dropdown.style";
 import { DropDown } from "../utils/dropdown/dropdown";
 import { Group } from "../../../../typo/admin/response";
-
+import { Checkbox, CircularProgress } from "@mui/material";
+import { useNavigation } from "react-router-dom";
+import { setFlashMessage } from "../../../../store/notification/flash-messsage-slice";
+import DeleteConfirmationModal from "../utils/model/ConfirmitionModal";
 export const DisplayRoles = () => {
   /**
    * Calling hooks and getting necessary imformations
@@ -33,7 +39,32 @@ export const DisplayRoles = () => {
   const dispacher = useAppDispatch();
   const navigate = useNavigate();
   const { roles, loading } = useAdmin();
+  const [checkedRoles, setCheckedRoles] = useState<string[]>([]);
+  const [action, setAction] = useState<string>("");
+  const isLoading = useNavigation().state === "loading";
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+    setAction("");
+    setCheckedRoles([]);
+  };
+
+  const handleDelete = () => {
+    dispacher(deleteRoleRequest(checkedRoles));
+    closeModal();
+  };
+  const handleCheckboxChange = (userId: string) => {
+    setCheckedRoles(
+      (currentChecked: string[]) =>
+        currentChecked.includes(userId)
+          ? currentChecked.filter((id) => id !== userId) // Uncheck
+          : [...currentChecked, userId] // Check
+    );
+  };
   useEffect(() => {
     dispacher(getRolesRequest());
   }, []);
@@ -55,6 +86,12 @@ export const DisplayRoles = () => {
           Add New
         </AddBtn>
       </ItemHeader>
+      {isOpen && (
+        <DeleteConfirmationModal
+          handleClose={closeModal}
+          action={handleDelete}
+        />
+      )}
       <ItemBody>
         <SearchContainer>
           <BlurredIcon>
@@ -65,12 +102,76 @@ export const DisplayRoles = () => {
         </SearchContainer>
         <ActionContainer>
           <Label>Action:</Label>
-          <DropDown />
-          <ActionButton>Apply</ActionButton>
+          <Select
+            onChange={(e) => {
+              e.target.value && setAction(e.target.value);
+            }}
+            value={action || "default"}
+          >
+            <Option value="default" disabled>
+              action
+            </Option>
+            <Option value="delete">Delete</Option>
+            <Option value="edit">edit</Option>
+          </Select>
+          {isLoading ? (
+            <CircularProgress size={20} />
+          ) : (
+            <ActionButton
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                if (checkedRoles.length < 1) {
+                  dispacher(
+                    setFlashMessage({
+                      title: "Reminder",
+                      desc: "Please select a role",
+                      status: true,
+                      duration: 5,
+                      type: "error",
+                    })
+                  );
+                  return;
+                }
+                if (action === "delete") {
+                  openModal();
+                } else if (action === "edit") {
+                  if (checkedRoles.length > 1) {
+                    dispacher(
+                      setFlashMessage({
+                        title: "Reminder",
+                        desc: "Please select only one role to edit",
+                        status: true,
+                        duration: 5,
+                        type: "error",
+                      })
+                    );
+                    return;
+                  }
+                  dispacher(getRolesRequest());
+                  navigate(`${checkedRoles[0]}/edit`);
+                } else {
+                  dispacher(
+                    setFlashMessage({
+                      title: "Reminder",
+                      desc: "Please select an action",
+                      status: true,
+                      duration: 5,
+                      type: "error",
+                    })
+                  );
+                  return;
+                }
+              }}
+            >
+              Apply
+            </ActionButton>
+          )}
         </ActionContainer>
         <CustomTable keys={Object.keys(roles[0]).length} className="shadow-md">
           <thead>
             <tr>
+              <th>Action</th>
               {Object.keys(roles[0]).map((key) => (
                 <th>{key.at(0)?.toUpperCase() + key.slice(1)}</th>
               ))}
@@ -79,6 +180,13 @@ export const DisplayRoles = () => {
           <tbody>
             {roles.map((user) => (
               <tr>
+                <td>
+                  <Checkbox
+                    value={user.id}
+                    checked={checkedRoles.includes(user.id)}
+                    onChange={() => handleCheckboxChange(user.id)}
+                  />
+                </td>
                 {Object.entries(user).map(([key, value]) => (
                   <td>
                     {key === "groups" ? (
