0f08b434ff07c588e6df9fca4915a69e39ba8e5f
ezira <ezratgab@gmail.com>
2024-10-16 08:35:48 +0300

    adding csrf token

--- client/src/config/api.tsx
+++ client/src/config/api.tsx
@@ -1,17 +1,21 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "../constants/token-constants";
-export const baseURL = import.meta.env.VITE_API_URL
-  ? import.meta.env.VITE_API_URL
-  : "";
+import { getCSRFToken } from "./csrf";
+import GLOBAL_URLS from "./global_urls";
+export const baseURL = import.meta.env.VITE_API_URL || "";
+
 const api = axios.create({
   baseURL,
 });
-
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
+    }
+    const csrfToken = getCSRFToken();
+    if (csrfToken) {
+      config.headers["X-CSRFToken"] = csrfToken;
     }
     return config;
   },
@@ -19,5 +23,21 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (axios.isAxiosError(error) && error.response) {
+      if (
+        error.response.status === 401 &&
+        !GLOBAL_URLS.includes(window.location.pathname)
+      ) {
+        localStorage.removeItem(ACCESS_TOKEN);
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 export default api;
