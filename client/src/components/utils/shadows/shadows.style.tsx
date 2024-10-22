a0409177db54e2c9db04838e5cbb507f0980fc5a
eziraa <ezratgab@gmail.com>
2024-10-22 21:31:31 +0300

    update custom shadow css object

--- client/src/components/utils/shadows/shadows.style.tsx
+++ client/src/components/utils/shadows/shadows.style.tsx
@@ -3,11 +3,11 @@ import { addOpacityToColor } from "../convertor/add-opacity-color";
 
 export const mini_shadow = css`
   box-shadow: 0.1rem 0.1rem 1rem
-      ${({ theme }) => addOpacityToColor(0.1, theme.colors.primary)},
+      ${({ theme }) => addOpacityToColor(0.1, theme.buttons.primary)},
     0.1rem 0.1rem 1rem
-      ${({ theme }) => addOpacityToColor(0.1, theme.colors.primary)},
+      ${({ theme }) => addOpacityToColor(0.1, theme.buttons.primary)},
     0.1rem 0.1rem 1rem
-      ${({ theme }) => addOpacityToColor(0.1, theme.colors.primary)},
+      ${({ theme }) => addOpacityToColor(0.1, theme.buttons.primary)},
     0.1rem 0.1rem 1rem
-      ${({ theme }) => addOpacityToColor(0.1, theme.colors.primary)};
+      ${({ theme }) => addOpacityToColor(0.1, theme.buttons.primary)};
 `;
