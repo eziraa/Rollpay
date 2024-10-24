1c419c8bba90d62c32228f7227d8d39f1343e5aa
eziraa <ezratgab@gmail.com>
2024-10-24 12:05:08 +0300

    adding end point to get one group

--- server/employee/urls/group.py
+++ server/employee/urls/group.py
@@ -3,6 +3,7 @@ from employee.views.group import GroupView
 urlpatterns = [
     path("add", GroupView.as_view(), name='__add_groups__'),
     path("list", GroupView.as_view(), name='__get_groups__'),
+    path("list/<group_id>", GroupView.as_view(), name='__get_groups__'),
     path("edit/<group_id>", GroupView.as_view(), name='__get_groups__'),
     path("add_user/<group_id>", GroupView.as_view(), name='__get_groups__'),
     path("delete", GroupView.as_view(), name='__delete_groups__'),
