9f5d562f7ba54eeda0e54d6ce6478e1bf2cc4452
eziraa <ezratgab@gmail.com>
2024-10-24 12:06:28 +0300

    create end points for crud operations on Role model

--- server/employee/urls/role.py
+++ server/employee/urls/role.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from employee.views.roles import RoleView
 urlpatterns = [
-    path('list', RoleView.as_view(), name='__get_roles__')
+    path("add", RoleView.as_view(), name='__add_roles__'),
+    path("list", RoleView.as_view(), name='__get_roles__'),
+    path("list/<role_id>", RoleView.as_view(), name='__get_roles__'),
+    path("edit/<role_id>", RoleView.as_view(), name='__get_roles__'),
+    path("add_user/<role_id>", RoleView.as_view(), name='__get_roles__'),
+    path("delete", RoleView.as_view(), name='__delete_roles__'),
+    path("edit", RoleView.as_view(), name='__delete_roles__'),
+    path("delete/<role_id>", RoleView.as_view(), name='__get_roles__'),
 ]
